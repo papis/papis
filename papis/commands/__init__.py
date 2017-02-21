f2c57e212e107e984f2efcba31ea670f9ba556ba
Alejandro Gallo <aamsgallo@gmail.com>
2017-02-21 23:37:16 +0100

    Add initialization function for commands

--- papis/commands/__init__.py
+++ papis/commands/__init__.py
@@ -1,11 +1,38 @@
+COMMANDS = [
+"add",
+"check",
+"config",
+"edit",
+"export",
+"list",
+"no_command",
+"open",
+"test",
+"update"
+]
+
+def init(parser):
+    """TODO: Docstring for init.
+
+    :parser: TODO
+    :returns: TODO
+
+    """
+    global COMMANDS
+    commands = dict()
+    for command in COMMANDS:
+        exec("from .%s import %s as cmd"%(command, command.capitalize()))
+        commands[command] = cmd(parser)
+    return commands
 
 class Command(object):
 
     def __init__(self, parser):
         self.parser = parser
         self.args = None
+        self.init(self.parser)
 
-    def initialize_parser(self):
+    def init(self):
         pass
 
     def main(self, args=None):
