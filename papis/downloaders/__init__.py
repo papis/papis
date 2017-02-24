ec849f4097a37db3b16e0200ac4ea1e41465babe
Alejandro Gallo <aamsgallo@gmail.com>
2017-02-24 18:08:50 +0100

    Add aps downloader

--- papis/downloaders/__init__.py
+++ papis/downloaders/__init__.py
@@ -1,17 +1,34 @@
+import logging
+import re
+import aps.Aps
 
+def getDownloader(url):
+    """TODO: Docstring for getDownloader.
+
+    :url: TODO
+    :returns: TODO
+
+    """
+    if re.match(r".*aps.org.*", url):
+        return aps.Aps
 
 class Downloader(object):
 
     """Base class for downloaders"""
 
-    def __init__(self):
-        self.url = ""
+    def __init__(self, url=""):
+        self.url = url
+        self.type = self.__class__.__name__.lower()
+        self.logger = logging.getLogger(self.type)
+        self.bibtex_data = ""
 
-    def getBibtex(self):
+    def getBibtexData(self):
         """TODO: Docstring for getBibtex.
         :returns: TODO
 
         """
+        return self.bibtex_data
+    def getBibtexUrl(self):
         pass
     def downloadBibtex(self):
         """Bibtex downloader, it should try to download bibtex information from
@@ -19,6 +36,8 @@ class Downloader(object):
         :returns: TODO
 
         """
+        pass
+    def getDocumentUrl(self):
         pass
     def downloadDocument(self):
         """Document downloader, it should try to download bibtex information
---
+++ papis/downloaders/aps.py
@@ -0,0 +1,31 @@
+from .downloaders import Downloader
+import re
+import urllib.request
+
+
+class Aps(Downloader):
+
+    """Docstring for Aps. """
+
+    def __init__(self, url):
+        """TODO: to be defined1. """
+        Downloader.__init__(self, url)
+
+    def getBibtexUrl(self):
+        # http://journals.aps.org/prl/export/10.1103/
+        # PhysRevLett.115.066402?type=bibtex&download=true
+        # http://journals.aps.org/prl/abstract/10.1103/PhysRevLett.115.066402
+        url = self.getUrl()
+        burl = re.sub(r"org/prl/abstract","org/prl/export", url)\
+                +"?type=bibtex&download=true"
+        self.logger.debug("Bibtex url %s"%burl)
+        return burl
+    def downloadBibtex(self, arg1):
+        """TODO: Docstring for downloadBibtex.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        data = urllib.request.urlopen(self.getBibtexUrl()).read().decode('utf-8')
+        self.bibtex_data = data
