0ad441cc0a1fe526b45696e8d94fa34d4d7def13
Alejandro Gallo <aamsgallo@gmail.com>
2017-04-17 15:55:35 +0500

    Add get_epub_info

--- papis/utils.py
+++ papis/utils.py
@@ -5,6 +5,8 @@ import os
 import re
 import papis.pick
 from .document import Document
+import zipfile
+from lxml import etree
 
 logger = logging.getLogger("utils")
 
@@ -103,3 +105,30 @@ def getInfoFileName():
 
     """
     return "info.yaml"
+
+
+def get_epub_info(fname):
+    ns = {
+        'n': 'urn:oasis:names:tc:opendocument:xmlns:container',
+        'pkg': 'http://www.idpf.org/2007/opf',
+        'dc': 'http://purl.org/dc/elements/1.1/'
+    }
+
+    # prepare to read from the .epub file
+    zip = zipfile.ZipFile(fname)
+
+    # find the contents metafile
+    txt = zip.read('META-INF/container.xml')
+    tree = etree.fromstring(txt)
+    cfname = tree.xpath('n:rootfiles/n:rootfile/@full-path', namespaces=ns)[0]
+
+    # grab the metadata block from the contents metafile
+    cf = zip.read(cfname)
+    tree = etree.fromstring(cf)
+    p = tree.xpath('/pkg:package/pkg:metadata', namespaces=ns)[0]
+
+    # repackage the data
+    res = {}
+    for s in ['title', 'language', 'creator', 'date', 'identifier']:
+        res[s] = p.xpath('dc:%s/text()' % (s), namespaces=ns)[0]
+    return res
