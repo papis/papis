0563fb400974b3b63723741c3f87c8ce031c7b39
Alejandro Gallo <aamsgallo@gmail.com>
2017-05-01 00:06:36 +0200

    Implement for libgen

---
+++ papis/downloaders/libgen.py
@@ -0,0 +1,34 @@
+import re
+import bs4
+import sys
+import papis.downloaders.base
+import urllib.request
+
+
+class Downloader(papis.downloaders.base.Downloader):
+
+    def __init__(self, url):
+        papis.downloaders.base.Downloader.__init__(self, url)
+
+    @classmethod
+    def match(cls, url):
+        # http://libgen.io/ads.php?md5=CBA569C45B32CA3DF52E736CD8EF6340
+        if re.match(r".*libgen.*", url):
+            return Downloader(url)
+        else:
+            return False
+
+    def downloadBibtex(self):
+        """Bibtex downloader, it should try to download bibtex information from
+        the url
+        :returns: TODO
+
+        """
+        raw_data = urllib.request.urlopen(self.getUrl())\
+            .read()\
+            .decode('utf-8')
+        soup = bs4.BeautifulSoup(raw_data, "html.parser")
+        textareas = soup.find_all("textarea")
+        if not textareas:
+            return False
+        self.bibtex_data = textareas[0].text
