100de9329c2e5469f6c0d452617ea5a227cd66fc
Alejandro Gallo <aamsgallo@gmail.com>
2017-05-09 00:07:44 +0200

    Add pick in list

--- papis/commands/list.py
+++ papis/commands/list.py
@@ -15,7 +15,8 @@ class List(Command):
         list_parser = self.parser.add_parser(
             "list",
             help="List documents from a given library"
-            )
+        )
+
         list_parser.add_argument(
             "document",
             help="Document search",
@@ -23,6 +24,7 @@ class List(Command):
             nargs="?",
             action="store"
         )
+
         list_parser.add_argument(
             "-i",
             "--info",
@@ -30,18 +32,26 @@ class List(Command):
             default=False,
             action="store_true"
         )
+
         list_parser.add_argument(
             "-f",
             "--file",
             help="Show the file name associated with the document",
-            default=False,
             action="store_true"
         )
+
         list_parser.add_argument(
             "-d",
             "--dir",
             help="Show the folder name associated with the document",
             default=True,
+            action="store_true"
+        )
+
+        list_parser.add_argument(
+            "-p",
+            "--pick",
+            help="Pick the document instead of listing everything",
             action="store_true"
         )
 
@@ -61,6 +71,8 @@ class List(Command):
             documentsDir,
             documentSearch
         )
+        if args.pick:
+            documents = [self.pick(documents, config)]
         for document in documents:
             if args.file:
                 for f in document.getFiles():
