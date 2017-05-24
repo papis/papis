e276e64c3d891b1f3e6e25ce59fbfcfa020802c2
Alejandro Gallo <aamsgallo@gmail.com>
2017-05-24 11:54:22 +0200

    Clean up main

--- papis/papis.py
+++ papis/papis.py
@@ -1,9 +1,6 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#  Import modules {{{1  #
-#########################
-
 import sys
 import logging
 
@@ -13,18 +10,16 @@ import papis.config
 
 logger = logging.getLogger("papis")
 
-if sys.version_info < (3, 0):
-    raise Exception("This script must use python 3.0 or greater")
-    sys.exit(1)
-
-
-#  Utility functions {{{1  #
-############################
-
 
 def main():
     papis.commands.init()
     papis.commands.main()
 
+
+if sys.version_info < (3, 2):
+    raise Exception("This script must use python 3.2 or greater")
+
+if __name__ == "__main__":
+    main()
 
 # vim:set et sw=4 ts=4 ft=python:
