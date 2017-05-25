4ef56f69bb73f346cfb4e194b5aba2f2b6a52081
Alejandro Gallo <aamsgallo@gmail.com>
2017-05-26 00:38:35 +0200

    Remove rofi from gui

--- papis/commands/gui.py
+++ papis/commands/gui.py
@@ -23,58 +23,9 @@ class Gui(papis.commands.Command):
         return papis.utils.get_documents_in_lib(self.args.lib)
 
     def rofi_main(self):
-        import rofi
         import papis.rofi
-        # Set default picker
-        key = None
-        index = None
-        options = papis.rofi.get_options()
-        header_format = papis.rofi.get_header_format()
-        header_filter = lambda x: header_format.format(doc=x)
-        esc_key = -1
-        quit_key = 1
-        edit_key = 2
-        delete_key = 3
-        open_key = 0
-        keys = {
-            "key%s" % quit_key: ('Alt+q', 'Quit'),
-            "key%s" % edit_key: ('Alt+e', 'Edit'),
-            "key%s" % delete_key: ('Alt+d', 'Delete'),
-            "key%s" % open_key: ('Enter', 'Open')
-        }
-        options.update(keys)
-        # Initialize window
-        w = rofi.Rofi()
-        while not (key == quit_key or key == esc_key):
-            index, key = w.select( "Select: ",
-                [
-                    header_filter(d) for d in
-                    self.documents
-                ],
-                select=index,
-                **options
-            )
-            if key == edit_key:
-                papis.utils.general_open(
-                    self.documents[index].get_info_file(),
-                    "xeditor",
-                    default_opener="xterm -e vim",
-                    wait=True
-                )
-                self.documents[index].load()
-            elif key == open_key:
-                return papis.utils.open_file(
-                    self.documents[index].get_files()
-                )
-            elif key == delete_key:
-                answer = w.text_entry(
-                    "Are you sure? (y/N)",
-                    message="<b>Be careful!</b>"
-                )
-                if answer and answer in "Yy":
-                    self.documents[index].rm()
-                    self.documents = self.fetch_documents()
+        return papis.rofi.Gui().main(self.documents)
 
     def main(self):
         self.documents = self.fetch_documents()
-        self.rofi_main()
+        return self.rofi_main()
