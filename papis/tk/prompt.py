0358ebb410b1f38c7e2815895c213c496ee2f362
Alejandro Gallo <aamsgallo@gmail.com>
2017-05-31 10:52:42 +0200

    Add Command class

--- papis/tk/prompt.py
+++ papis/tk/prompt.py
@@ -1,6 +1,17 @@
 from papis.tk import PapisWidget
 from papis.tk import tk
 
+class Command(object):
+
+    def __init__(self, name, function, prompt, nargs=0):
+        self.name = name
+        self.function = function
+        self.nargs = nargs
+        self.prompt = prompt
+
+    def run(self, *args):
+        return self.function(*args)
+
 class Prompt(tk.Text,PapisWidget):
 
     def __init__(self, *args, **kwargs):
@@ -9,6 +20,7 @@ class Prompt(tk.Text,PapisWidget):
         self.bind("<Control-u>", self.clear)
         self.command = ""
         self.last_command = ""
+        self.registered_commands = []
 
     def changed(self):
         self.get_command()
@@ -16,6 +28,13 @@ class Prompt(tk.Text,PapisWidget):
             return False
         else:
             return True
+
+    def register_command(self, cmd):
+        self.register_command.append(cmd)
+
+    def add_new_command(self, name, function, nargs=0):
+        cmd = Command(name, function, self, nargs)
+        self.register_command(cmd)
 
     def get_command(self):
         self.last_command = self.command
@@ -37,5 +56,8 @@ class Prompt(tk.Text,PapisWidget):
         self.focus_set()
 
     def autocomplete(self, event=None):
+        pass
+
+    def register_command(self, arg1):
         pass
 
