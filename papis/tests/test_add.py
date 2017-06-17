91ff7dfcf6fe641478c00ee3ccb2607c1f67fa2d
Alejandro Gallo <aamsgallo@gmail.com>
2017-06-17 15:03:11 +0200

    Add test

--- papis/tests/test_add.py
+++ papis/tests/test_add.py
@@ -23,6 +23,14 @@ class TestAdd(CommandTest):
         super(self, self).setUpClass()
         self.args = dict()
         self.command = papis.commands.get_commands()["add"]
+        self.config_path = os.path.join(
+            os.path.dirname(__file__),
+            "resources",
+            "config_1.ini"
+        )
+        # papis.config.set_config_file(self.config_path)
+        # papis.commands.Command.config = papis.config.reset_configuration()
+        # self.papers_dir = papis.config.get_configuration()["papers"]["dir"]
 
     @classmethod
     def tearDownClass(self):
@@ -32,15 +40,19 @@ class TestAdd(CommandTest):
         self.assertTrue(self.command is not None)
 
     def test_config_file_exists(self):
-        self.assertTrue(
-            os.path.exists(
-                os.path.join(
-                    os.path.dirname(__file__),
-                    "resources",
-                    "config_1.ini"
-                )
-            )
+        self.assertTrue(os.path.exists(self.config_path))
+
+    def test_help_message(self):
+        # TODO: Check if thread returns succesfully or not
+        import threading
+        args = ( "add", "-h" )
+        t = threading.Thread(
+            target=papis.commands.main,
+            args=[args]
         )
+        t.start()
+        t.join()
+        self.assertFalse(t.isAlive())
 
     def test_extension(self):
         docs = [
