a99f54715196229703dc187cc321b7b6e0d13778
Alejandro Gallo <aamsgallo@gmail.com>
2017-07-27 11:15:19 +0200

    Add exceptions module to papis

---
+++ papis/exceptions.py
@@ -0,0 +1,9 @@
+"""This module implements custom exceptions used to make the code more
+readable.
+"""
+
+class NotImplemented(Exception):
+    """This exception is to be used when something
+    throughout the code is not yet implemented.
+    """
+    pass
