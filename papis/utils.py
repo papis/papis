00396228df7cd5fdcf62e2ca3f214d6829e75f92
Alejandro Gallo <aamsgallo@gmail.com>
2017-07-28 23:30:46 +0200

    Remove which function, not needed

--- papis/utils.py
+++ papis/utils.py
@@ -69,24 +69,6 @@ def get_libraries():
     return libs
 
 
-def which(program):
-    # source
-    # stackoverflow.com/questions/377017/test-if-executable-exists-in-python
-    def is_exe(fpath):
-        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
-    fpath, fname = os.path.split(program)
-    if fpath:
-        if is_exe(program):
-            return program
-    else:
-        for path in os.environ["PATH"].split(os.pathsep):
-            path = path.strip('"')
-            exe_file = os.path.join(path, program)
-            if is_exe(exe_file):
-                return exe_file
-    return None
-
-
 def pick(options, papis_config={}, pick_config={}):
     import papis.config
     logger.debug("Parsing picktool")
