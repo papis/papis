37c82c579dcf416dff22257443794d4883cd8385
Alejandro Gallo <aamsgallo@gmail.com>
2017-07-28 11:34:32 +0200

    Update config command

--- papis/commands/config.py
+++ papis/commands/config.py
@@ -7,59 +7,28 @@ import papis.commands
 
 class Config(papis.commands.Command):
     def init(self):
-        """TODO: Docstring for init.
-
-        :subparser: TODO
-        :returns: TODO
-
-        """
 
         self.parser = self.get_subparsers().add_parser(
             "config",
-            help="Manage the configuration options"
+            help="Print configuration values"
         )
 
         self.parser.add_argument(
             "option",
-            help="Set or get option",
+            help="Variable to print",
             default="",
-            nargs="*",
             action="store"
         )
 
     def main(self):
-        documentsDir = os.path.expanduser(
-            self.get_config()[self.args.lib]["dir"]
-        )
-        self.logger.debug("Using directory %s" % documentsDir)
-        # FIXME: Replacing values does not work
-        option = " ".join(self.args.option)
+        option = self.args.option.split(".")
         self.logger.debug(option)
-        value = False
-        m = re.match(r"([^ ]*)\.(.*)", option)
-        if not m:
-            self.logger.error("Syntax for option %s not recognised" % option)
-            sys.exit(1)
-        lib = m.group(1)
-        preKey = m.group(2)
-        m = re.match(r"(.*)\s*=\s*(.*)", preKey)
-        if m:
-            key = m.group(1)
-            value = m.group(2)
-        else:
-            key = preKey
-        self.logger.debug("lib -> %s" % lib)
-        self.logger.debug("key -> %s" % key)
-        if not value:
-            if key in self.get_config()[lib].keys():
-                print(self.get_config()[lib][key])
-            else:
-                sys.exit(1)
-        else:
-            try:
-                self.get_config().remove_option(lib, key)
-                self.get_config().set(lib, key, value)
-            except configparser.NoSectionError:
-                self.get_config().add_section(lib)
-                self.get_config().set(lib, key, value)
-            self.get_config().save()
+        if len(option) == 1:
+            key = option[0]
+            section = None
+        elif len(option) == 2:
+            section = option[0]
+            key = option[1]
+        self.logger.debug("key = %s, sec = %s" % (key, section))
+        val = papis.config.get(key, section=section)
+        print(val)
