5ac60b6d8210e5cbf0e6cac076c513c6e023e4e7
Alejandro Gallo <aamsgallo@gmail.com>
2017-09-14 00:59:37 +0200

    Add tools for updating pypi

---
+++ tools/update-pypi.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+DIST_DIR=dist
+
+echo "Updating man pages"
+make -C doc man
+
+rm -rf ${DIST_DIR}
+python3 setup.py sdist
+
+read -p "Do you want to push? (y/N)" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  #echo "Uploading to test server"
+  #twine upload -r test ${DIST_DIR}/*.tar.gz
+  twine upload ${DIST_DIR}/*.tar.gz
+fi
+REPLY= # unset REPLY after using it
