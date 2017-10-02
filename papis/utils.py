931b7416dcfd6e1d6efb3b62b7e4593aa90e3d95
Alejandro Gallo <aamsgallo@gmail.com>
2017-10-02 16:26:30 +0200

    Fix alphanums for papis_key

--- papis/utils.py
+++ papis/utils.py
@@ -173,7 +173,7 @@ class DocMatcher(object):
         cls.logger.debug('Parsing search')
         search = search or cls.search
         papis_alphas = pyparsing.printables.replace('=', '')
-        papis_key = pyparsing.Word(pyparsing.alphas)
+        papis_key = pyparsing.Word(pyparsing.alphanums)
         papis_value = pyparsing.QuotedString(
             quoteChar='"', escChar='\\', escQuote='\\'
         ) ^ pyparsing.QuotedString(
