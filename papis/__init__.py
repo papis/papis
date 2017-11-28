e9f9e69355d930ff467ba1ec632a5c9b269da97f
Alejandro Gallo <aamsgallo@gmail.com>
2017-11-28 22:29:32 +0100

    Update version

--- papis/__init__.py
+++ papis/__init__.py
@@ -1,7 +1,7 @@
 
 # Information
-__license__ = 'GPL3'
-__version__ = '0.4.6'
+__license__ = 'GPLv3'
+__version__ = '0.4.7'
 __author__ = __maintainer__ = 'Alejandro Gallo'
 __email__ = 'aamsgallo@gmail.com'
 
