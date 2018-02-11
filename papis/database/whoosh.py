3971973df14b8c228314e78f4bd10dc883182fb1
Alejandro Gallo <aamsgallo@gmail.com>
2018-02-11 02:52:15 +0100

    Implement bare minimum for whoosh

--- papis/database/whoosh.py
+++ papis/database/whoosh.py
@@ -32,16 +32,93 @@ class Database(papis.database.base.Database):
     def index_exists(self):
         return whoosh.index.exists_in(self.get_index_dir())
 
-    def get_index_dir(self):
-        return os.path.join(
-            papis.database.cache.get_cache_home(),
-            'whoosh',
-            papis.database.cache.get_name(self.get_dir())
-        )
+    def do_indexing(self):
+        self.logger.debug('Indexing the library, this might take a while...')
+        folders = papis.utils.get_folders(self.get_dir())
+        documents = papis.database.cache.folders_to_documents(folders)
+        schema_keys = self.get_schema_init_fields().keys()
+        writer = self.get_writer()
+        for doc in documents:
+            doc_d = dict()
+            doc_d.update(
+                {
+                    k: str(doc[k]) or ''
+                    for k in schema_keys
+                }
+            )
+            doc_d['whoosh_id_'] = doc.get_main_folder()
+            writer.add_document(**doc_d)
+        writer.commit()
 
-    def get_index_file(self):
-        return os.path.join(
-            self.get_index_dir(),
-            papis.database.cache.get_name(self.get_dir())
+    def initialize(self):
+        if self.index_exists():
+            self.logger.debug('Initialized index found for library')
+            return True
+        self.create_index()
+        self.do_indexing()
+
+    def get_index(self):
+        return whoosh.index.open_dir(self.get_index_dir())
+
+    def get_writer(self):
+        return self.get_index().writer()
+
+    def get_schema(self):
+        from whoosh.fields import Schema
+        self.logger.debug('Creating schema')
+        fields = self.get_schema_init_fields()
+        schema = Schema(**fields)
+        return schema
+
+    def get_schema_init_fields(self):
+        from whoosh.fields import TEXT, KEYWORD, ID, STORED
+        return {
+            'whoosh_id_': ID(stored=True),
+            'author': TEXT(stored=True),
+            'title': TEXT(stored=True),
+            'year': TEXT(stored=True),
+            'tags': TEXT(stored=True)
+        }
+
+    def query(self, query_string):
+        self.logger.debug('Query string %s' % query_string)
+        index = self.get_index()
+        qp = whoosh.qparser.QueryParser(
+            'title',
+            schema=index.schema
         )
+        query = qp.parse(query_string)
+        with index.searcher() as searcher:
+            results = searcher.search(query, limit=None)
+            self.logger.debug(results)
+            documents = [
+                papis.document.from_folder(r.get('whoosh_id_'))
+                for r in results
+            ]
+        return documents
+
+
+    def clear(self):
+        import shutil
+        self.logger.warning('Clearing the database')
+        if self.index_exists():
+            shutil.rmtree(self.get_index_dir())
+
+    def get_cache_dir(self):
+        path = os.path.join(
+            papis.database.cache.get_cache_home(),
+            'whoosh'
+        )
+        #self.logger.debug('Cache dir %s' % path)
+        return path
+
+    def get_index_dir(self):
+        path = os.path.expanduser(
+            os.path.join(
+                self.get_cache_dir(),
+                papis.database.cache.get_name(self.get_dir())
+            )
+        )
+        #self.logger.debug('Index dir %s' % path)
+        return path
 
