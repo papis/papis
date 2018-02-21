59777824643d2ba28d5f7cb39b16c236480d0728
Alejandro Gallo <aamsgallo@gmail.com>
2018-02-22 00:40:31 +0100

    Fix add cli command to work well with database

--- papis/database/__init__.py
+++ papis/database/__init__.py
@@ -2,10 +2,13 @@
 
 def get(library=None):
     import papis.config
-    if papis.config.get("database-backend") == "papis":
+    backend = papis.config.get('database-backend')
+    if backend == "papis":
         import papis.database.cache
         return papis.database.cache.Database(library)
-    elif papis.config.get("database-backend") == "whoosh":
+    elif backend == "whoosh":
         import papis.database.whoosh
         return papis.database.whoosh.Database(library)
+    else:
+        raise Exception('No valid database type: {}'.format(backend))
 
