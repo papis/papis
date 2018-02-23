0a161c9d75c00bba52ece42de6b2ca62cae70787
Alejandro Gallo <aamsgallo@gmail.com>
2018-02-23 22:13:08 +0100

    Add dictionary with database in database.get

--- papis/database/__init__.py
+++ papis/database/__init__.py
@@ -1,14 +1,23 @@
+import papis.config
+import papis.exceptions
+import logging
+logger = logging.getLogger('database')
 
+DATABASES = dict()
 
 def get(library=None):
     import papis.config
     backend = papis.config.get('database-backend')
+    if DATABASES.get(library) is not None:
+        return DATABASES.get(library)
     if backend == "papis":
         import papis.database.cache
-        return papis.database.cache.Database(library)
+        DATABASES[library] = papis.database.cache.Database(library)
+        return DATABASES.get(library)
     elif backend == "whoosh":
         import papis.database.whoosh
-        return papis.database.whoosh.Database(library)
+        DATABASES[library] = papis.database.whoosh.Database(library)
+        return DATABASES.get(library)
     else:
         raise Exception('No valid database type: {}'.format(backend))
 
