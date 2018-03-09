097db0fd725135f8b8575fe8ed5f5cc85b192680
Alejandro Gallo <aamsgallo@gmail.com>
2018-03-09 18:28:36 +0100

    Implement test for bibtex in update

---
+++ papis/tests/test_update.py
@@ -0,0 +1,62 @@
+import os
+import papis.bibtex
+import json
+import yaml
+import tempfile
+import unittest
+import papis.tests
+import papis.config
+from papis.commands.update import run
+
+
+class Test(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        papis.tests.setup_test_library()
+
+    def get_docs(self):
+        db = papis.database.get()
+        return db.get_all_documents()
+
+    def test_bibtex(self):
+        db = papis.database.get()
+        bibstring = """
+        @article{10.1002/andp.19053221004,
+          author = { A. Einstein },
+          doi = { 10.1002/andp.19053221004 },
+          issue = { 10 },
+          journal = { Ann. Phys. },
+          pages = { 891--921 },
+          title = { Zur Elektrodynamik bewegter K\"{o}rper },
+          type = { article },
+          volume = { 322 },
+          year = { 1905 },
+        }
+
+        @article{10.1002/andp.19053221004,
+          author = { Perico de los palotes },
+          doi = { 10.1002/andp.19053221004 },
+          issue = { 10 },
+          journal = { Ann. Phys. },
+          pages = { 891--921 },
+          title = { La biblia en HD },
+          type = { article },
+          volume = { 322 },
+          year = { 1905 },
+        }
+        """
+        bibfile = tempfile.mktemp()
+        with open(bibfile, 'w+') as fd:
+            fd.write(bibstring)
+
+        docs = self.get_docs()
+        run(docs[0], from_bibtex=bibfile)
+        docs = db.query_dict(dict(title='elektrodynamik'))
+        self.assertTrue(len(docs) == 0)
+
+        docs = self.get_docs()
+        run(docs[0], force=True, from_bibtex=bibfile)
+        docs = db.query_dict(dict(title='elektrodynamik'))
+        self.assertTrue(len(docs) > 0)
+        self.assertTrue(docs[0]["volume"] == "322")
