6310e8aa15c964297c4eabbf5c7d941a545b7e30
Alejandro Gallo <aamsgallo@gmail.com>
2018-08-02 03:14:14 +0200

    Update check

--- papis/commands/check.py
+++ papis/commands/check.py
@@ -1,26 +1,28 @@
 """
 This command checks for several attributes in every document.
 
-For example if you want to chekc that every document of your library has valid
+For example if you want to check that every document of your library has valid
 files related to it you can just do
 
 ::
 
-    papis check --keys files
+    papis check --key files
 
 this will check that every info file has the key files and that every file
 listed exists.
 
-You can also define your own ones by inputing comma separated keys, e.g. if you
-want to check that every document has files, a valid author and title you would
-just hut
+You can also define more complicated ones, e.g., if you want to check that
+every document has files, a valid author and title you would just hit
 
 ::
 
-    papis check --keys files author title
+    papis check --key files --key author --key title
 """
 import papis.api
 import papis.config
+import papis.cli
+import click
+import logging
 
 
 def run(keys, documents):
@@ -43,36 +45,31 @@ def run(keys, documents):
     return result
 
 
-class Command(papis.commands.Command):
-    def init(self):
 
-        self.parser = self.get_subparsers().add_parser(
-            "check",
-            help="Check document from a given library"
-        )
-
-        self.add_search_argument()
-
-        self.parser.add_argument(
-            "--keys", "-k",
-            help="Space separated fields to check against",
-            nargs='*',
-            default=eval(papis.config.get('check-keys')),
-            action="store"
-        )
-
-    def main(self):
-        documents = self.get_db().query(self.args.search)
-        self.logger.debug(self.args.keys)
-        troubled_docs = run(self.args.keys, documents)
-        for doc in troubled_docs:
-            print(
-                "{d[key]} - {d[msg]} - {folder}".format(
-                    d=doc, folder=doc['doc'].get_main_folder()
-                )
+@click.command()
+@click.help_option('--help', '-h')
+@papis.cli.query_option()
+@click.option(
+    "--key", "-k",
+    help="Space separated fields to check against",
+    type=str,
+    multiple=True,
+    default=lambda: eval(papis.config.get('check-keys'))
+)
+def cli(query, key):
+    """Check document from a given library"""
+    documents = papis.database.get().query(query)
+    logger = logging.getLogger('cli:check')
+    logger.debug(key)
+    troubled_docs = run(key, documents)
+    for doc in troubled_docs:
+        print(
+            "{d[key]} - {d[msg]} - {folder}".format(
+                d=doc, folder=doc['doc'].get_main_folder()
             )
+        )
 
-        if not len(troubled_docs) == 0:
-            print("Errors were detected, please fix the info files")
-        else:
-            print("No errors detected")
+    if not len(troubled_docs) == 0:
+        print("Errors were detected, please fix the info files")
+    else:
+        print("No errors detected")
