14a5a623d0246965fd1797b433e60c3ad6522ad6
Alejandro Gallo <aamsgallo@gmail.com>
2018-08-02 11:55:40 +0200

    Update explore

--- papis/commands/explore.py
+++ papis/commands/explore.py
@@ -5,197 +5,209 @@ import papis.config
 import papis.bibtex
 import urllib.request
 import tempfile
+import papis.cli
+import click
+import logging
 
 
-class Command(papis.commands.Command):
+logger = logging.getLogger('explore')
 
-    def init(self):
 
-        self.parser = self.get_subparsers().add_parser(
-            "explore",
-            help="Explore on the internet"
-        )
-
-        self.parser.add_argument(
-            "search",
-            help="Search string",
-            default="",
-            action="store"
-        )
-
-        self.parser.add_argument(
-            "--isbnplus",
-            help="Search through isbnplus.org",
-            action="store_true"
-        )
-
-        self.parser.add_argument(
-            "--arxiv",
-            help="Search on the arxiv",
-            action="store_true"
-        )
-
-        self.parser.add_argument(
-            "--libgen",
-            help="Search on library genesis",
-            action="store_true"
-        )
-
-        self.parser.add_argument(
-            "--crossref",
-            help="Search on library genesis",
-            action="store_true"
-        )
-
-        self.parser.add_argument(
-            "--add",
-            help="Add document selected",
-            action="store_true"
-        )
-
-        self.parser.add_argument(
-            "--max",
-            help="Maximum number of items",
-            default=30,
-            action="store"
-        )
-
-        self.parser.add_argument(
-            "--cmd",
-            help="Issue a command on the retrieved document "
-                 "using papis format",
-            default=None,
-            action="store"
-        )
-
-    def parse_search(self):
-        import papis.docmatcher
-        key_vals = papis.docmatcher.parse_query(self.args.search)
-        result = {'query': ""}
-        self.logger.debug('Parsed set %s' % key_vals)
-        for pair in key_vals:
-            if len(pair) == 3:
-                key = pair[0]
-                val = pair[2]
-                result[key] = val
-            else:
-                val = pair[0]
-                result['query'] += ' ' + val
-        return result
-
-    def add(self, doc):
-        if self.args.libgen:
-            if not doc.has('doc_url'):
-                self.logger.error('No doc_url data retrieved')
-                return 1
-            self.logger.info('Downloading document')
-            doc_data = urllib.request.urlopen(
-                doc['doc_url']
-            ).read()
-            file_name = tempfile.mktemp()
-            with open(file_name, 'wb+') as fd:
-                fd.write(doc_data)
-            papis.commands.main(
-                ['add', '--from-url', doc['doc_url'], file_name]
-            )
-        elif self.args.arxiv:
-            if not doc.has('url'):
-                self.logger.error('No url data retrieved')
-                return 1
-            papis.commands.main(
-                ['add', '--from-url', doc['url']]
-            )
-
-    def libgen(self, search):
-        from pylibgen import Library
-        parsed = self.parse_search()
-        lg = Library()
-        ids = []
-        for key in ['title', 'author', 'isbn']:
-            if parsed.get(key):
-                ids += lg.search(ascii(parsed.get(key)), key)
-        if len(ids) == 0:
-            ids = lg.search(ascii(parsed.get('query')), 'title')
-        if len(ids):
-            data = lg.lookup(ids)
+def parse_search(query):
+    import papis.docmatcher
+    key_vals = papis.docmatcher.parse_query(query)
+    result = {'query': ""}
+    logger.debug('Parsed set %s' % key_vals)
+    for pair in key_vals:
+        if len(pair) == 3:
+            key = pair[0]
+            val = pair[2]
+            result[key] = val
         else:
-            self.logger.error("No documents found")
-            return None
-        doc = self.pick(
-            [papis.document.Document(data=d) for d in data]
-        )
-        if doc:
-            doc['doc_url'] = lg.get_download_url(doc['md5'])
-        return doc
+            val = pair[0]
+            result['query'] += ' ' + val
+    return result
 
-    def crossref(self, search):
-        import papis.crossref
-        parsed = self.parse_search()
-        data = papis.crossref.get_data(
-            query=parsed.get('query'),
-            author=parsed.get('author'),
-            title=parsed.get('title'),
-            year=parsed.get('year'),
-            max_results=self.args.max
+def add(doc, libgen=False, arxiv=False):
+    if libgen:
+        if not doc.has('doc_url'):
+            logger.error('No doc_url data retrieved')
+            return 1
+        logger.info('Downloading document')
+        doc_data = urllib.request.urlopen(
+            doc['doc_url']
+        ).read()
+        file_name = tempfile.mktemp()
+        with open(file_name, 'wb+') as fd:
+            fd.write(doc_data)
+        papis.commands.main(
+            ['add', '--from-url', doc['doc_url'], file_name]
         )
-        documents = [papis.document.Document(data=d) for d in data]
-        doc = self.pick(
-            documents
+    elif arxiv:
+        if not doc.has('url'):
+            error('No url data retrieved')
+            return 1
+        papis.commands.main(
+            ['add', '--from-url', doc['url']]
         )
-        return doc
 
-    def isbnplus(self, search):
-        import papis.isbn
-        data = papis.isbn.get_data(query=search)
-        doc = self.pick(
-            [papis.document.Document(data=d) for d in data]
-        )
-        return doc
+def libgen(query):
+    from pylibgen import Library
+    parsed = parse_search(query)
+    lg = Library()
+    ids = []
+    for key in ['title', 'author', 'isbn']:
+        if parsed.get(key):
+            ids += lg.search(ascii(parsed.get(key)), key)
+    if len(ids) == 0:
+        ids = lg.search(ascii(parsed.get('query')), 'title')
+    if len(ids):
+        data = lg.lookup(ids)
+    else:
+        logger.error("No documents found")
+        return None
+    doc = papis.cli.pick(
+        [papis.document.Document(data=d) for d in data]
+    )
+    if doc:
+        doc['doc_url'] = lg.get_download_url(doc['md5'])
+    return doc
 
-    def arxiv(self, search):
-        import papis.arxiv
-        parsed = self.parse_search()
-        data = papis.arxiv.get_data(
-            query=parsed.get('query'),
-            author=parsed.get('author'),
-            title=parsed.get('title'),
-            abstract=parsed.get('abstract'),
-            comment=parsed.get('comment'),
-            journal=parsed.get('journal'),
-            report_number=parsed.get('report_number'),
-            category=parsed.get('category'),
-            id_list=parsed.get('id_list'),
-            page=parsed.get('page') or 0,
-            max_results=self.args.max
-        )
-        doc = self.pick(
-            [papis.document.Document(data=d) for d in data]
-        )
-        return doc
+def crossref(search, max_results):
+    import papis.crossref
+    parsed = parse_search(query)
+    data = papis.crossref.get_data(
+        query=parsed.get('query'),
+        author=parsed.get('author'),
+        title=parsed.get('title'),
+        year=parsed.get('year'),
+        max_results=max_results
+    )
+    documents = [papis.document.Document(data=d) for d in data]
+    doc = papis.cli.pick(
+        documents
+    )
+    return doc
 
-    def main(self):
-        doc = None
-        if self.args.arxiv:
-            doc = self.arxiv(self.args.search)
-        elif self.args.isbnplus:
-            doc = self.isbnplus(self.args.search)
-        elif self.args.crossref:
-            doc = self.crossref(self.args.search)
-        elif self.args.libgen:
-            doc = self.libgen(self.args.search)
-        else:
-            self.args.arxiv = True
-            doc = self.arxiv(self.args.search)
+def isbnplus(query):
+    import papis.isbn
+    data = papis.isbn.get_data(query=query)
+    doc = papis.cli.pick(
+        [papis.document.Document(data=d) for d in data]
+    )
+    return doc
 
-        if doc:
-            print(papis.document.dump(doc))
-            if self.args.add:
-                self.add(doc)
-            elif self.args.cmd is not None:
-                from subprocess import call
-                command = papis.utils.format_doc(
-                    self.args.cmd,
-                    doc
-                )
-                self.logger.debug('Calling "%s"' % command)
-                call(command.split(" "))
+def arxiv(query, max_results):
+    import papis.arxiv
+    parsed = parse_search(query)
+    data = papis.arxiv.get_data(
+        query=parsed.get('query'),
+        author=parsed.get('author'),
+        title=parsed.get('title'),
+        abstract=parsed.get('abstract'),
+        comment=parsed.get('comment'),
+        journal=parsed.get('journal'),
+        report_number=parsed.get('report_number'),
+        category=parsed.get('category'),
+        id_list=parsed.get('id_list'),
+        page=parsed.get('page') or 0,
+        max_results=max_results
+    )
+    doc = papis.cli.pick(
+        [papis.document.Document(data=d) for d in data]
+    )
+    return doc
+
+
+
+@click.command()
+@click.help_option('--help', '-h')
+@papis.cli.query_option()
+
+@click.option(
+    "--isbnplus",
+    help="Search through isbnplus.org",
+    default=False,
+    is_flag=True
+)
+
+@click.option(
+    "--arxiv",
+    help="Search on the arxiv",
+    default=False,
+    is_flag=True
+)
+
+@click.option(
+    "--libgen",
+    help="Search on library genesis",
+    default=False,
+    is_flag=True
+)
+
+@click.option(
+    "--crossref",
+    help="Search on library genesis",
+    default=False,
+    is_flag=True
+)
+
+@click.option(
+    "--add",
+    help="Add document selected",
+    default=False,
+    is_flag=True
+)
+
+@click.option(
+    "--max",
+    help="Maximum number of items",
+    default=30,
+    type=int
+)
+
+@click.option(
+    "--cmd",
+    help="Issue a command on the retrieved document "
+         "using papis format",
+    default=None
+)
+
+
+def cli(
+        query,
+        isbnplus,
+        arxiv,
+        libgen,
+        crossref,
+        add,
+        max,
+        cmd,
+    ):
+    """Explore on the internet"""
+    doc = None
+    if self.args.arxiv:
+        doc = self.arxiv(self.args.search)
+    elif self.args.isbnplus:
+        doc = self.isbnplus(self.args.search)
+    elif self.args.crossref:
+        doc = self.crossref(self.args.search)
+    elif self.args.libgen:
+        doc = self.libgen(self.args.search)
+    else:
+        self.args.arxiv = True
+        doc = self.arxiv(self.args.search)
+
+    if doc:
+        print(papis.document.dump(doc))
+        if self.args.add:
+            self.add(doc)
+        elif self.args.cmd is not None:
+            from subprocess import call
+            command = papis.utils.format_doc(
+                self.args.cmd,
+                doc
+            )
+            self.logger.debug('Calling "%s"' % command)
+            call(command.split(" "))
+
