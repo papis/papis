6f48d0dc7a3f7906ccdb925d2c56bc18c0bca5f5
Alejandro Gallo <aamsgallo@gmail.com>
2018-08-05 22:24:02 +0200

    Quick out tk from the papis main core

--- papis/commands/gui.py
+++ papis/commands/gui.py
@@ -9,12 +9,6 @@ import click
 @click.help_option('--help', '-h')
 @papis.cli.query_option()
 @click.option(
-    "--tk",
-    help="Tk based UI",
-    default=False,
-    is_flag=True
-)
-@click.option(
     "--rofi",
     help="Rofi based UI",
     default=False,
@@ -32,25 +26,20 @@ import click
     default=False,
     is_flag=True
 )
-def cli(query, tk, rofi, urwid, vim):
+def cli(query, rofi, urwid, vim):
     """Graphical/Text user interface"""
     import papis.database
 
     documents = papis.database.get().query(query)
     default_gui = papis.config.get('default-gui')
-    if not tk and not urwid and not vim and not rofi:
-        if default_gui == 'tk':
-            tk = True
-        elif default_gui == 'urwid':
+    if not urwid and not vim and not rofi:
+        if default_gui == 'urwid':
             urwid = True
         elif default_gui == 'vim':
             vim = True
         elif default_gui == 'rofi':
             rofi = True
 
-    if tk:
-        import papis.gui.tk
-        return papis.gui.tk.Gui().main(documents)
     if urwid:
         import papis.gui.urwid
         return papis.gui.urwid.Gui().main()
--- papis/gui/__init__.py
+++ papis/gui/__init__.py
@@ -29,128 +29,6 @@ Vim gui
 .. papis-config:: header-format
     :section: vim-gui
 
-Tk gui
-*******
-.. papis-config:: open
-    :section: tk-gui
-
-.. papis-config:: edit
-    :section: tk-gui
-
-.. papis-config:: exit
-    :section: tk-gui
-
-.. papis-config:: clear
-    :section: tk-gui
-
-.. papis-config:: help
-    :section: tk-gui
-
-.. papis-config:: focus_prompt
-    :section: tk-gui
-
-.. papis-config:: move_down
-    :section: tk-gui
-
-.. papis-config:: move_up
-    :section: tk-gui
-
-.. papis-config:: move_top
-    :section: tk-gui
-
-.. papis-config:: move_bottom
-    :section: tk-gui
-
-.. papis-config:: print_info
-    :section: tk-gui
-
-.. papis-config:: half_down
-    :section: tk-gui
-
-.. papis-config:: half_up
-    :section: tk-gui
-
-.. papis-config:: scroll_down
-    :section: tk-gui
-
-.. papis-config:: scroll_up
-    :section: tk-gui
-
-.. papis-config:: prompt-fg
-    :section: tk-gui
-
-.. papis-config:: prompt-bg
-    :section: tk-gui
-
-    Color of the foreground of an entry
-
-.. papis-config:: entry-fg
-    :section: tk-gui
-
-    Color of the foreground of an active entry
-
-.. papis-config:: activeforeground
-    :section: tk-gui
-
-    Color of the background of an active entry
-
-.. papis-config:: activebackground
-    :section: tk-gui
-
-.. papis-config:: insertbackground
-    :section: tk-gui
-
-.. papis-config:: prompt-font-size
-    :section: tk-gui
-
-.. papis-config:: entry-bg-size
-    :section: tk-gui
-
-.. papis-config:: entry-font-size
-    :section: tk-gui
-
-.. papis-config:: entry-font-name
-    :section: tk-gui
-
-.. papis-config:: entry-font-style
-    :section: tk-gui
-
-.. papis-config:: entry-lines
-    :section: tk-gui
-
-.. papis-config:: entry-bg-odd
-    :section: tk-gui
-
-.. papis-config:: entry-bg-pair
-    :section: tk-gui
-
-.. papis-config:: cursor
-    :section: tk-gui
-
-.. papis-config:: height
-    :section: tk-gui
-
-.. papis-config:: labels-per-page
-    :section: tk-gui
-
-.. papis-config:: borderwidth
-    :section: tk-gui
-
-.. papis-config:: window-width
-    :section: tk-gui
-
-.. papis-config:: window-bg
-    :section: tk-gui
-
-.. papis-config:: window-height
-    :section: tk-gui
-
-.. papis-config:: match-format
-    :section: tk-gui
-
-.. papis-config:: header-format
-    :section: tk-gui
-
 Rofi gui
 ********
 .. papis-config:: key-quit
@@ -215,7 +93,6 @@ Rofi gui
 
 .. papis-config:: header-format
     :section: rofi-gui
-
 
 dmenu gui
 *********
@@ -287,54 +164,6 @@ def get_default_settings():
                 "Author: {doc[author]}\n"
                 "Year  : {doc[year]}\n"
                 "-------\n",
-        },
-        "tk-gui": {
-            "open": "o",
-            "edit": "e",
-            "exit": "<Control-q>",
-            "clear": "q",
-            "help": "h",
-            "focus_prompt": ":",
-            "move_down": "j",
-            "move_up": "k",
-            "move_top": "g",
-            "move_bottom": "<Shift-G>",
-            "print_info": "i",
-            "half_down": "<Control-d>",
-            "half_up": "<Control-u>",
-            "scroll_down": "<Control-e>",
-            "scroll_up": "<Control-y>",
-            "prompt-fg": "lightgreen",
-            "prompt-bg": "black",
-            # Color of the foreground of an entry
-            "entry-fg": "grey77",
-            # Color of the foreground of an active entry
-            "activeforeground": "gray99",
-            # Color of the background of an active entry
-            "activebackground": "#394249",
-            "insertbackground": "red",
-            "prompt-font-size": "14",
-            "entry-bg-size": "14",
-            "entry-font-size": "14",
-            "entry-font-name": "Times",
-            "entry-font-style": "normal",
-            "entry-lines": "3",
-            "entry-bg-odd": "#273238",
-            "entry-bg-pair": "#273238",
-            "cursor": "xterm",
-            "height": 1,
-            "labels-per-page": 6,
-            "borderwidth": -1,
-            "window-width": "1200",
-            "window-bg": "#273238",
-            "window-height": "700",
-            "match-format": \
-                "{doc[tags]}{doc.subfolder}{doc[title]}"
-                "{doc[author]}{doc[year]}",
-            "header-format": \
-                "{doc[title]}\n"\
-                "{doc[empty]}   {doc[author]}\n"\
-                "({doc[year]:->4})",
         },
         "rofi-gui": {
             "key-quit": "Alt+q",
