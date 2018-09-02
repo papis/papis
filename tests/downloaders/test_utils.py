95075b3c82461121969392eb3338acb6ba53bd9d
Alejandro Gallo <aamsgallo@gmail.com>
2018-09-02 11:32:25 +0200

    Add test_get_downloader

--- tests/downloaders/test_utils.py
+++ tests/downloaders/test_utils.py
@@ -1,4 +1,7 @@
-from papis.downloaders.utils import get_available_downloaders
+from papis.downloaders.utils import (
+    get_available_downloaders,
+    get_downloader
+)
 
 
 def test_get_available_downloaders():
@@ -7,3 +10,14 @@ def test_get_available_downloaders():
     for d in downloaders:
         assert(d is not None)
         assert(callable(d.match))
+
+
+def test_get_downloader():
+    down = get_downloader('https://google.com', 'get')
+    assert(down is not None)
+    assert(down.name == 'get')
+    assert(str(down) == 'get')
+
+    down = get_downloader('arXiv:1701.08223v2')
+    assert(down is not None)
+    assert(down.name == 'arxiv')
