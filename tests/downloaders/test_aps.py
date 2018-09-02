718bb29014e28ac26eea5f9722f3bc3abd5e71f1
Alejandro Gallo <aamsgallo@gmail.com>
2018-09-02 13:24:40 +0200

    Update aps test

--- tests/downloaders/test_aps.py
+++ tests/downloaders/test_aps.py
@@ -1,43 +1,49 @@
-import unittest
 import papis.downloaders
 import papis.downloaders.utils
 from papis.downloaders.aps import Downloader
+import papis.bibtex
 
 
-class TestRecognizer(unittest.TestCase):
-
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-    def test_main(self):
-        self.assertTrue(papis.downloaders)
-
-    def test_match(self):
-        assert(
-            Downloader.match(
-                'blah://pubs.aps.org/doi/abs/10.1021/acs.jchemed.6b00559'
-            )
+def test_match():
+    assert(
+        Downloader.match(
+            'blah://pubs.aps.org/doi/abs/10.1021/acs.jchemed.6b00559'
         )
-        assert(
-            Downloader.match(
-                'blah://pubs.aps.org/!@#!@$!%!@%!$che.6b00559'
-            )
+    )
+    assert(
+        Downloader.match(
+            'blah://pubs.aps.org/!@#!@$!%!@%!$che.6b00559'
         )
-        assert(
-            Downloader.match(
-                'aps.com/!@#!@$!%!@%!$chemed.6b00559'
-            ) is False
-        )
+    )
+    assert(
+        Downloader.match(
+            'aps.com/!@#!@$!%!@%!$chemed.6b00559'
+        ) is False
+    )
 
-    def test_downloader_getter(self):
-        self.assertTrue(papis.downloaders.utils.get_downloader)
-        aps = papis.downloaders.utils.get_downloader(
-            "http://journals.aps.org/prb/abstract/10.1103/PhysRevB.95.085434"
-        )
-        self.assertTrue(len(aps.get_bibtex_url()))
-        aps.download_bibtex()
-        self.assertTrue(len(aps.get_bibtex_data()) != 0)
-        print(aps.get_bibtex_data())
+
+def test_downloader_getter():
+    assert(papis.downloaders.utils.get_downloader is not None)
+    aps = papis.downloaders.utils.get_downloader(
+        "http://journals.aps.org/prb/abstract/10.1103/PhysRevB.95.085434"
+    )
+
+    assert(len(aps.get_bibtex_url()) > 0)
+    assert(len(aps.get_bibtex_data()) > 0)
+    bibs = papis.bibtex.bibtex_to_dict(aps.get_bibtex_data())
+    assert(len(bibs) == 1)
+    doc = aps.get_document_data()
+    assert(doc is not None)
+    assert(not aps.check_document_format())
+
+    # this is an open access paper, so it should work
+    aps = papis.downloaders.utils.get(
+     'https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.121.091601'
+    )
+    assert(isinstance(aps, dict))
+    assert(aps.get('doi') is not None)
+    assert(aps.get('doi') == aps.get('data').get('doi'))
+    assert(aps.get('data') is not None)
+    assert(isinstance(aps.get('data'), dict))
+    assert(aps.get('documents_paths') is not None)
+    assert(len(aps.get('documents_paths')) == 1)
