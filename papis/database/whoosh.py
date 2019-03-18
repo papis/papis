a72ad59e5bb1ca80aef966456fc6b14c4753566b
Alejandro Gallo <aamsgallo@gmail.com>
2019-03-18 19:01:40 +0100

    Fix whoosh cache name

--- papis/database/whoosh.py
+++ papis/database/whoosh.py
@@ -293,7 +293,7 @@ class Database(papis.database.base.Database):
         path = os.path.expanduser(
             os.path.join(
                 self.get_cache_dir(),
-                papis.database.cache.get_name(self.lib.path_format())
+                papis.database.cache.get_cache_file_name(self.lib.path_format())
             )
         )
         # self.logger.debug('Index dir %s' % path)
