209cf25236211a108bb8b5fa9b537c3c13dbaf8b
Alejandro Gallo <aamsgallo@gmail.com>
2019-03-20 03:15:59 +0100

    Parse javascript doi before https

--- papis/downloaders/fallback.py
+++ papis/downloaders/fallback.py
@@ -19,11 +19,11 @@ class Downloader(papis.downloaders.base.Downloader):
         doi_regex = re.compile(r'doi:([^"\'<>#?&]+)', re.I)
         # This should be last, since many references are given by this
         https_regex = re.compile(r'doi.org/([^"\'<>#?&]+)', re.I)
-        # Sometimes is in the javascript defined
+        # Sometimes it is in the javascript defined
         var_doi = re.compile(r'var *doi *= *"([^"]+)"', re.I)
         body = self.session.get(self.get_url()).content.decode('utf-8')
         self.logger.info('trying to parse doi...')
-        for regex in [doi_regex, https_regex, var_doi]:
+        for regex in [doi_regex, var_doi, https_regex]:
             miter = regex.finditer(body)
             try:
                 m = next(miter)
