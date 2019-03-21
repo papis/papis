988ba186681961e734f002596998b83d9c780209
Alejandro Gallo <aamsgallo@gmail.com>
2019-03-21 01:22:35 +0100

    Test more database

--- tests/database/test_papis.py
+++ tests/database/test_papis.py
@@ -21,3 +21,23 @@ class Test(tests.database.DatabaseTest):
     def test_cache_path(self):
         database = papis.database.get()
         assert(os.path.exists(database._get_cache_file_path()))
+
+    def test_load_again(self):
+        db = papis.database.get()
+        Ni = len(db.get_documents())
+        db.save()
+        db.documents = None
+        # Now the pickled path exists but no documents
+        Nf = len(db.get_documents())
+        self.assertEqual(Ni, Nf)
+
+    def test_failed_location_in_cache(self):
+        db = papis.database.get()
+        doc = db.get_documents()[0]
+        db.delete(doc)
+        try:
+            db._locate_document(doc)
+        except Exception as e:
+            self.assertTrue(True)
+        else:
+            self.assertTrue(False)
