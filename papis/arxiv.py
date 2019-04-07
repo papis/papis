014c0c0ab8bdcd995a75cba22de4066fcf471ac6
Alejandro Gallo <aamsgallo@gmail.com>
2019-04-07 20:45:14 +0200

    Move arxiv downloader to papis.arxiv

--- papis/arxiv.py
+++ papis/arxiv.py
@@ -22,6 +22,8 @@ import urllib.parse  # import urlencode
 import papis.config
 import re
 import click
+import papis.downloaders.base
+import arxiv2bib
 
 
 logger = logging.getLogger('arxiv')
@@ -174,10 +176,9 @@ def explorer(ctx, query, author, title, abstract, comment,
         papis explore arxiv -a '"John Smith"' pick
 
     """
-    import papis.arxiv
     logger = logging.getLogger('explore:arxiv')
     logger.info('Looking up...')
-    data = papis.arxiv.get_data(
+    data = get_data(
         query=query,
         author=author,
         title=title,
@@ -193,3 +194,50 @@ def explorer(ctx, query, author, title, abstract, comment,
     docs = [papis.document.from_data(data=d) for d in data]
     ctx.obj['documents'] += docs
     logger.info('{} documents found'.format(len(docs)))
+
+
+class Downloader(papis.downloaders.base.Downloader):
+
+    def __init__(self, url):
+        papis.downloaders.base.Downloader.__init__(self, url, name="arxiv")
+        self.expected_document_extension = 'pdf'
+        self.arxivid = None
+
+    @classmethod
+    def match(cls, url):
+        arxivid = find_arxivid_in_text(url)
+        if arxivid:
+            url = "https://arxiv.org/abs/{0}".format(arxivid)
+            down = Downloader(url)
+            down.arxivid = arxivid
+            return down
+        else:
+            return False
+
+    def get_identifier(self):
+        """Get arxiv identifier
+        :returns: Identifier
+        """
+        if not self.arxivid:
+            self.arxivid = find_arxivid_in_text(self.get_url())
+        return self.arxivid
+
+    def get_bibtex_url(self):
+        identifier = self.get_identifier()
+        return identifier
+
+    def download_bibtex(self):
+        bib_url = self.get_bibtex_url()
+        bibtexCli = arxiv2bib.Cli([bib_url])
+        bibtexCli.run()
+        self.logger.debug("[bibtex url] = %s" % bib_url)
+        data = ''.join(bibtexCli.output).replace('\n', ' ')
+        self.bibtex_data = data
+
+    def get_document_url(self):
+        # https://arxiv.org/pdf/1702.01590
+        arxivid = self.get_identifier()
+        self.logger.debug("arxivid %s" % arxivid)
+        pdf_url = "https://arxiv.org/pdf/{arxivid}.pdf".format(arxivid=arxivid)
+        self.logger.debug("[pdf url] = %s" % pdf_url)
+        return pdf_url
