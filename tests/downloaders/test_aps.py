96f0e1955194ab4bc761bba59079dc2fd89b6dd8
Alejandro Gallo <aamsgallo@gmail.com>
2019-04-15 22:13:34 +0200

    Simplify aps

--- tests/downloaders/test_aps.py
+++ tests/downloaders/test_aps.py
@@ -1,43 +1,21 @@
 import papis.downloaders
 from papis.downloaders.aps import Downloader
-import papis.bibtex
+from unittest.mock import patch
+from tests.downloaders import get_resource, get_json_resource
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 
-def test_match():
-    assert(Downloader.match(
-        'blah://pubs.aps.org/doi/abs/10.1021/acs.jchemed.6b00559'
-    ))
-    assert(Downloader.match(
-        'blah://pubs.aps.org/!@#!@$!%!@%!$che.6b00559'
-    ))
-    assert(Downloader.match(
-        'aps.com/!@#!@$!%!@%!$chemed.6b00559'
-    ) is False)
-
-
-def test_downloader_getter():
-    assert(papis.downloaders.get_downloader is not None)
-    aps = papis.downloaders.get_downloader(
-        "http://journals.aps.org/prb/abstract/10.1103/PhysRevB.95.085434"
-    )
-    assert(aps.expected_document_extension == 'pdf')
-    assert(len(aps.get_bibtex_url()) > 0)
-    assert(len(aps.get_bibtex_data()) > 0)
-    bibs = papis.bibtex.bibtex_to_dict(aps.get_bibtex_data())
-    assert(len(bibs) == 1)
-    doc = aps.get_document_data()
-    assert(doc is not None)
-    # This is problematic
-    #assert(not aps.check_document_format())
-
-    # this is an open access paper, so it should work
-    aps = papis.downloaders.get_info_from_url(
-     'https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.121.091601'
-    )
-    assert(isinstance(aps, dict))
-    assert(aps.get('doi') is not None)
-    assert(aps.get('doi') == aps.get('data').get('doi'))
-    assert(aps.get('data') is not None)
-    assert(isinstance(aps.get('data'), dict))
-    assert(aps.get('documents_paths') is not None)
-    assert(len(aps.get('documents_paths')) == 1)
+def test_1():
+    url = 'https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.122.145901'
+    down = papis.downloaders.get_downloader(url)
+    assert(down.name == 'aps')
+    with patch.object(down, '_get_body',
+            lambda: get_resource('prl_1.html')):
+        with patch.object(down, 'download_document', lambda: None):
+            down.fetch()
+            correct_data = get_json_resource('prl_1_out.json')
+            assert(down.ctx.data == correct_data)
+            # with open('prl_1_out.json', 'w+') as f:
+                # import json
+                # json.dump(down.ctx.data, f)
