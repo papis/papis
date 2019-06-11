5772469de1ad1fa19230b15d4860b9feffefa192
Alexandru Fikl <alexfikl@gmail.com>
2019-06-11 15:51:28 -0500

    tandfonline: update html test resources

--- tests/downloaders/test_tandfonline.py
+++ tests/downloaders/test_tandfonline.py
@@ -19,10 +19,14 @@ def test_1():
     url = 'https://www.tandfonline.com/doi/full/10.1080/00268976.2013.788745'
     down = papis.downloaders.get_downloader(url)
     assert(down.name == 'tandfonline')
+
+    # with open('tandfonline_1.html', 'w+') as f:
+    #     f.write(down.session.get(url).content.decode())
+
     with patch.object(down, '_get_body',
             lambda: get_resource('tandfonline_1.html')):
+        down.fetch()
         with patch.object(down, 'download_document', lambda: None):
-            down.fetch()
             # with open('tandfonline_1_out.json', 'w+') as f:
             #     import json
             #     json.dump(down.ctx.data, f,
@@ -34,9 +38,13 @@ def test_1():
 
 
 def test_2():
-    url = 'https://www.tandfonline.com/doi/full/20.2080/00268976.2023.788745'
+    url = 'https://www.tandfonline.com/doi/full/10.1080/23311932.2015.1117749'
     down = papis.downloaders.get_downloader(url)
     assert(down.name == 'tandfonline')
+
+    # with open('tandfonline_2.html', 'w+') as f:
+    #     f.write(down.session.get(url).content.decode())
+
     with patch.object(down, '_get_body',
             lambda: get_resource('tandfonline_2.html')):
         down.fetch()
