6c06f13c309da1cac7cbef38beb0bbef24967810
Alejandro Gallo <aamsgallo@gmail.com>
2019-10-15 21:27:25 +0200

    cli: Add AliasedGroup for aliases in chained commands

--- papis/cli.py
+++ papis/cli.py
@@ -1,5 +1,26 @@
 import click
 import papis.config
+import difflib
+
+
+class AliasedGroup(click.Group):
+    """
+    This group command is taken from
+        http://click.palletsprojects.com/en/5.x/advanced/#command-aliases
+    and is to be used for groups with aliases
+    """
+
+    def get_command(self, ctx, cmd_name):
+        rv = click.Group.get_command(self, ctx, cmd_name)
+        if rv is not None:
+            return rv
+        matches = difflib.get_close_matches(
+            cmd_name, self.list_commands(ctx), n=2)
+        if not matches:
+            return None
+        elif len(matches) == 1:
+            return click.Group.get_command(self, ctx, matches[0])
+        ctx.fail('Too many matches: %s' % ', '.join(sorted(matches)))
 
 
 def query_option(**attrs):
