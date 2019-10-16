b3437254315acb75f61dc0f63548a5833e68a42e
Alejandro Gallo <aamsgallo@gmail.com>
2019-10-16 22:29:02 +0200

    Simplify isbn test

--- tests/test_isbn.py
+++ tests/test_isbn.py
@@ -6,7 +6,3 @@ def test_get_data():
     assert(isinstance(mattuck, list))
     assert(isinstance(mattuck[0], dict))
     assert(mattuck[0]['isbn-13'] == '9780486670478')
-
-    result = get_data(query='Asd sdfiw 123 1Blufsh oiadsj faoisdf ')
-    assert(not result)
-    assert(isinstance(result, list))
