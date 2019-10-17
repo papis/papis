2a0a85ba9c614af008a38bfef5179af308d0544a
Alejandro Gallo <aamsgallo@gmail.com>
2019-10-18 00:02:23 +0200

    Set version for lxml

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setup(
         "pygments>=2.2.0",
         "stevedore>=1.30",
         "python-doi>=0.1.1",
-        "lxml"
+        "lxml<=4.4.0"
     ],
     python_requires='>=3',
     classifiers=[
