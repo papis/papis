97553cf8722b64d989c27f32815b9da81e756ab6
Alejandro Gallo <aamsgallo@gmail.com>
2019-11-09 14:21:19 +0100

    Typed library.py

--- papis/library.py
+++ papis/library.py
@@ -1,25 +1,25 @@
 import os
 import glob
+from typing import List
 
 
 class Library:
 
-    def __init__(self, name, paths):
+    def __init__(self, name: str, paths: List[str]):
         assert(isinstance(name, str)), '`name` must be a string'
         assert(isinstance(paths, list)), '`paths` must be a list'
         self.name = name
         self.paths = sum(
             [glob.glob(os.path.expanduser(p)) for p in paths],
-            []
-        )
+            [])  # type: List[str]
 
-    def path_format(self):
+    def path_format(self) -> str:
         return ":".join(self.paths)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
-def from_paths(paths):
+def from_paths(paths: List[str]) -> Library:
     name = ":".join(paths)
     return Library(name, paths)
