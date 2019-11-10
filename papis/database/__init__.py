5891f1097490a3fa073be42914ed14b8346bcb9c
Alejandro Gallo <aamsgallo@gmail.com>
2019-11-10 02:09:46 +0100

    Typechceck database __init__

--- papis/database/__init__.py
+++ papis/database/__init__.py
@@ -1,39 +1,44 @@
 import logging
+from typing import Optional, Dict
+from .base import Database
+from papis.library import Library
 logger = logging.getLogger('database')
 
-DATABASES = dict()
+DATABASES = dict()  # type: Dict[Library, Database]
 
 
-def get(library=None):
+def get(library_name: Optional[str] = None) -> Database:
     global DATABASES
     import papis.config
-    if library is None:
+    if library_name is None:
         library = papis.config.get_lib()
-    elif isinstance(library, str):
-        library = papis.config.get_lib_from_name(library)
-    backend = papis.config.get('database-backend')
-    database = DATABASES.get(library)
-    # if there is already a database and the backend of the database
-    # is the same as the config backend, then return that library
-    # else we will (re)define the database in the dictionary DATABASES
-    if database is not None and database.get_backend_name() == backend:
-        return DATABASES.get(library)
-    if backend == "papis":
-        import papis.database.cache
-        DATABASES[library] = papis.database.cache.Database(library)
-        return DATABASES.get(library)
-    elif backend == "whoosh":
-        import papis.database.whoosh
-        DATABASES[library] = papis.database.whoosh.Database(library)
-        return DATABASES.get(library)
     else:
-        raise Exception('No valid database type: {}'.format(backend))
+        library = papis.config.get_lib_from_name(library_name)
+    backend = papis.config.get('database-backend') or 'papis'
+    try:
+        database = DATABASES[library]
+    except KeyError:
+        database = _instantiate_database(backend, library)
+        DATABASES[library] = database
+    return database
 
 
-def get_all_query_string():
+def _instantiate_database(backend_name: str, library: Library) -> Database:
+    if backend_name == "papis":
+        import papis.database.cache
+        return papis.database.cache.Database(library)
+    elif backend_name == "whoosh":
+        import papis.database.whoosh
+        return papis.database.whoosh.Database(library)
+    else:
+        raise Exception('No valid database type: {}'.format(backend_name))
+
+
+
+def get_all_query_string() -> str:
     return get().get_all_query_string()
 
 
-def clear_cached():
+def clear_cached() -> None:
     global DATABASES
     DATABASES = dict()
