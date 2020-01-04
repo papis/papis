d8f1731fb33a33e3e4a1e165a1be3ea0b196f501
Alejandro Gallo <aamsgallo@gmail.com>
2020-01-04 03:34:40 +0100

    Typecheck fallback

--- papis/downloaders/fallback.py
+++ papis/downloaders/fallback.py
@@ -1,32 +1,40 @@
 import doi
 import papis.downloaders.base
 
+from typing import Dict, Any, List, Optional
+
 
 class Downloader(papis.downloaders.base.Downloader):
 
-    def __init__(self, url, name="fallback"):
-        papis.downloaders.base.Downloader.__init__(self, url, name=name)
+    def __init__(self, url: str):
+        papis.downloaders.base.Downloader.__init__(self, url, name="fallback")
         self.priority = -1
 
     @classmethod
-    def match(cls, url):
+    def match(cls, url: str) -> Optional[papis.downloaders.base.Downloader]:
         return Downloader(url)
 
-    def get_data(self):
+    def get_data(self) -> Dict[str, Any]:
         data = dict()
         soup = self._get_soup()
         data.update(papis.downloaders.base.parse_meta_headers(soup))
         return data
 
-    def get_doi(self):
-        if 'doi' in self.ctx.data:
-            return self.ctx.data['doi']
+    def get_doi(self) -> Optional[str]:
+        if self.ctx.data and 'doi' in self.ctx.data:
+            _doi = self.ctx.data['doi']
+            return str(_doi) if _doi else None
         soup = self._get_soup()
-        self.logger.info('trying to parse doi...')
-        return doi.find_doi_in_text(str(soup))
+        self.logger.info('trying to parse doi from url body...')
+        if soup:
+            return doi.find_doi_in_text(str(soup))
+        else:
+            return None
 
-    def get_document_url(self):
+    def get_document_url(self) -> Optional[str]:
         if 'pdf_url' in self.ctx.data:
             url = self.ctx.data.get('pdf_url')
             self.logger.debug("got a pdf url = %s" % url)
             return url
+        else:
+            return None
