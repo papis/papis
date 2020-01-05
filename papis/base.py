8544d0534941e34683b59fb1775672bfd55f89e8
Alejandro Gallo <aamsgallo@gmail.com>
2020-01-05 22:18:22 +0100

    Typecheck base

--- papis/base.py
+++ papis/base.py
@@ -9,18 +9,18 @@ import urllib.parse
 import urllib.request  # import urlencode
 import logging
 import json
+from typing import Optional, Dict, Any, List, Union, Callable, Tuple, NamedTuple
+
 import click
 
 logger = logging.getLogger('base')
 
-BASE_BASEURL = (
-    "https://api.base-search.net/"
-    "cgi-bin/BaseHttpSearchInterface.fcgi/"
-)
 
-
-def get_data(query="", max=20):
-    global BASE_BASEURL
+def get_data(query: str = "", max: int = 20) -> List[Dict[str, Any]]:
+    base_baseurl = (
+        "https://api.base-search.net/"
+        "cgi-bin/BaseHttpSearchInterface.fcgi/"
+    )
 
     logger.warning('BASE engine in papis is experimental')
 
@@ -37,7 +37,7 @@ def get_data(query="", max=20):
     params = urllib.parse.urlencode(
         {x: dict_params[x] for x in dict_params if dict_params[x]}
     )
-    req_url = BASE_BASEURL + "search?" + params
+    req_url = base_baseurl + "search?" + params
     logger.debug("url = " + req_url)
     url = urllib.request.Request(
         req_url,
@@ -51,7 +51,7 @@ def get_data(query="", max=20):
     return list(map(basedoc_to_papisdoc, docs))
 
 
-def basedoc_to_papisdoc(basedoc):
+def basedoc_to_papisdoc(basedoc: Dict[str, Any]) -> Dict[str, Any]:
     """Convert a json doc from the base database into a papis document
 
     :basedoc: Json doc from base database
@@ -59,27 +59,34 @@ def basedoc_to_papisdoc(basedoc):
 
     """
     doc = dict()
+    _action_type = Optional[Callable[[List[str]], str]]
+    _key_translate = NamedTuple("_key_translate", [("basekey", str),
+                                                   ("papiskey", str),
+                                                   ("mode", str),
+                                                   ("action", _action_type)
+                                                   ])
     keys_translate = [
-        ("dctitle", "title", "single"),
-        ("dcyear", "year", "single"),
-        ("dclink", "url", "single"),
-        ("dcdescription", "abstract", "single"),
-        ("dcpublisher", "publisher", "multi", lambda x: x[0]),
-        ("dcperson", "author", "multi", lambda x: " and ".join(x)),
-        ("dcsubject", "tags", "multi", lambda x: " ".join(x)),
-        ("dcdoi", "doi", "multi", lambda x: x[0]),
-        ("dctype", "type", "multi", lambda x: x[0].lower()),
-        ("dclang", "lang", "mutli", lambda x: x[0]),
-    ]
+        _key_translate("dctitle", "title", "s", None),
+        _key_translate("dcyear", "year", "s", None),
+        _key_translate("dclink", "url", "s", None),
+        _key_translate("dcdescription", "abstract", "s", None),
+        _key_translate("dcpublisher", "publisher", "m", lambda x: x[0]),
+        _key_translate("dcperson", "author", "m", lambda x: " and ".join(x)),
+        _key_translate("dcsubject", "tags", "m", lambda x: " ".join(x)),
+        _key_translate("dcdoi", "doi", "m", lambda x: x[0]),
+        _key_translate("dctype", "type", "m", lambda x: x[0].lower()),
+        _key_translate("dclang", "lang", "mutli", lambda x: x[0]),
+    ]  # type: List[_key_translate]
     for kt in keys_translate:
-        if kt[0] in basedoc.keys():
-            key = kt[1]
-            if kt[2] == "multi":
-                value = basedoc[kt[0]]
-                value = kt[3](value) if kt[3] is not None else value
-            else:
-                value = basedoc[kt[0]]
-            doc[key] = value
+        if kt.basekey not in basedoc.keys():
+            continue
+        key = kt.papiskey
+        if kt.mode == "m":
+            value = basedoc[kt.basekey]
+            value = kt.action(value) if kt.action is not None else value
+        else:
+            value = basedoc[kt.basekey]
+        doc[key] = value
     return doc
 
 
@@ -87,7 +94,7 @@ def basedoc_to_papisdoc(basedoc):
 @click.pass_context
 @click.help_option('--help', '-h')
 @click.option('--query', '-q', default=None)
-def explorer(ctx, query):
+def explorer(ctx: click.core.Context, query: str) -> None:
     """
     Look for documents on the BielefeldAcademicSearchEngine
 
