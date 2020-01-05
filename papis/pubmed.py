ba1ab4dfe50887f71523f2ad07ea0405e5553120
Alejandro Gallo <aamsgallo@gmail.com>
2020-01-05 20:44:37 +0100

    Typecheck pubmed

--- papis/pubmed.py
+++ papis/pubmed.py
@@ -1,35 +1,34 @@
+from typing import Optional
+
 import papis.importer
 import papis.downloaders.base
 
 
 class Downloader(papis.downloaders.base.Downloader):
 
-    def __init__(self, url):
+    def __init__(self, url: str):
         papis.downloaders.base.Downloader.__init__(
             self, uri=url, name="pubmed")
 
-    def get_bibtex_url(self):
-        return (
-            "http://pubmed.macropus.org/articles/"
-            "?format=text%2Fbibtex&id={pmid}"
-            .format(pmid=self.uri)
-        )
+    def get_bibtex_url(self) -> Optional[str]:
+        return ("http://pubmed.macropus.org/articles/"
+                "?format=text%2Fbibtex&id={pmid}"
+                .format(pmid=self.uri))
 
 
 class Importer(papis.importer.Importer):
 
     """Importer downloading data from a pubmed id"""
 
-    def __init__(self, uri='', **kwargs):
-        papis.importer.Importer.__init__(
-            self, name='pubmed', uri=uri, **kwargs)
+    def __init__(self, uri: str = ''):
+        papis.importer.Importer.__init__(self, name='pubmed', uri=uri)
         self.downloader = Downloader(uri)
 
     @classmethod
-    def match(cls, uri):
+    def match(cls, uri: str) -> Optional[papis.importer.Importer]:
         # TODO:
         pass
 
-    def fetch(self):
+    def fetch(self) -> None:
         self.downloader.fetch()
         self.ctx = self.downloader.ctx
