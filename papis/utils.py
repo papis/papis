b53aaf0e66b25911947b03d891e2bedcdc2c429f
Alejandro Gallo <aamsgallo@gmail.com>
2020-01-05 13:20:36 +0100

    Remove filetype from utils

--- papis/utils.py
+++ papis/utils.py
@@ -428,24 +428,6 @@ def locate_document(
     return None
 
 
-def get_document_extension(document_path: str) -> str:
-    """Get document extension
-
-    :document_path: Path of the document
-    :returns: Extension (string)
-
-    """
-    import filetype
-    filetype.guess(document_path)
-    kind = filetype.guess(document_path)
-    if kind is None:
-        m = re.match(r"^.*\.([^.]+)$", os.path.basename(document_path))
-        return m.group(1) if m else 'data'
-    else:
-        assert isinstance(kind.extension, str)
-        return kind.extension
-
-
 def folders_to_documents(folders: List[str]) -> List[papis.document.Document]:
     """Turn folders into documents, this is done in a multiprocessing way, this
     step is quite critical for performance.
