ff0ed8f300c5ffb8f83ee8d03dc8d2381724064e
Alejandro Gallo <aamsgallo@gmail.com>
2020-06-02 17:31:32 +0200

    Document plugin architecture

--- doc/source/plugins.rst
+++ doc/source/plugins.rst
@@ -1,4 +1,20 @@
 Plugin architecture
 ===================
 
-.. note:: TODO
+.. automodule:: papis.plugin
+
+Exporter
+--------
+TO DOCUMENT
+
+Command
+-------
+TO DOCUMENT
+
+Importer
+--------
+TO DOCUMENT
+
+Explore
+-------
+TO DOCUMENT
--- papis/plugin.py
+++ papis/plugin.py
@@ -1,3 +1,61 @@
+"""
+General architecture
+--------------------
+
+Papis uses the package
+`stevedore <https://github.com/openstack/stevedore/>`_
+for general plugin management.
+
+The only papis module invoking ``stevedore`` should be
+``papis/plugin.py``.
+
+The different plugins in papis like ``papis.command``,
+``papis.exporter`` etc. define a so-called ``ExtensionManager``
+which loads various objects that have been declared in a python
+package somewhere.
+
+For example, the yaml exporter is defined as
+
+.. code:: python
+
+    def exporter(documents: List[papis.document.Document]) -> str:
+        string = yaml.dump_all(
+            [papis.document.to_dict(document) for document in documents],
+            allow_unicode=True)
+        return str(string)
+
+and declared in ``setup.py`` as
+
+.. code:: python
+
+    setup(
+        ...
+        entry_points={
+            'papis.exporter': [
+                'bibtex=papis.bibtex:exporter',
+                'json=papis.json:exporter',
+                'yaml=papis.yaml:exporter',
+            ],
+        ...
+    )
+
+and the exporter can be used as in the code below
+
+.. code:: python
+
+    import papis.plugin
+    extension_manager = papis.plugin.get_extension_manager("papis.exporter")
+    # yaml_exporter is the function defined above
+    yaml_exporter = extension_manager["yaml"].plugin
+
+    yaml_string = yaml_exporter(mydocs)
+
+Now a developer is able to write another exporter in some package
+and install the package in the system.
+The ``extension_manager`` will be able to access the provided functions
+in the package if they have been declared in the entry points of
+the ``setup.py`` script of the named package.
+"""
 import logging
 from stevedore import ExtensionManager
 from typing import List, Dict, Any  # noqa: ignore
