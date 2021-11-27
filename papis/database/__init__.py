94f4d14df6a9395dab313eca1710d8622bfde419
Alexandru Fikl <alexfikl@gmail.com>
2021-11-26 21:03:34 -0600

    use positional logging in database modules

--- papis/database/__init__.py
+++ papis/database/__init__.py
@@ -2,6 +2,7 @@ import logging
 from typing import Optional, Dict  # noqa: ignore
 from .base import Database
 from papis.library import Library
+
 logger = logging.getLogger('database')
 
 DATABASES = dict()  # type: Dict[Library, Database]
