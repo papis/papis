e461a6c8f4eef6705e25f471840ca68cf21a34f0
Sébastien M. Popoff <sebastien.popoff@espci.fr>
2022-02-22 12:51:56 +0100

    add dynamic search with Datatable.js (#361)

--- papis/commands/serve.py
+++ papis/commands/serve.py
@@ -40,6 +40,9 @@ HEADER_TEMPLATE = """
   src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js'
   integrity='sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ'
   crossorigin='anonymous'></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.css">
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.js"></script>
 </head>
 """
 
@@ -112,10 +115,25 @@ INDEX_TEMPLATE = (
                        placeholder="{placeholder}">
             </form>
             </h3>
-            <ol class="list-group">
-                {documents}
-            </ol>
+            <table border="1" class="display" id="pub_table">
+                <thead style='display:none;'>
+                    <tr style="text-align: right;">
+                    <th>info</th>
+                    <th>data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {documents}
+                </tbody>
+            </table>
         </div>
+        <script type="text/javascript">
+            $(document).ready(function(){{
+                $('#pub_table').DataTable( {{
+                    "bSort": false
+                }} )
+            ;}});
+            </script>'
     </body>
 </html>
 """)
@@ -251,8 +269,7 @@ def render_document_item(libname: str,
 
     tag_renderer = functools.partial(render_tag, libname=libname)
 
-    return (("""<li class="list-group-item d-md-flex d-sm-block \
-                           justify-content-md-between align-items-start">
+    return (("""<tr><td>
                   <div class="ms-2 me-auto">
                     <div class="fw-bold">{doc[title]}</div>
                       {doc[author]}<br>
@@ -267,8 +284,8 @@ def render_document_item(libname: str,
                                                    libname,
                                                    libfolder))
             + """
-                </div>
-                <div class="me-2 d-block align-self-end">
+                </td>
+                <td>
               """
             + render_if_doc_has("tags",
                                 """
@@ -333,7 +350,7 @@ def render_document_item(libname: str,
                                 """)
             + """
                 </ul>
-            </div>
+            </td></tr>
             </li>
             """)
 
@@ -342,10 +359,10 @@ def render_index(docs: List[papis.document.Document],
                  libname: str,
                  placeholder: str = "query") -> str:
     libfolder = papis.config.get_lib_from_name(libname).paths[0]
-    documents = ("\n"
-                 .join(map(functools.partial(render_document_item,
-                                             libname,
-                                             libfolder), docs)))
+    documents = ("\n".join([render_document_item(libname, libfolder, d) for d in docs]))
+                #  .join(map(functools.partial(render_document_item,
+                #                              libname,
+                #                              libfolder), docs)))
     return (INDEX_TEMPLATE
             .format(documents=documents,
                     placeholder=placeholder,
