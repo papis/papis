4fc33cae4053afeaa6eca00e206e6c50c2c11847
lennonhill <@>
2022-04-16 00:58:35 +0200

    Use configured default-library instead of default value
    Fix #249

--- papis/config.py
+++ papis/config.py
@@ -595,8 +595,9 @@ def get_lib() -> papis.library.Library:
     if _CURRENT_LIBRARY is None:
         # Do not put papis.config.get because get is a special function
         # that also needs the library to see if some key was overridden!
-        default_settings = get_default_settings()[get_general_settings_name()]
-        lib = default_settings['default-library']
+        config = papis.config.get_configuration()
+        lib = config.get(papis.config.get_general_settings_name(),
+                         'default-library')
         set_lib_from_name(lib)
     assert(isinstance(_CURRENT_LIBRARY, papis.library.Library))
     return _CURRENT_LIBRARY
