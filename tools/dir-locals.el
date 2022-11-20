6fd60cb4596e3954a9d0f7f19b88f4ad6d7139c9
Alejandro Gallo <aamsgallo@gmail.com>
2022-11-20 15:21:16 +0100

    Update mypy and dir-locals

--- tools/dir-locals.el
+++ tools/dir-locals.el
@@ -10,6 +10,11 @@
                                        (format "%s/env/bin/python" root)
                                        flycheck-pycheckers-args
                                        (format "--venv-path=%s/env" root)
-                                       flycheck-python-flake8-executable python-shell-interpreter flycheck-python-mypy-executable python-shell-interpreter flycheck-python-pylint-executable python-shell-interpreter)))
+                                       flycheck-python-flake8-executable
+                                       (format "%s/env/bin/flake8" root)
+                                       flycheck-python-mypy-executable
+                                       (format "%s/env/bin/mypy" root)
+                                       flycheck-python-pylint-executable
+                                       (format "%s/env/bin/pylint" root))))
                  (mode . flycheck)
                  (flycheck-pycheckers-checkers . (mypy3 flake8)))))
