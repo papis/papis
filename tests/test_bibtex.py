66781b53664ea3867cc1bdd109ade8f152284039
Alexandru Fikl <alexfikl@gmail.com>
2022-12-03 10:17:30 +0200

    test_bibtex: small formatting changes

--- tests/test_bibtex.py
+++ tests/test_bibtex.py
@@ -9,29 +9,31 @@ import papis.document
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
+BIBTEX_RESOURCES = os.path.join(os.path.dirname(__file__), "resources", "bibtex")
+
 
 def test_bibtex_to_dict():
-    bibpath = os.path.join(os.path.dirname(__file__),
-                           "resources", "bibtex", "1.bib")
+    bibpath = os.path.join(BIBTEX_RESOURCES, "1.bib")
     bibs = papis.bibtex.bibtex_to_dict(bibpath)
-    keys = ["title",
-            "author",
-            "journal",
-            "abstract",
-            "volume",
-            "issue",
-            "pages",
-            "numpages",
-            "year",
-            "month",
-            "publisher",
-            "doi",
-            "url"]
+    expected_keys = {
+        "title",
+        "author",
+        "journal",
+        "abstract",
+        "volume",
+        "issue",
+        "pages",
+        "numpages",
+        "year",
+        "month",
+        "publisher",
+        "doi",
+        "url",
+        }
 
     assert len(bibs) == 1
     for bib in bibs:
-        for key in keys:
-            assert key in bib
+        assert not (expected_keys - bib.keys())
 
     assert bib["type"] == "article"
     assert re.match(r".*Rev.*", bib["journal"])
@@ -39,22 +41,21 @@ def test_bibtex_to_dict():
 
 
 def test_bibkeys_exist():
+    assert hasattr(papis.bibtex, "bibtex_keys")
     assert len(papis.bibtex.bibtex_keys) != 0
 
 
 def test_bibtypes_exist():
+    assert hasattr(papis.bibtex, "bibtex_types")
     assert len(papis.bibtex.bibtex_types) != 0
 
 
 @pytest.mark.parametrize("bibfile", ["1.bib", "2.bib", "3.bib"])
 def test_author_list_conversion(bibfile, overwrite=False):
     jsonfile = "{}_out.json".format(os.path.splitext(bibfile)[0])
-    bibpath = os.path.join(
-        os.path.dirname(__file__),
-        "resources", "bibtex", bibfile)
-    jsonpath = os.path.join(
-        os.path.dirname(__file__),
-        "resources", "bibtex", jsonfile)
+
+    bibpath = os.path.join(BIBTEX_RESOURCES, bibfile)
+    jsonpath = os.path.join(BIBTEX_RESOURCES, jsonfile)
 
     bib = papis.bibtex.bibtex_to_dict(bibpath)[0]
     if overwrite or not os.path.exists(jsonpath):
@@ -73,10 +74,9 @@ def test_author_list_conversion(bibfile, overwrite=False):
 
 
 def test_clean_ref() -> None:
-    for (r, rc) in [("Einstein über etwas und so 1923",
-                     "EinsteinUberEtwasUndSo1923"),
-                    ("Äöasf () : Aλבert Eιنς€in",
-                     "AoasfAlbertEinseurin"),
-                    ("Albert_Ein\_stein\.1923.b",
-                     "AlbertEin_stein.1923B")]:
+    for (r, rc) in [
+            ("Einstein über etwas und so 1923", "EinsteinUberEtwasUndSo1923"),
+            ("Äöasf () : Aλבert Eιنς€in", "AoasfAlbertEinseurin"),
+            (r"Albert_Ein\_stein\.1923.b", "AlbertEin_stein.1923B"),
+            ]:
         assert rc == papis.bibtex.ref_cleanup(r)
