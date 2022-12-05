85a87ab5cfc780839b72d3090e136e12f75f9d75
Alexandru Fikl <alexfikl@gmail.com>
2022-12-05 16:28:03 +0200

    commands.config: document how to access custom config sections

--- papis/commands/config.py
+++ papis/commands/config.py
@@ -1,34 +1,52 @@
 """
-The command config is a useful command because it allows you to check
+The ``config`` command is a useful command because it allows you to check
 the configuration settings' values that your current `papis` session
 is using.
 
 For example let's say that you want to see which ``dir`` setting your
-current library is using (i.e., the directory or the dir that appears
+default library is using (i.e., the directory or the dir that appears
 in the definition of the library in the configuration file), then you
-would simply do:
+would simply do
 
 .. code::
 
     papis config dir
 
-If you wanted to see which ``dir`` the library ``books`` has, for example
+If you wanted to see which ``dir`` the ``books`` library has, for example,
 then you would do
 
 .. code::
 
     papis -l books config dir
 
-This works as well for default settings, i.e., settings that you have not
-customized, for example the setting ``match-format``, you would check
-it with
+This works equally for any default settings, i.e. settings that have not been
+customized. For example, querying the ``match-format`` setting is done using
 
 .. code::
 
     papis config match-format
     > {doc[tags]}{doc.subfolder}{doc[title]}{doc[author]}{doc[year]}
 
-You can find a list of all available settings in the configuration section.
+Settings from a specific section in the configuration file can also be
+accessed by adding a dot ``"."`` between the section and the setting name. For
+example, if your ``books`` library is configured as a section, you could
+(equivalently) do
+
+.. code::
+
+    papis config books.dir
+
+For a more complex example, the :ref:`Bibtex` command has its own
+configuration settings. These can be accessed through
+
+.. code::
+
+    papis config bibtex.default-read-bibfile
+    > main.bib
+
+You can find a list of all available settings in the configuration section
+at :ref:`general-settings`. Commands and other plugins can define their own
+settings, which are documented separately.
 
 Command-line Interface
 ^^^^^^^^^^^^^^^^^^^^^^
