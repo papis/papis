5b11a44596093b90d881fdd005c362f1814b01bc
Alexandru Fikl <alexfikl@gmail.com>
2022-12-15 20:15:40 +0200

    test_citeseerx: fix and update tests

--- tests/downloaders/test_citeseerx.py
+++ tests/downloaders/test_citeseerx.py
@@ -1,20 +1,52 @@
-from unittest.mock import patch
+import os
+import pytest
+from typing import Callable
 
 import papis.downloaders
-
-from tests.downloaders import get_resource, get_json_resource
+from papis.downloaders.citeseerx import Downloader
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
+CITESEERX_URLS = (
+    "https://citeseerx.ist.psu.edu/doc_view/pid/497490d0d3ab2724e58b03765055f7a134ce89d3",  # noqa: E501
+    "https://citeseerx.ist.psu.edu/doc_view/pid/dd95519adf528fd234316f6d65ec1727d532ad97",  # noqa: E501
+    )
 
-def test_1():
-    url = "https://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.26.2787"
-    down = papis.downloaders.get_matching_downloaders(url)[0]
-    assert down.name == "citeseerx"
-    with patch.object(down,
-                      "_get_body",
-                      lambda: get_resource("citeseerx_1.html")):
-        down.fetch_data()
-        correct_data = get_json_resource("citeseerx_1_out.json")
-        assert down.ctx.data == correct_data
+
+def get_citeseerx_resource(
+        filename: str, url: str,
+        force: bool = False,
+        ) -> Callable[[], bytes]:
+    from tests.downloaders import get_remote_resource
+
+    pid = os.path.basename(url)
+    return get_remote_resource(
+        filename, Downloader.API_URL,
+        params={"paper_id": pid},
+        headers={"token": "undefined", "referer": url})
+
+
+@pytest.mark.parametrize("url", CITESEERX_URLS)
+def test_citeseerx_fetch(monkeypatch, url: str) -> None:
+    cls = papis.downloaders.get_downloader_by_name("citeseerx")
+    assert cls is Downloader
+
+    down = cls.match(url)
+    assert down is not None
+
+    uid = os.path.basename(url).replace("-", "_")
+    infile = "CiteSeerX_{}.json".format(uid)
+    outfile = "CiteSeerX_{}_Out.json".format(uid)
+
+    from tests.downloaders import get_local_resource
+
+    with monkeypatch.context() as m:
+        m.setattr(down, "_get_raw_data", get_citeseerx_resource(infile, url))
+        m.setattr(down, "download_document", lambda: None)
+
+        down.fetch()
+        extracted_data = down.ctx.data
+        expected_data = get_local_resource(outfile, extracted_data)
+
+        assert extracted_data == expected_data
