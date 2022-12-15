cbe66261f4ddeda67dee4e8ba05a03c9fe0558e2
Alexandru Fikl <alexfikl@gmail.com>
2022-12-15 14:25:21 +0200

    move all dependencies from ci-install to setup.py

--- tools/ci-install.sh
+++ tools/ci-install.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-python -m pip install --upgrade pip setuptools
-python -m pip install 'flake8<6' flake8-quotes flake8-bugbear
-python -m pip install \
-    "pep8-naming; python_version>='3.6'" \
-    "pep8-naming<0.13.0; python_version<'3.6'"
-python -m pip install python-coveralls
-python -m pip install types-requests types-PyYAML types-contextvars
+python -m pip install --upgrade pip setuptools wheel
 python -m pip install -e .[develop]
 python -m pip install -e .[optional]
