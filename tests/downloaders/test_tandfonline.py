1fc265dc565d62ffbb865d1207f5fffde8221205
Alexandru Fikl <alexfikl@gmail.com>
2022-12-20 12:07:01 +0200

    test_tandfonline: fix and update tests

--- tests/downloaders/test_tandfonline.py
+++ tests/downloaders/test_tandfonline.py
@@ -1,60 +1,59 @@
-from unittest.mock import patch
+import os
+import pytest
 
 import papis.downloaders
 from papis.downloaders.tandfonline import Downloader
 
-from tests.downloaders import get_resource, get_json_resource
-
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-
-def test_match():
-    assert Downloader.match(
-        "https://www.tandfonline.com/doi/full/10.1080/00268976.2013.788745"
-    ).name == "tandfonline"
-    assert Downloader.match(
-        "https://www.tandfonline.com/doi/full/10.1080/23311932.2015.1117749"
-    ).name == "tandfonline"
+TANDFONLINE_URLS = (
+    "https://www.tandfonline.com/doi/full/10.1080/00268976.2013.788745",
+    "https://www.tandfonline.com/doi/full/10.1080/23311932.2015.1117749"
+    )
 
 
-def test_1():
-    url = "https://www.tandfonline.com/doi/full/10.1080/00268976.2013.788745"
-    down = papis.downloaders.get_downloader(url)
-    assert down.name == "tandfonline"
+def test_tandfonline_match():
+    valid_urls = (
+        "https://tandfonline.com",
+        "http://tandfonline.com",
+        "https://tandfonline.com/bogus/link/10.1007",
+        ) + TANDFONLINE_URLS
+    invalid_urls = (
+        "https://torfonline.com/article/123",
+        "https://tandfonline.co.uk/article/123",
+        )
 
-    # with open("tandfonline_1.html", "w+") as f:
-    #     f.write(down.session.get(url).content.decode())
+    for url in valid_urls:
+        assert isinstance(Downloader.match(url), Downloader)
 
-    with patch.object(down, "_get_body", lambda: get_resource("tandfonline_1.html")):
+    for url in invalid_urls:
+        assert Downloader.match(url) is None
+
+
+@pytest.mark.parametrize("url", TANDFONLINE_URLS)
+def test_tandfonline_fetch(monkeypatch, url: str) -> None:
+    cls = papis.downloaders.get_downloader_by_name("tandfonline")
+    assert cls is Downloader
+
+    down = cls.match(url)
+    assert down is not None
+
+    uid = os.path.basename(url).replace("-", "_")
+    infile = "TFOnline_{}.html".format(uid)
+    outfile = "TFOnline_{}_Out.json".format(uid)
+
+    from tests.downloaders import get_remote_resource, get_local_resource
+
+    with monkeypatch.context() as m:
+        m.setattr(down, "_get_body", get_remote_resource(infile, url))
+        m.setattr(down, "download_document", lambda: None)
+
+        # NOTE: bibtex add some extra fields, so we just disable it for the test
+        m.setattr(down, "download_bibtex", lambda: None)
+
         down.fetch()
-        with patch.object(down, "download_document", lambda: None):
-            # with open("tandfonline_1_out.json", "w+") as f:
-            #     import json
-            #     json.dump(down.ctx.data, f,
-            #             indent=2,
-            #             sort_keys=True,
-            #             ensure_ascii=False)
-            correct_data = get_json_resource("tandfonline_1_out.json")
-            assert down.ctx.data == correct_data
+        extracted_data = down.ctx.data
+        expected_data = get_local_resource(outfile, extracted_data)
 
-
-def test_2():
-    url = "https://www.tandfonline.com/doi/full/10.1080/23311932.2015.1117749"
-    down = papis.downloaders.get_downloader(url)
-    assert down.name == "tandfonline"
-
-    # with open("tandfonline_2.html", "w+") as f:
-    #     f.write(down.session.get(url).content.decode())
-
-    with patch.object(down, "_get_body", lambda: get_resource("tandfonline_2.html")):
-        down.fetch()
-        with patch.object(down, "download_document", lambda: None):
-            # with open("tandfonline_2_out.json", "w+") as f:
-            #     import json
-            #     json.dump(down.ctx.data, f,
-            #             indent=2,
-            #             sort_keys=True,
-            #             ensure_ascii=False)
-            correct_data = get_json_resource("tandfonline_2_out.json")
-            assert down.ctx.data == correct_data
+        assert extracted_data == expected_data
