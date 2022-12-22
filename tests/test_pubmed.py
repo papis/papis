5081dea51ccf9cfdec603f7d3efd86927698c812
Alexandru Fikl <alexfikl@gmail.com>
2022-12-22 14:18:02 +0200

    xfail isbn and pubmed tests that sometimes timeout

--- tests/test_pubmed.py
+++ tests/test_pubmed.py
@@ -1,6 +1,9 @@
+import pytest
+
 import papis.pubmed
 
 
+@pytest.mark.xfail(reason="remote pmid validity check can timeout")
 def test_match():
     assert papis.pubmed.Importer.match("28012456")
     assert papis.pubmed.Importer.match("5503630")
