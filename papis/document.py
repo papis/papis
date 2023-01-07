e962e63c4c931a1fb82ae06d742166deadfb8208
Alexandru Fikl <alexfikl@gmail.com>
2023-01-07 11:24:28 +0200

    format: use codecs.decode to allow newlines

--- papis/document.py
+++ papis/document.py
@@ -351,13 +351,16 @@ def move(document: Document, path: str) -> None:
         document.set_folder(path)
 
 
-def from_data(data: Dict[str, Any]) -> Document:
-    """Construct a document object from a data dictionary.
+def from_data(data: Union[Document, Dict[str, Any]]) -> Document:
+    """Construct a document object from a dictionary.
 
-    :param data: Data to be copied to a new document
-    :returns: A papis document
+    :param data: A dictionary to be copied to a new document. If this is already
+        a document, a (deep) copy is performed.
     """
-    return Document(data=data)
+    if isinstance(data, Document):
+        return Document(folder=data.get_main_folder(), data=data)
+    else:
+        return Document(data=data)
 
 
 def sort(docs: List[Document], key: str, reverse: bool) -> List[Document]:
