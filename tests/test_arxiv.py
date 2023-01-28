cea3801deff79eb45421e40f56513b4f114c5706
Alexandru Fikl <alexfikl@gmail.com>
2023-01-28 09:18:31 +0200

    test_arxiv: update tests

--- tests/test_arxiv.py
+++ tests/test_arxiv.py
@@ -1,13 +1,30 @@
 import pytest
-import papis.arxiv
 import papis.downloaders
 
 from tests.testlib import TemporaryConfiguration
 
+ARXIV_TEST_URLS = [
+    ("/URI(https://arxiv.org/abs/1305.2291v2)>>", "1305.2291v2"),
+    ("/URI(https://arxiv.org/abs/1205.0093)>>", "1205.0093"),
+    ("/URI(https://arxiv.org/abs/1205.1494)>>", "1205.1494"),
+    ("/URI(https://arxiv.org/abs/1011.2840)>>", "1011.2840"),
+    ("/URI(https://arxiv.org/abs/1110.3658)>>", "1110.3658"),
+    ("https://arxiv.org/abs/1110.3658>", "1110.3658"),
+    ("http://arxiv.org/abs/1110.3658>", "1110.3658"),
+    ("https://arxiv.com/abs/1110.3658>", "1110.3658"),
+    ("https://arxiv.org/1110.3658>", "1110.3658"),
+    ("arXiv:1701.08223v2?234", "1701.08223v2"),
+    ("https://arxiv.org/pdf/1110.3658.pdf", "1110.3658"),
+    ("http://arxiv.org/pdf/1110.3658.pdf", "1110.3658"),
+    ("https://arxiv.com/pdf/1110.3658.pdf", "1110.3658"),
+    ("http://arxiv.com/pdf/1110.3658.pdf", "1110.3658"),
+]
+
 
 @pytest.mark.xfail(reason="arxiv times out sometimes")
 def test_get_data(tmp_config: TemporaryConfiguration) -> None:
-    data = papis.arxiv.get_data(
+    from papis.arxiv import get_data
+    data = get_data(
         author="Garnet Chan",
         max_results=1,
         title="Finite Temperature"
@@ -18,45 +35,36 @@ def test_get_data(tmp_config: TemporaryConfiguration) -> None:
 
 
 def test_find_arxiv_id(tmp_config: TemporaryConfiguration) -> None:
-    test_data = [
-        ("/URI(https://arxiv.org/abs/1305.2291v2)>>", "1305.2291v2"),
-        ("/URI(https://arxiv.org/abs/1205.0093)>>", "1205.0093"),
-        ("/URI(https://arxiv.org/abs/1205.1494)>>", "1205.1494"),
-        ("/URI(https://arxiv.org/abs/1011.2840)>>", "1011.2840"),
-        ("/URI(https://arxiv.org/abs/1110.3658)>>", "1110.3658"),
-        ("https://arxiv.org/abs/1110.3658>", "1110.3658"),
-        ("http://arxiv.org/abs/1110.3658>", "1110.3658"),
-        ("https://arxiv.com/abs/1110.3658>", "1110.3658"),
-        ("https://arxiv.org/1110.3658>", "1110.3658"),
-        ("https://arxiv.org/pdf/1110.3658.pdf", "1110.3658"),
-        ("http://arxiv.org/pdf/1110.3658.pdf", "1110.3658"),
-        ("https://arxiv.com/pdf/1110.3658.pdf", "1110.3658"),
-        ("http://arxiv.com/pdf/1110.3658.pdf", "1110.3658"),
-    ]
+    from papis.arxiv import find_arxivid_in_text
 
-    for url, arxivid in test_data:
-        assert papis.arxiv.find_arxivid_in_text(url) == arxivid, \
-            f"Could not retrieve correct arxivid from {url}"
+    for url, arxivid in ARXIV_TEST_URLS:
+        assert find_arxivid_in_text(url) == arxivid, (
+            f"Could not retrieve correct arxivid from {url}")
 
 
-def test_match(tmp_config: TemporaryConfiguration) -> None:
-    down = papis.arxiv.Downloader.match("arxiv.org/sdf")
+def test_downloader_match(tmp_config: TemporaryConfiguration) -> None:
+    from papis.arxiv import Downloader, ARXIV_ABS_URL
+
+    down = Downloader.match("arxiv.org/sdf")
     assert isinstance(down, papis.arxiv.Downloader)
 
-    down = papis.arxiv.Downloader.match("arxiv.com/!@#!@$!%!@%!$chemed.6b00559")
+    down = Downloader.match("arxiv.com/!@#!@$!%!@%!$chemed.6b00559")
     assert down is None
 
-    down = papis.arxiv.Downloader.match("arXiv:1701.08223v2?234")
-    assert isinstance(down, papis.arxiv.Downloader)
-    assert down.uri == "https://arxiv.org/abs/1701.08223v2"
-    assert down.arxivid == "1701.08223v2"
+    for uri, arxivid in ARXIV_TEST_URLS[-2:]:
+        down = Downloader.match(uri)
+        assert down
+        assert down.arxivid == arxivid
+        assert down.uri == "{}/{}".format(ARXIV_ABS_URL, arxivid)
 
 
 @pytest.mark.xfail(reason="arxiv times out sometimes")
-def test_downloader_getter(tmp_config: TemporaryConfiguration) -> None:
-    import papis.bibtex
-
-    url = "https://arxiv.org/abs/1001.3032"
+@pytest.mark.parametrize("url", [
+    "https://arxiv.org/abs/1001.3032",
+    "https://arxiv.org/abs/1001.3032vunknown",
+    ])
+def test_importer_downloader_fetch(tmp_config: TemporaryConfiguration,
+                                   url: str) -> None:
     downs = papis.downloaders.get_matching_downloaders(url)
     assert len(downs) >= 1
 
@@ -64,16 +72,13 @@ def test_downloader_getter(tmp_config: TemporaryConfiguration) -> None:
     assert down.name == "arxiv"
     assert down.expected_document_extensions == ("pdf",)
 
-    bibtex = down.get_bibtex_data()
-    assert bibtex is not None
-    assert len(bibtex) > 0
-
-    bibs = papis.bibtex.bibtex_to_dict(bibtex)
-    assert len(bibs) == 1
-
     doc = down.get_document_data()
-    assert doc is not None
-    assert down.check_document_format()
+    if down.result:
+        assert doc is not None
+        assert down.check_document_format()
+    else:
+        assert down.arxivid is not None
+        assert doc is None
 
 
 def test_validate_arxivid(tmp_config: TemporaryConfiguration) -> None:
@@ -82,7 +87,7 @@ def test_validate_arxivid(tmp_config: TemporaryConfiguration) -> None:
     papis.arxiv.validate_arxivid("1206.6272v1")
     papis.arxiv.validate_arxivid("1206.6272v2")
 
-    import pytest
+    # bad
     for bad in ["1206.6272v3", "blahv2"]:
         with pytest.raises(ValueError, match="not an arxivid"):
             papis.arxiv.validate_arxivid(bad)
