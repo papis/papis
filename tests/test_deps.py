c735815f5227256747e1bea646b8677dd1343848
Alexandru Fikl <alexfikl@gmail.com>
2023-03-05 10:28:00 +0200

    test_deps: add type annotations

--- tests/test_deps.py
+++ tests/test_deps.py
@@ -1,11 +1,11 @@
-def test_pygments():
+def test_pygments() -> None:
     # This function exists after version 2.2.0 only
     from pygments.lexers import find_lexer_class_by_name
     yaml = find_lexer_class_by_name("yaml")
     assert yaml is not None
 
 
-def test_colorama():
+def test_colorama() -> None:
     import colorama
     assert colorama.Back
     assert colorama.Style
@@ -15,7 +15,7 @@ def test_colorama():
     assert colorama.init
 
 
-def test_prompt_toolkit():
+def test_prompt_toolkit() -> None:
     from prompt_toolkit.formatted_text.html import HTML, html_escape    # noqa: F401
     from prompt_toolkit.application import Application                  # noqa: F401
 
@@ -23,7 +23,7 @@ def test_prompt_toolkit():
     from prompt_toolkit.buffer import Buffer                            # noqa: F401
     from prompt_toolkit.enums import EditingMode                        # noqa: F401
     from prompt_toolkit.key_binding import KeyBindings                  # noqa: F401
-    from prompt_toolkit.layout.screen import Point                      # noqa: F401
+    from prompt_toolkit.data_structures import Point                    # noqa: F401
     from prompt_toolkit.layout.containers import HSplit, Window         # noqa: F401
     from prompt_toolkit.layout.controls import (                        # noqa: F401
         BufferControl,
