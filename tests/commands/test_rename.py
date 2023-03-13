e2e7b98ea8f2c12c1b1d41ab38081d0c49763bc2
Alexandru Fikl <alexfikl@gmail.com>
2023-03-13 17:23:32 +0200

    test_rename: port to new pytest fixtures

--- tests/commands/test_rename.py
+++ tests/commands/test_rename.py
@@ -1,25 +1,22 @@
 import os
 
 import papis.database
-from papis.commands.rename import run
 
-import tests
-import tests.cli
+from tests.testlib import TemporaryLibrary
 
 
-class Test(tests.cli.TestWithLibrary):
+def test_rename_run(tmp_library: TemporaryLibrary) -> None:
+    from papis.commands.rename import run
 
-    def get_docs(self):
-        db = papis.database.get()
-        return db.get_all_documents()
+    db = papis.database.get()
+    docs = db.get_all_documents()
+    doc = docs[0]
 
-    def test_simple_update(self) -> None:
-        docs = self.get_docs()
-        document = docs[0]
-        title = document["title"]
-        new_name = "Some title with spaces too"
-        run(document, new_name)
-        docs = papis.database.get().query_dict(dict(title=title))
-        self.assertEqual(len(docs), 1)
-        self.assertEqual(docs[0].get_main_folder_name(), new_name)
-        self.assertTrue(os.path.exists(docs[0].get_main_folder()))
+    old_title = doc["title"]
+    new_name = "Some title with spaces too"
+
+    run(doc, new_name)
+
+    doc, = db.query_dict({"title": old_title})
+    assert doc.get_main_folder_name() == new_name
+    assert os.path.exists(doc.get_main_folder())
