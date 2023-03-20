b4414512425de8b69ce33162ca60bf941386fa28
Alexandru Fikl <alexfikl@gmail.com>
2023-03-20 16:26:03 +0200

    tests: use https instead of http

--- tests/test_arxiv.py
+++ tests/test_arxiv.py
@@ -17,15 +17,15 @@ def test_general():
 
 def test_find_arxiv_id():
     test_data = [
-        ("/URI(http://arxiv.org/abs/1305.2291v2)>>", "1305.2291v2"),
-        ("/URI(http://arxiv.org/abs/1205.0093)>>", "1205.0093"),
-        ("/URI(http://arxiv.org/abs/1205.1494)>>", "1205.1494"),
-        ("/URI(http://arxiv.org/abs/1011.2840)>>", "1011.2840"),
-        ("/URI(http://arxiv.org/abs/1110.3658)>>", "1110.3658"),
-        ("http://arxiv.org/abs/1110.3658>", "1110.3658"),
+        ("/URI(https://arxiv.org/abs/1305.2291v2)>>", "1305.2291v2"),
+        ("/URI(https://arxiv.org/abs/1205.0093)>>", "1205.0093"),
+        ("/URI(https://arxiv.org/abs/1205.1494)>>", "1205.1494"),
+        ("/URI(https://arxiv.org/abs/1011.2840)>>", "1011.2840"),
+        ("/URI(https://arxiv.org/abs/1110.3658)>>", "1110.3658"),
         ("https://arxiv.org/abs/1110.3658>", "1110.3658"),
-        ("http://arxiv.com/abs/1110.3658>", "1110.3658"),
-        ("http://arxiv.org/1110.3658>", "1110.3658"),
+        ("http://arxiv.org/abs/1110.3658>", "1110.3658"),
+        ("https://arxiv.com/abs/1110.3658>", "1110.3658"),
+        ("https://arxiv.org/1110.3658>", "1110.3658"),
     ]
     for url, arxivid in test_data:
         assert find_arxivid_in_text(url) == arxivid
