441bf249d6ad87c0dec1632d9c51b1c4469e7422
Alexandru Fikl <alexfikl@gmail.com>
2023-06-12 20:45:34 +0300

    isbn: set a default language to English

--- papis/isbn.py
+++ papis/isbn.py
@@ -48,11 +48,14 @@ def data_to_papis(data: Dict[str, Any]) -> Dict[str, Any]:
         _k("authors", [{
             "key": "author_list",
             "action": papis.document.split_authors_name
-            }]),
+        }]),
         _k("isbn-13", [
             {"key": "isbn", "action": None},
             {"key": "isbn-13", "action": None},
-            ]),
+        ]),
+        _k("language", [
+            {"key": "language", "action": lambda x: x if x else "en"}
+        ])
         ]
 
     data = {k.lower(): data[k] for k in data}
