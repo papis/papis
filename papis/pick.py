d4e378b392e6539d672655a5716c086ab5c3de71
Alexandru Fikl <alexfikl@gmail.com>
2023-06-30 11:13:14 +0300

    docs: add papis.pick to the docs

--- papis/pick.py
+++ papis/pick.py
@@ -1,7 +1,6 @@
 import os
-import functools
 from abc import ABC, abstractmethod
-from typing import Callable, TypeVar, Generic, Sequence, Type
+from typing import Any, Callable, TypeVar, Generic, Sequence, Type, List
 
 import papis.config
 import papis.document
@@ -10,11 +9,18 @@ import papis.logging
 
 logger = papis.logging.get_logger(__name__)
 
+#: Name of the entry points for :class:`Picker` plugins.
+PICKER_EXTENSION_NAME = "papis.picker"
+
+#: Invariant :class:`~typing.TypeVar` with no bounds.
 T = TypeVar("T")
-Option = TypeVar("Option")
 
 
 class Picker(ABC, Generic[T]):
+    """An interface used to select items from a list.
+
+    .. automethod:: __call__
+    """
 
     @abstractmethod
     def __call__(
@@ -23,40 +29,58 @@ class Picker(ABC, Generic[T]):
             header_filter: Callable[[T], str],
             match_filter: Callable[[T], str],
             default_index: int = 0
-            ) -> Sequence[T]:
-        pass
+            ) -> List[T]:
+        """
+        :arg items: a sequence of items from which to pick a subset.
+        :arg header_filter: (optional) a callable that takes an item from *items*
+            and returns a string representation shown to the user.
+        :arg match_filter: (optional) a callable that takes an item from *items*
+            and returns a string representation that is used when searching or
+            filtering the items.
+        :arg default_index: (optional) sets the selected item when the picker
+            is first shown to the user.
+
+        :returns: a subset of *items* that were picked.
+        """
 
 
-def _extension_name() -> str:
-    return "papis.picker"
+def get_picker(name: str) -> Type[Picker[Any]]:
+    """Get a picker by its plugin name.
 
-
-def get_picker(name: str) -> Type[Picker[Option]]:
-    """Get the picker named 'name' declared as a plugin"""
-    picker: Type[Picker[Option]] = (
-        papis.plugin.get_extension_manager(_extension_name())[name].plugin
+    :arg name: the name of an entrypoint to load a :class:`Picker` plugin from.
+    :returns: a :class:`Picker` subclass implemented in the plugin.
+    """
+    picker: Type[Picker[Any]] = (
+        papis.plugin.get_extension_manager(PICKER_EXTENSION_NAME)[name].plugin
     )
 
     return picker
 
 
-def pick(
-        options: Sequence[Option],
-        default_index: int = 0,
-        header_filter: Callable[[Option], str] = str,
-        match_filter: Callable[[Option], str] = str) -> Sequence[Option]:
+def pick(items: Sequence[T],
+         header_filter: Callable[[T], str] = str,
+         match_filter: Callable[[T], str] = str,
+         default_index: int = 0) -> List[T]:
+    """Load a :class:`Picker` plugin and select a subset of *items*.
+
+    The arguments to this function match those of :meth:`Picker.__call__`. The
+    specific picker is chosen through the :ref:`config-settings-picktool`
+    configuration option.
+
+    :returns: a subset of *items* that were picked.
+    """
 
     name = papis.config.getstring("picktool")
     try:
-        picker: Type[Picker[Option]] = get_picker(name)
+        picker: Type[Picker[T]] = get_picker(name)
     except KeyError:
-        entrypoints = papis.plugin.get_available_entrypoints(_extension_name())
+        entrypoints = papis.plugin.get_available_entrypoints(PICKER_EXTENSION_NAME)
         logger.error(
             "Invalid picker: '%s'. Registered pickers are '%s'.",
             name, "', '".join(entrypoints))
         return []
     else:
-        return picker()(options,
+        return picker()(items,
                         header_filter,
                         match_filter,
                         default_index)
@@ -64,45 +88,53 @@ def pick(
 
 def pick_doc(
         documents: Sequence[papis.document.Document]
-        ) -> Sequence[papis.document.Document]:
-    """Pick a document from documents with the correct formatting
+        ) -> List[papis.document.Document]:
+    """Pick from a sequence of *documents* using :func:`pick`.
 
-    :documents: List of documents
-    :returns: Document
+    This function uses the :ref:`config-settings-header-format-file` setting
+    or, if not available, the :ref:`config-settings-header-format` setting
+    to construct a *header_filter* for the picker. It also uses the configuration
+    setting :ref:`config-settings-match-format` to construct a *match_filter*.
 
+    :arg documents: a sequence of documents.
+    :returns: a subset of *documents* that was picked.
     """
+
     header_format_path = papis.config.get("header-format-file")
     if header_format_path is not None:
-        with open(os.path.expanduser(header_format_path)) as _fd:
-            header_format = _fd.read().rstrip()
+        with open(os.path.expanduser(header_format_path)) as fd:
+            header_format = fd.read().rstrip()
     else:
         header_format = papis.config.getstring("header-format")
     match_format = papis.config.getstring("match-format")
-    header_filter = functools.partial(papis.format.format, header_format)
-    match_filter = functools.partial(papis.format.format, match_format)
+
+    from functools import partial
+
+    header_filter = partial(papis.format.format, header_format)
+    match_filter = partial(papis.format.format, match_format)
+
     return pick(documents,
                 header_filter=header_filter,
                 match_filter=match_filter)
 
 
-def pick_subfolder_from_lib(lib: str) -> Sequence[str]:
-    """Pick a subfolder from all existings subfolders in library
+def pick_subfolder_from_lib(lib: str) -> List[str]:
+    """Pick subfolders from all existings subfolders in *lib*.
 
-    Args:
-        lib (str): Library to search for subfolders
+    Note that this includes document folders in *lib* as well nested library
+    folders.
 
-    Returns:
-        Sequence[str]: Paths to subfolder
+    :arg lib: the name of an existing library to search in.
+    :returns: a subset of the subfolders in the library.
     """
     import papis.api
-
     documents = papis.api.get_all_documents_in_lib(lib)
 
-    # find all folders containing documents
+    # get all document directories
     folders = [os.path.dirname(str(d.get_main_folder())) for d in documents]
+    # get all library directories
     folders.append(*papis.config.get_lib_dirs())
-
     # remove duplicates and sort paths
-    folders = sorted([*set(folders)])
+    folders = sorted(set(folders))
 
     return pick(folders)
