46387387f7abc7cc4ec7969536a158302f37a384
Alexandru Fikl <alexfikl@gmail.com>
2023-07-01 11:39:17 +0300

    docs: add papis.format to the docs

--- papis/format.py
+++ papis/format.py
@@ -1,22 +1,20 @@
-from typing import Any, Dict, Optional, Union
+from typing import Any, Dict, Optional
 
 import papis.config
 import papis.plugin
 import papis.document
 import papis.logging
-from papis.document import Document
 
 logger = papis.logging.get_logger(__name__)
 
-FormatDocType = Union[Document, Dict[str, Any]]
-_FORMATER: Optional["Formater"] = None
+FORMATER: Optional["Formater"] = None
+
+#: The entry point name for formater plugins.
+FORMATER_EXTENSION_NAME = "papis.format"
 
 
 class InvalidFormaterError(ValueError):
-    pass
-
-
-InvalidFormatterValue = InvalidFormaterError
+    """An exception that is thrown when an invalid formater is selected."""
 
 
 def unescape(fmt: str) -> str:
@@ -24,19 +22,21 @@ def unescape(fmt: str) -> str:
 
 
 class Formater:
+    """A generic formatter that works on templated strings using a document."""
+
     def __init__(self) -> None:
         self.default_doc_name = papis.config.getstring("format-doc-name")
 
     def format(self,
                fmt: str,
-               doc: FormatDocType,
+               doc: papis.document.DocumentLike,
                doc_key: str = "",
                additional: Optional[Dict[str, Any]] = None) -> str:
         """
         :param fmt: a format string understood by the formater.
         :param doc: an object convertible to a document.
         :param doc_key: the name of the document in the format string. By
-            default, this falls back to ``"format-doc-name"``.
+            default, this falls back to :ref:`config-settings-format-doc-name`.
         :param additional: a :class:`dict` of additional entries to pass to the
             formater.
 
@@ -47,15 +47,15 @@ class Formater:
 
 class PythonFormater(Formater):
     """Construct a string using a `PEP 3101 <https://peps.python.org/pep-3101/>`__
-    (`str.format` based) format string.
+    (*str.format* based) format string.
 
-    This formater is named ``"python"`` and can be set using the ``formater``
-    setting in the configuration file (see :ref:`general-settings`).
+    This formater is named ``"python"`` and can be set using the
+    :ref:`config-settings-formater` setting in the configuration file.
     """
 
     def format(self,
                fmt: str,
-               doc: FormatDocType,
+               doc: papis.document.DocumentLike,
                doc_key: str = "",
                additional: Optional[Dict[str, Any]] = None) -> str:
         if additional is None:
@@ -76,8 +76,8 @@ class Jinja2Formater(Formater):
     """Construct a string using `Jinja2 <https://palletsprojects.com/p/jinja/>`__
     templates.
 
-    This formater is named ``"jinja2"`` and can be set using the ``formater``
-    setting in the configuration file (see :ref:`general-settings`).
+    This formater is named ``"jinja2"`` and can be set using the
+    :ref:`config-settings-formater` setting in the configuration file.
     """
 
     def __init__(self) -> None:
@@ -94,7 +94,7 @@ class Jinja2Formater(Formater):
 
     def format(self,
                fmt: str,
-               doc: FormatDocType,
+               doc: papis.document.DocumentLike,
                doc_key: str = "",
                additional: Optional[Dict[str, Any]] = None) -> str:
         if additional is None:
@@ -113,42 +113,45 @@ class Jinja2Formater(Formater):
             return "{}: {}".format(type(exc).__name__, exc)
 
 
-def _extension_name() -> str:
-    return "papis.format"
-
-
 def get_formater(name: Optional[str] = None) -> Formater:
-    """Initialize a formater plugin.
+    """Initialize and return a formater plugin.
 
-    Note that the formater is cached and all subsequence calls to this function
+    Note that the formater is cached and all subsequent calls to this function
     will return the same formater.
 
-    :param name: the name of the desired formater.
+    :param name: the name of the desired formater, by default this uses
+        the value of :ref:`config-settings-formater`.
     """
-    global _FORMATER
+    global FORMATER
 
-    if _FORMATER is None:
-        mgr = papis.plugin.get_extension_manager(_extension_name())
+    if FORMATER is None:
+        mgr = papis.plugin.get_extension_manager(FORMATER_EXTENSION_NAME)
 
         if name is None:
             name = papis.config.getstring("formater")
 
         try:
-            _FORMATER = mgr[name].plugin()
+            FORMATER = mgr[name].plugin()
         except Exception as exc:
-            entrypoints = papis.plugin.get_available_entrypoints(_extension_name())
+            entrypoints = (
+                papis.plugin.get_available_entrypoints(FORMATER_EXTENSION_NAME))
             logger.error("Invalid formater '%s'. Registered formaters are '%s'.",
                          name, "', '".join(entrypoints), exc_info=exc)
             raise InvalidFormaterError("Invalid formater: '{}'".format(name))
 
         logger.debug("Using '%s' formater.", name)
 
-    return _FORMATER
+    return FORMATER
 
 
 def format(fmt: str,
-           doc: FormatDocType,
+           doc: papis.document.DocumentLike,
            doc_key: str = "",
            additional: Optional[Dict[str, Any]] = None) -> str:
+    """Format a string using the selected formater.
+
+    This is the user-facing function that should be called when formating a
+    string. The formaters should not be called directly.
+    """
     formater = get_formater()
     return formater.format(fmt, doc, doc_key=doc_key, additional=additional)
