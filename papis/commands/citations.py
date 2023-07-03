316fc99b9a7b586459f95e56b91a3a57f4fac1bc
Alexandru Fikl <alexfikl@gmail.com>
2023-07-03 16:36:03 +0300

    docs: small cleanups in commands.citations

--- papis/commands/citations.py
+++ papis/commands/citations.py
@@ -1,8 +1,50 @@
 """
-See ../../doc/source/commands/citations.rst
+The ``citations`` command updates and creates the ``citations.yaml`` and
+``cited.yaml`` files for every document.
 
-papis citations --fetch-citations
+Examples
+^^^^^^^^
+
+- Create the ``citations.yaml`` file for a document that you pick
+
+    .. code:: sh
+
+        papis citations --fetch-citations
+
+- Create the ``citations.yaml`` file for all documents matching an author
+
+    .. code:: sh
+
+        papis citations --all --fetch-citations 'author:einstein'
+
+- Overwrite the ``citations.yaml`` file with the ``--force`` flag for all
+  papers matching a query
+
+    .. code:: sh
+
+        papis citations --force --fetch-citations 'author:einstein'
+
+- Update the ``citations.yaml`` file with citations of documents existing in
+  your library
+
+    .. code:: sh
+
+        papis citations --all --update-from-database 'author:einstein'
+
+- Create the ``cited-by.yaml`` for all documents in your library (this might
+  take a while)
+
+    .. code:: sh
+
+        papis citations --fetch-cited-by --all
+
+Command-line Interface
+^^^^^^^^^^^^^^^^^^^^^^
+
+.. click:: papis.commands.citations:cli
+    :prog: papis citations
 """
+
 from typing import Optional
 
 import click
