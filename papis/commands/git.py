969e4f468a4d0190d835231f24b5ab1cbcfcedb9
Alexandru Fikl <alexfikl@gmail.com>
2023-07-03 19:07:30 +0300

    docs: small cleanups in commands.git

--- papis/commands/git.py
+++ papis/commands/git.py
@@ -1,25 +1,30 @@
 """
-This command is useful if your library is itself a git repository.
-You can use this command to issue git commands in your library
+This command is useful if your library is itself a `git <https://git-scm.com/>`__
+repository. You can use this command to issue ``git`` commands in your library
 repository without having to change your current directory.
 
-CLI Examples
-^^^^^^^^^^^^
+Examples
+^^^^^^^^
 
-    - Check the status of the library repository:
+- Check the status of the library repository:
 
-    .. code::
+.. code::
 
-        papis git status
+    papis git status
 
-    - Commit all changes:
+- Commit all changes:
 
-    .. code::
+.. code::
 
-        papis git commit -a
+    papis git commit -a
 
+Command-line Interface
+^^^^^^^^^^^^^^^^^^^^^^
 
+.. click:: papis.commands.git:cli
+    :prog: papis git
 """
+
 import copy
 
 import click
