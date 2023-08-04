1abe084bcfbd33988779cc5e005b23af4ad42ae7
Alejandro Gallo <aamsgallo@gmail.com>
2023-08-04 18:50:58 +0200

    Add first argument as a path for the library

--- papis/commands/init.py
+++ papis/commands/init.py
@@ -14,19 +14,14 @@ Go to the directory where you want to have your papers
 
 and answer the questions.
 """
-
-from typing import List, Optional
+from typing import Optional  # noqa: F401
 import os
 
 import click
 
-import papis.pick
-import papis.cli
 import papis.utils
 import papis.config
-import papis.document
-import papis.database
-import papis.tui
+import papis.tui.utils
 import papis.logging
 
 logger = papis.logging.get_logger(__name__)
@@ -36,7 +31,15 @@ INIT_PROMPTS = ["opentool", "editor", "use-git", "notes-name"]
 
 @click.command("init")
 @click.help_option("--help", "-h")
-def cli() -> None:
+@click.argument(
+    "dir_path",
+    required=False,
+    type=click.Path(),
+    default=None,
+    metavar="<LIBRARY DIRECTORY>",
+    nargs=1,
+)
+def cli(dir_path: Optional[str]) -> None:
     """Initialize a papis library"""
 
     config = papis.config.get_configuration()
@@ -48,20 +51,22 @@ def cli() -> None:
             "Config file %s already exists,"
             " this command may change some of its contents,", config_file)
         if not papis.tui.utils.confirm("Do you want to continue?"):
-            return 1
+            return
 
     logger.info("Config file %s", config_file)
 
-    library_name = papis.tui.utils.prompt("Name of the library: ",
-                                          default="papers")
+    library_name = papis.tui.utils.prompt(
+        "Name of the library: ",
+        default="papers" if dir_path is None else os.path.basename(dir_path))
     if library_name not in config:
         config[library_name] = {}
     local = config[library_name]
     glob = config[papis.config.get_general_settings_name()]
 
-    library_path = papis.tui.utils.prompt("Path of the library: ",
-                                          default=local.get(
-                                              "dir", os.getcwd()))
+    library_path = papis.tui.utils.prompt(
+        "Path of the library: ",
+        default=local.get("dir",
+                          os.getcwd() if dir_path is None else dir_path))
 
     if papis.tui.utils.confirm(
             "Make '{}' the default library?".format(library_name)):
@@ -78,5 +83,5 @@ def cli() -> None:
                     [setting])))
 
     if papis.tui.utils.confirm("Do you want to save?"):
-        with open(config_file, 'w') as configfile:
+        with open(config_file, "w") as configfile:
             config.write(configfile)
