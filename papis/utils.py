400d3355039dda24965735b6326b21616b10e250
Twix <twixgithub@protonmail.com>
2023-08-15 13:44:59 +0200

    merge_data str = ""

--- papis/utils.py
+++ papis/utils.py
@@ -572,7 +572,7 @@ def collect_importer_data(
         importers: Iterable[papis.importer.Importer],
         batch: bool = True,
         only_data: bool = True,
-        merge_data: Union[str, None],
+        merge_data: str = "",
         ) -> papis.importer.Context:
     """Collect all data from the given *importers*.
 
