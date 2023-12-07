8d693d6145c11e234866fcbd33f8f0383c9929d5
Alexandru Fikl <alexfikl@gmail.com>
2023-12-07 22:14:01 +0200

    commands.list: consistent naming and option handling

--- papis/commands/list.py
+++ papis/commands/list.py
@@ -4,7 +4,7 @@ This command is used to list items in a library.
 Examples
 ^^^^^^^^
 
-- List all document files associated will all documents in the library:
+- List all files associated will all documents in the library:
 
     .. code:: sh
 
@@ -59,68 +59,44 @@ Command-line Interface
 """
 
 import os
-from typing import List, Optional, Union, Sequence, Tuple
+from typing import List, Optional, Sequence, Tuple
 
 import click
 
-import papis
 import papis.id
-import papis.utils
-import papis.strings
-import papis.config
-import papis.database
-import papis.document
-import papis.downloaders
 import papis.cli
-import papis.pick
+import papis.config
+import papis.strings
+import papis.document
 import papis.format
 import papis.logging
 
 logger = papis.logging.get_logger(__name__)
 
 
-def run(documents: List[papis.document.Document],
-        libraries: bool = False,
-        downloaders: bool = False,
-        pick: bool = False,
-        files: bool = False,
-        folders: bool = False,
-        papis_id: bool = False,
-        info_files: bool = False,
-        notes: bool = False,
-        fmt: str = "",
+def run(documents: Sequence[papis.document.Document],
+        show_files: bool = False,
+        show_dir: bool = False,
+        show_id: bool = False,
+        show_info: bool = False,
+        show_notes: bool = False,
+        show_format: str = "",
         template: Optional[str] = None
-        ) -> Sequence[Union[str, papis.document.Document]]:
-    """Main method to the list command
+        ) -> List[str]:
+    """List document properties.
 
-    :returns: List different objects
+    :arg template: a path to a file containing a format string that can be
+        used instead of *show_format*.
+    :return: a list of properties depending on the given flags.
     """
-    if downloaders:
-        return [str(d) for d in papis.downloaders.get_available_downloaders()]
 
-    if template is not None:
-        if not os.path.exists(template):
-            logger.error("Template file '%s' not found.", template)
-            return []
-        with open(template) as fd:
-            fmt = fd.read()
+    if show_files:
+        return [f for doc in documents for f in doc.get_files()]
 
-    if libraries:
-        config = papis.config.get_configuration()
-        return [
-            section + " " + config[section]["dir"]
-            for section in config
-            if "dir" in config[section]]
-
-    if files:
-        return [
-            doc_file for files in [
-                document.get_files() for document in documents
-            ] for doc_file in files
-        ]
-    elif papis_id:
+    if show_id:
         return [papis.id.get(d) for d in documents]
-    elif notes:
+
+    if show_notes:
         return [
             os.path.join(d.get_main_folder() or "", d["notes"])
             for d in documents
@@ -128,97 +104,119 @@ def run(documents: List[papis.document.Document],
             and "notes" in d and isinstance(d["notes"], str)
             and os.path.exists(
                 os.path.join(d.get_main_folder() or "", d["notes"]))]
-    elif info_files:
+
+    if show_info:
         return [d.get_info_file() for d in documents]
-    elif fmt:
+
+    if show_format or template is not None:
+        if not show_format and template is not None:
+            if not os.path.exists(template):
+                logger.error("Template file '%s' not found.", template)
+                return []
+
+            with open(template) as fd:
+                show_format = fd.read()
+
         return [
-            papis.format.format(fmt, document,
+            papis.format.format(show_format, document,
                                 default=papis.document.describe(document))
             for document in documents
         ]
-    elif folders:
-        return [
-            str(d.get_main_folder()) for d in documents
-            if d.get_main_folder() is not None
-        ]
-    else:
-        return documents
+
+    if show_dir:
+        return [f for d in documents if (f := d.get_main_folder()) is not None]
+
+    raise ValueError("At least one of the flags should be True")
 
 
 @click.command("list")
 @click.help_option("--help", "-h")
 @papis.cli.query_argument()
-@papis.cli.sort_option()
 @papis.cli.bool_flag(
-    "-i", "--info",
-    help="Show the info file name associated with the document")
+    "-i", "--info", "show_info",
+    help="Show the info file for each document")
 @papis.cli.bool_flag(
-    "--id", "_papis_id",
-    help="Show the papis_id")
+    "--id", "show_id",
+    help="Show the papis id for each document")
 @papis.cli.bool_flag(
-    "-f", "--file", "_file",
-    help="Show the file name associated with the document")
+    "-f", "--file", "show_files",
+    help="Show the files for each document")
 @papis.cli.bool_flag(
-    "-d", "--dir", "_dir",
-    help="Show the folder name associated with the document")
+    "-d", "--dir", "show_dir",
+    help="Show the folder name containing each document")
 @papis.cli.bool_flag(
-    "-n", "--notes",
-    help="List notes files, if any")
+    "-n", "--notes", "show_notes",
+    help="Show notes files for each document")
 @click.option(
-    "--format", "_format",
-    help="List entries using a custom papis format, e.g."
-    " '{doc[year] {doc[title]}",
+    "--format", "show_format",
+    help="Show documents using a custom format, e.g. '{doc[year]} {doc[title]}",
     default="")
-@click.option(
-    "--template",
-    help="Template file containing a papis format to list entries",
-    default=None)
 @papis.cli.bool_flag(
-    "--downloaders",
+    "--downloaders", "show_downloaders",
     help="List available downloaders")
 @papis.cli.bool_flag(
-    "--libraries",
+    "--libraries", "show_libraries",
     help="List defined libraries")
+@click.option(
+    "--template",
+    help="Template file containing a papis format to list documents",
+    default=None)
 @papis.cli.all_option()
+@papis.cli.sort_option()
 @papis.cli.doc_folder_option()
 def cli(query: str,
-        info: bool,
-        _papis_id: bool,
-        _file: bool,
-        notes: bool,
-        _dir: bool,
-        _format: str,
+        show_info: bool,
+        show_id: bool,
+        show_files: bool,
+        show_notes: bool,
+        show_dir: bool,
+        show_format: str,
+        show_downloaders: bool,
+        show_libraries: bool,
+        template: Optional[str],
+        _all: bool,
         doc_folder: Tuple[str, ...],
-        template: Optional[str], _all: bool, downloaders: bool,
-        libraries: bool,
-        sort_field: Optional[str], sort_reverse: bool) -> None:
-    """List documents' properties"""
+        sort_field: Optional[str],
+        sort_reverse: bool) -> None:
+    """List document metadata"""
     documents: List[papis.document.Document] = []
 
-    if (not libraries and not downloaders
-            and not _file and not info and not _dir):
-        _dir = True
+    show_doc = (
+        show_info or show_id or show_files or show_notes or show_dir or show_format
+    )
+    show_entrypoints = show_downloaders
 
-    if not libraries and not downloaders:
-        documents = papis.cli.handle_doc_folder_query_all_sort(query,
-                                                               doc_folder,
-                                                               sort_field,
-                                                               sort_reverse,
-                                                               _all)
+    if not show_libraries and not show_entrypoints and not show_doc:
+        show_dir = show_doc = True
+
+    if not show_libraries and not show_downloaders and show_doc:
+        documents = papis.cli.handle_doc_folder_query_all_sort(
+            query, doc_folder, sort_field, sort_reverse, _all)
+
         if not documents:
             logger.warning(papis.strings.no_documents_retrieved_message)
             return
 
+    if show_libraries:
+        for lib in papis.config.get_libs():
+            click.echo(f"{lib} {papis.config.get('dir', section=lib)}")
+        return
+
+    if show_downloaders:
+        from papis.downloaders import get_available_downloaders
+        for d in get_available_downloaders():
+            down = d("https://www.example.com")
+            click.echo(f"{down.name} {d.__module__}")
+        return
+
     objects = run(
         documents,
-        libraries=libraries,
-        downloaders=downloaders,
-        notes=notes,
-        files=_file,
-        folders=_dir,
-        papis_id=_papis_id,
-        info_files=info,
-        fmt=_format,
+        show_notes=show_notes,
+        show_files=show_files,
+        show_dir=show_dir,
+        show_id=show_id,
+        show_info=show_info,
+        show_format=show_format,
         template=template)
 
     for o in objects:
