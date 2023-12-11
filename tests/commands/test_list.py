28bbb6f6ade6e24680ba9cca2e5cd1edcd764116
Alexandru Fikl <alexfikl@gmail.com>
2023-12-11 10:59:49 +0200

    test: fix test_list

--- tests/commands/test_list.py
+++ tests/commands/test_list.py
@@ -6,26 +6,26 @@ from papis.testing import TemporaryLibrary
 
 
 def test_list_run(tmp_library: TemporaryLibrary) -> None:
-    from papis.commands.list import run
+    from papis.commands.list import list_plugins, list_documents
 
     assert papis.config.get_lib_name() == tmp_library.libname
 
     db = papis.database.get()
     docs = db.get_all_documents()
 
-    objs = run(docs, info_files=True)
+    objs = list_documents(docs, show_info=True)
     assert len(objs) == len(docs)
     assert all(os.path.exists(f) for f in objs)
 
-    objs = run(docs, notes=True)
+    objs = list_documents(docs, show_notes=True)
     assert len(objs) == 0
 
-    libs = run([], libraries=True)
+    libs = list_plugins(show_libraries=True)
     assert len(libs) == 1
 
-    folders = run(docs, folders=True)
+    folders = list_documents(docs, show_dir=True)
     assert len(folders) == len(docs)
     assert all(os.path.exists(f) for f in folders)
 
-    ids = run(docs, papis_id=True)
+    ids = list_documents(docs, show_id=True)
     assert len(ids) == len(docs)
