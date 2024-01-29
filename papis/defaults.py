bdab8e265c1aa4fd42b5c12fb4f04af7aee239ce
Alexandru Fikl <alexfikl@gmail.com>
2024-01-29 15:08:26 +0200

    defaults: reorder to match docs

--- papis/defaults.py
+++ papis/defaults.py
@@ -16,55 +16,111 @@ def get_default_opener() -> str:
         # NOTE: should work on Linux / FreeBSD / cygwin
         return "xdg-open"
 
+# NOTE: Try to keep these in the same order as in the `default-settings.rst`
+# document in the docs, so they can be easily checked and updated
+
 
 settings: Dict[str, Any] = {
+    # unused or deprecated
+    "add-interactive": False,
+    "mvtool": "mv",
+    "formater": None,
+
+    # general settings
     "local-config-file": ".papis.config",
-    "database-backend": "papis",
-    "default-query-string": ".",
+    "dir-umask": 0o755,
+    "use-git": False,
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3)",
+    "scripts-short-help-regex": ".*papis-short-help: *(.*)",
+    "info-name": "info.yaml",
+    "doc-url-key-name": "doc_url",
+    "default-library": "papers",
+    "format-doc-name": "doc",
+    "match-format": "{doc[tags]}{doc.subfolder}{doc[title]}{doc[author]}{doc[year]}",
+    "header-format": (
+        "<ansired>{doc.html_escape[title]}</ansired>\n"
+        " <ansigreen>{doc.html_escape[author]}</ansigreen>\n"
+        "  <ansiblue>({doc.html_escape[year]})</ansiblue> "
+        "[<ansiyellow>{doc.html_escape[tags]}</ansiyellow>]"
+    ),
+    "header-format-file": None,
+    "info-allow-unicode": True,
+    "unique-document-keys": "['doi','ref','isbn','isbn10','url','doc_url']",
+    "document-description-format": "{doc[title]} - {doc[author]}",
     "sort-field": None,
     "sort-reverse": False,
+    "formatter": "python",
 
+    # tools
     "opentool": get_default_opener(),
-    "dir-umask": 0o755,
     "browser": os.environ.get("BROWSER") or get_default_opener(),
     "picktool": "papis",
-    "mvtool": "mv",
-    "editor": os.environ.get("EDITOR")
-                        or os.environ.get("VISUAL")
-                        or get_default_opener(),
-    "notes-name": "notes.tex",
-    "notes-template": "",
-    "use-cache": True,
-    "cache-dir": None,
-    "use-git": False,
+    "editor": (
+        os.environ.get("EDITOR")
+        or os.environ.get("VISUAL")
+        or get_default_opener()),
+    "file-browser": get_default_opener(),
 
-    "auto-doctor": False,
+    # bibtex
+    "bibtex-journal-key": "journal",
+    "extra-bibtex-keys": "[]",
+    "bibtex-ignore-keys": "[]",
+    "extra-bibtex-types": "[]",
+    "bibtex-unicode": False,
+    "bibtex-export-file": False,
+    "multiple-authors-separator": " and ",
+    "multiple-authors-format": "{au[family]}, {au[given]}",
 
-    "add-confirm": False,
+    # add
+    "ref-format": "{doc[title]:.15} {doc[author]:.6} {doc[year]}",
     "add-folder-name": "",
-    "add-subfolder": "",
     "add-file-name": None,
-    "add-interactive": False,
+    "add-subfolder": "",
+    "add-confirm": False,
     "add-edit": False,
     "add-open": False,
-    "add-fetch-citations": False,
     "add-download-files": True,
+    "add-fetch-citations": False,
+    "auto-doctor": False,
+    "time-stamp": True,
 
-    # papis-doctor configuration
+    # browse
+    "browse-key": "auto",
+    "browse-query-format": "{doc[title]} {doc[author]}",
+    "search-engine": "https://duckduckgo.com",
+
+    # edit
+    "notes-name": "notes.tex",
+    "notes-template": "",
+
+    # doctor
     "doctor-default-checks": ["files", "keys-exist", "duplicated-keys"],
     "doctor-keys-exist-keys": ["title", "author", "author_list", "ref"],
     "doctor-duplicated-keys-keys": ["ref"],
     "doctor-duplicated-values-keys": ["files", "author_list"],
     "doctor-html-codes-keys": ["title", "author", "abstract", "journal"],
     "doctor-html-tags-keys": ["title", "author", "abstract", "journal"],
-    "doctor-key-type-check-separator": None,
     "doctor-key-type-check-keys": ["year:int",
                                    "month:int",
                                    "files:list",
                                    "notes:str",
                                    "author_list:list"],
+    "doctor-key-type-check-separator": None,
 
-    # papis-serve configuration
+    # open
+    "open-mark": False,
+    "mark-key-name": "marks",
+    "mark-format-name": "mark",
+    "mark-header-format": "{mark[name]} - {mark[value]}",
+    "mark-match-format": "{mark[name]} - {mark[value]}",
+    "mark-opener-format": get_default_opener(),
+
+    # serve
+    "serve-empty-query-get-all-documents": False,
+    "serve-default-tag-sorting": "numeric",
+    "serve-timeline-max": 500,
+    "serve-enable-timeline": False,
+    # serve-frameworks
     "serve-user-css": [],
     "serve-user-js": [],
     "serve-font-awesome-css": [("https://cdnjs.cloudflare.com/ajax/"
@@ -92,7 +148,6 @@ settings: Dict[str, Any] = {
     "serve-katex-auto-render-js":
     ("https://cdn.jsdelivr.net/npm/"
      "katex@0.16.4/dist/contrib/auto-render.min.js"),
-
     "serve-ace-urls": [("https://cdnjs.cloudflare.com/ajax/libs/ace/"
                         "1.14.0/ace.min.js"),
                        ("https://cdnjs.cloudflare.com/ajax/libs/ace/"
@@ -114,67 +169,24 @@ settings: Dict[str, Any] = {
                        ("https://cdnjs.cloudflare.com/ajax/libs/ace/"
                         "1.14.0/keybinding-emacs.min.js"),
                        ],
-
-    "serve-empty-query-get-all-documents": False,
-    "serve-default-tag-sorting": "numeric",
-
-    # citations configuration
-    "citations-file-name": "citations.yaml",
-    "cited-by-file-name": "cited-by.yaml",
-
-    # timeline options
-    "serve-timeline-max": 500,
-    "serve-enable-timeline": False,
     "serve-timeline-js": ("https://cdn.knightlab.com/libs/timeline3/"
                           "latest/js/timeline.js"),
     "serve-timeline-css": ("https://cdn.knightlab.com/libs/timeline3/"
                            "latest/css/timeline.css"),
 
-    "browse-key": "auto",
-    "browse-query-format": "{doc[title]} {doc[author]}",
-    "search-engine": "https://duckduckgo.com",
-    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3)",
-    "scripts-short-help-regex": ".*papis-short-help: *(.*)",
-    "info-name": "info.yaml",
-    "doc-url-key-name": "doc_url",
+    # citations
+    "citations-file-name": "citations.yaml",
+    "cited-by-file-name": "cited-by.yaml",
 
-    "open-mark": False,
-    "mark-key-name": "marks",
-    "mark-format-name": "mark",
-    "mark-header-format": "{mark[name]} - {mark[value]}",
-    "mark-match-format": "{mark[name]} - {mark[value]}",
-    "mark-opener-format": get_default_opener(),
+    # downloaders
+    "downloader-proxy": None,
+    "isbn-service": "openl",
 
-    "file-browser": get_default_opener(),
-    "bibtex-unicode": False,
-    "bibtex-journal-key": "journal",
-    "bibtex-export-file": False,
-    "bibtex-ignore-keys": "[]",
-    "extra-bibtex-keys": "[]",
-    "extra-bibtex-types": "[]",
-    "default-library": "papers",
-    "format-doc-name": "doc",
-    "match-format":
-        "{doc[tags]}{doc.subfolder}{doc[title]}{doc[author]}{doc[year]}",
-    "header-format-file": None,
-    "header-format": (
-        "<ansired>{doc.html_escape[title]}</ansired>\n"
-        " <ansigreen>{doc.html_escape[author]}</ansigreen>\n"
-        "  <ansiblue>({doc.html_escape[year]})</ansiblue> "
-        "[<ansiyellow>{doc.html_escape[tags]}</ansiyellow>]"
-    ),
-
-    "formatter": "python",
-    # FIXME: this is deprecated and should be removed at some point
-    "formater": None,
-    "time-stamp": True,
-    "info-allow-unicode": True,
-    "ref-format": "{doc[title]:.15} {doc[author]:.6} {doc[year]}",
-    "multiple-authors-separator": " and ",
-    "multiple-authors-format": "{au[family]}, {au[given]}",
-    "document-description-format": "{doc[title]} - {doc[author]}",
-    "unique-document-keys": "['doi','ref','isbn','isbn10','url','doc_url']",
-
+    # database
+    "default-query-string": ".",
+    "database-backend": "papis",
+    "use-cache": True,
+    "cache-dir": None,
     "whoosh-schema-fields": "['doi']",
     "whoosh-schema-prototype":
     "{\n"
@@ -199,8 +211,4 @@ settings: Dict[str, Any] = {
                           "«{doc[year]:4}»"
                           "{c.Style.RESET_ALL}"
                           ":{doc[tags]}"),
-
-    # importer / downloader options
-    "downloader-proxy": None,
-    "isbn-service": "openl",
 }
