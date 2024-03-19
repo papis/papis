53063c26638b38733fa4092fcb666e33825a6ae4
Enric Morales <me@enric.me>
2024-03-19 21:20:00 +0100

    fix(thesesfr): update to support the new API (#792)

--- papis/downloaders/thesesfr.py
+++ papis/downloaders/thesesfr.py
@@ -5,73 +5,37 @@ import papis.downloaders.base
 
 
 class Downloader(papis.downloaders.Downloader):
-    """Retrieve documents from `theses.fr <https://theses.fr/en/>`__"""
+    """Retrieve documents from `theses.fr <https://theses.fr/en/>`__
+
+    API Documentation: https://api.gouv.fr/documentation/scanR
+    """
 
     def __init__(self, url: str) -> None:
         super().__init__(url, name="thesesfr", expected_document_extension="pdf")
 
     @classmethod
     def match(cls, url: str) -> Optional[papis.downloaders.Downloader]:
-        if re.match(r".*theses.fr.*", url):
+        # ID format ("nnt" in french). Not specified in the docs, but it's
+        # the pattern that all the published theses until now follow.
+        # https://documentation.abes.fr/aidetheses/thesesfr/index.html
+        if re.match(r"(?:https?://(:?www\.)?theses\.fr/)?(\d{4}[A-Z]{3,5}\d{3,5})",
+                    url):
             return Downloader(url)
         else:
             return None
 
     def get_identifier(self) -> Optional[str]:
-        """
-        >>> d = Downloader("https://www.theses.fr/2014TOU30305")
-        >>> d.get_identifier()
-        '2014TOU30305'
-        >>> d = Downloader("https://www.theses.fr/2014TOU30305.bib/?asdf=2")
-        >>> d.get_identifier()
-        '2014TOU30305'
-        """
-        m = re.match(r".*theses.fr/([^/?.&]+).*", self.uri)
-        return m.group(1) if m is not None else None
+        if match := re.search(r"(\d{4}[A-Z]{3,5}\d{3,5})", self.uri):
+            return match.group(1)
+        else:
+            return None
 
     def get_document_url(self) -> Optional[str]:
-        """
-        >>> d = Downloader("https://www.theses.fr/2014TOU30305")
-        >>> d.get_document_url()
-        'https://thesesups.ups-tlse.fr/2722/1/2014TOU30305.pdf'
-        >>> d = Downloader("https://theses.fr/1998ENPC9815")
-        >>> d.get_document_url()
-        'https://pastel.archives-ouvertes.fr/tel-00005590v2/file/Cances.pdf'
-        """
-        import bs4
-
-        # TODO: Simplify this function for typing
-        raw_data = self.session.get(self.uri).content.decode("utf-8")
-        soup = bs4.BeautifulSoup(raw_data, "html.parser")
-        a = list(filter(
-            lambda t: re.match(r".*en ligne.*", t.text),
-            soup.find_all("a")
-        ))
-
-        if not a:
-            self.logger.error("No document found for '%s'.", self.uri)
-            return None
-
-        second_url = a[0]["href"]
-        raw_data = self.session.get(second_url).content.decode("utf-8")
-        soup = bs4.BeautifulSoup(raw_data, "html.parser")
-        a = list(filter(
-            lambda t: re.match(r".*pdf$", t.get("href", "")),
-            soup.find_all("a")
-        ))
-
-        if not a:
-            self.logger.error("No document found for '%s'.", second_url)
-            return None
-
-        return str(a[0]["href"])
+        baseurl = "https://theses.fr/api/v1/document"
+        identifier = self.get_identifier()
+        return f"{baseurl}/{identifier}"
 
     def get_bibtex_url(self) -> Optional[str]:
-        """
-        >>> d = Downloader("https://www.theses.fr/2014TOU30305")
-        >>> d.get_bibtex_url()
-        'https://www.theses.fr/2014TOU30305.bib'
-        """
         url = f"https://www.theses.fr/{self.get_identifier()}.bib"
         self.logger.debug("Using BibTeX URL: '%s'.", url)
         return url
---
+++ tests/downloaders/test_thesesfr.py
@@ -0,0 +1,63 @@
+import pytest
+
+from papis.testing import TemporaryConfiguration
+from papis.downloaders.thesesfr import Downloader
+
+
+def test_match(tmp_config: TemporaryConfiguration):
+    valid_urls = [
+        "https://www.theses.fr/2014TOU30305",
+        "2014TOU30305",
+        "https://www.theses.fr/2014TOU30305.bib/?asdf=2",
+    ]
+    invalid_urls = ["http://google.com", "2014TOU", "ASDF"]
+    for url in valid_urls:
+        assert isinstance(Downloader.match(url), Downloader)
+
+    for url in invalid_urls:
+        assert Downloader.match(url) is None
+
+
+@pytest.mark.parametrize(
+    ("query", "expected"),
+    [
+        ("https://www.theses.fr/2014TOU30305", "2014TOU30305"),
+        ("https://www.theses.fr/2014TOU30305.bib/?asdf=2", "2014TOU30305"),
+        ("2014TOU30305", "2014TOU30305"),
+        ("2014TOU", None),
+    ],
+)
+def test_get_identifier(query: str, expected: str, tmp_config: TemporaryConfiguration):
+    d = Downloader(query)
+    actual = d.get_identifier()
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    ("query", "expected"),
+    [
+        (
+            "https://theses.fr/2019REIMS014",
+            "https://theses.fr/api/v1/document/2019REIMS014",
+        )
+    ],
+)
+def test_get_document_url(query: str, expected: str):
+    d = Downloader(query)
+    actual = d.get_document_url()
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    ("query", "expected"),
+    [
+        (
+            "https://www.theses.fr/2014TOU30305",
+            "https://www.theses.fr/2014TOU30305.bib",
+        ),
+    ],
+)
+def test_get_bibtex_url(query: str, expected: str, tmp_config: TemporaryConfiguration):
+    d = Downloader(query)
+    actual = d.get_bibtex_url()
+    assert actual == expected
