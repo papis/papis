af14961bd60069e5c889ec514b529b9a62538049
Alexandru Fikl <alexfikl@gmail.com>
2024-04-02 20:41:55 +0300

    scripts: add argparse to populate_wxs_artifacts

--- scripts/windows/populate_wxs_artifacts.py
+++ scripts/windows/populate_wxs_artifacts.py
@@ -7,57 +7,77 @@ import uuid
 
 
 def get_id(*path_components: str) -> str:
-    """Create an element ID based on the WiX constraints: 72 chars or less, a-zA-Z_-, starting with underscore or letter.
-    Here we use the hash (int) of the path, removing the negative symbol, and prefixed by underscore
-    For details, search "Identifier" in
-    https://github.com/wixtoolset/wix/blob/2b6fa3ab686188cfe983abbc3d51d1542069b2c8/src/api/wix/WixToolset.Data/ErrorMessages.cs#L966
-    """
-    path = os.path.join(*path_components)
-    result = f"_{abs(hash(path))}"
-    return result
+    """Create an element ID based on the WiX constraints.
+
+    The allowed characters as of version 4.0.5 can be found
+    `here <https://github.com/wixtoolset/wix/blob/289c93dc24ba203903d9c8a6261a68de95d3d911/src/wix/WixToolset.BuildTasks/ToolsCommon.cs#L18>`__.
+    Largely, the identifier should be 72 chars or less, contain ``[a-zA-Z0-9_-]``,
+    and start with underscore or letter.
+
+    This function uses the hash of the joined *path_components* as the identifier.
+    This is guaranteed to be less than 72 characters and only uses digits.
+    """  # noqa: E501
+
+    result = abs(hash("/".join(path_components)))
+    return f"_{result}"
 
 
-wxs_tree = et.parse(".\\scripts\\windows\\papis_template.wxs")
-wxs_root = wxs_tree.getroot()
+def render_template(template: str, outfile: str) -> None:
+    wxs_tree = et.parse(template)
+    wxs_root = wxs_tree.getroot()
 
-xpath_ns = {"main": "http://wixtoolset.org/schemas/v4/wxs"}
-component = wxs_root.xpath(
-    ".//main:Directory[@Id='INSTALLFOLDER']", namespaces=xpath_ns
-)[0]
-package = wxs_root.xpath(".//main:Package", namespaces=xpath_ns)[0]
+    xpath_ns = {"main": "http://wixtoolset.org/schemas/v4/wxs"}
+    component = wxs_root.xpath(
+        ".//main:Directory[@Id='INSTALLFOLDER']", namespaces=xpath_ns
+    )[0]
+    package = wxs_root.xpath(".//main:Package", namespaces=xpath_ns)[0]
 
-root_dir = "dist\\papis\\_internal"
+    root_dir = os.path.join("dist", "papis", "_internal")
+    directories = et.SubElement(
+        component, "Directory", Name="_internal", Id=get_id(root_dir)
+    )
+    component_group = et.SubElement(package, "ComponentGroup", Id="_internal")
 
-directories = et.SubElement(
-    component, "Directory", Name="_internal", Id=get_id(root_dir)
-)
-component_group = et.SubElement(package, "ComponentGroup", Id="_internal")
+    for root, dirs, files in os.walk(root_dir):
+        for dir in dirs:
+            relpath = os.path.relpath(root, root_dir)
+            parent_path = os.path.dirname(relpath)
+            if parent_path == "":
+                parent_element = directories
+            else:
+                parent_element_id = get_id(root_dir, parent_path)
+                xpath = f".//Directory[@Id='{parent_element_id}']"
+                parent_element = directories.xpath(xpath)[0]
 
-for root, dirs, files in os.walk(root_dir):
-    for dir in dirs:
-        relpath = os.path.relpath(root, root_dir)
-        parent_path = os.path.dirname(relpath)
-        if parent_path == "":
-            parent_element = directories
-        else:
-            parent_element_id = get_id(root_dir, parent_path)
-            xpath = f".//Directory[@Id='{parent_element_id}']"
-            parent_element = directories.xpath(xpath)[0]
+            et.SubElement(parent_element, "Directory", Name=dir, Id=get_id(root, dir))
 
-        et.SubElement(parent_element, "Directory", Name=dir, Id=get_id(root, dir))
+        if files:
+            for file in files:
+                component = et.SubElement(
+                    component_group,
+                    "Component",
+                    Guid=str(uuid.uuid4()),
+                    Id=f"c{get_id(root, file)}",
+                    Directory=get_id(root),
+                )
+                et.SubElement(
+                    component, "File", KeyPath="yes", Source=os.path.join(root, file)
+                )
 
-    if files:
-        for index, file in enumerate(files):
-            component = et.SubElement(
-                component_group,
-                "Component",
-                Guid=str(uuid.uuid4()),
-                Id=f"c{get_id(root, file)}",
-                Directory=get_id(root),
-            )
-            et.SubElement(
-                component, "File", KeyPath="yes", Source=os.path.join(root, file)
-            )
+    wxs_tree.write(outfile, pretty_print=True)
 
-with open("scripts\\windows\\papis.wxs", mode="wb") as f:
-    f.write(et.tostring(wxs_root, pretty_print=True))
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("template", help="path to a template file")
+    parser.add_argument("-o", "--outfile", default=None,
+                        help="output file generated from the template")
+    args = parser.parse_args()
+
+    outfile = args.outfile
+    if outfile is None:
+        outfile = os.path.join(os.path.dirname(args.template), "papis.wxs")
+
+    render_template(args.template, outfile)
