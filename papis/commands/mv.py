534fd81c9c4172af5e23ed4314f8ad5fe4f0ca36
Enric Morales <me@enric.me>
2024-04-17 22:08:24 +0200

    feat: add batch move with formatting of folder names

--- papis/commands/mv.py
+++ papis/commands/mv.py
@@ -1,5 +1,54 @@
 """
-This command can be used to move a document to a new folder.
+The ``mv`` command is used to organise a library moving their documents inside
+subfolders, which can be optionally created from a format adapted to each
+document.
+
+It will (except when run with ``--all``) bring up the picker with a list of
+documents that match the query. In the picker, you can select one or more
+documents and then initiate renaming by pressing enter. Folder names are cleaned,
+so that various characters (white spaces, punctuation, capital letters, and some
+others) are automatically converted.
+
+Examples
+^^^^^^^^
+
+- Query documents by author "Rick Astley" and move some of them. After picking
+  the relevant documents, Papis will prompt you for the subfolder where they
+  should be placed.
+
+   .. code:: sh
+
+        papis mv author:"Rick Astley"
+
+- You can use ``--folder-name`` to pass in the subfolder they should be
+  placed into, and ``--all`` to act on all documents that match the query.
+  Here we'll move all documents by author "Rick Astley" into a subfolder named
+  ``to_research``:
+
+   .. code:: sh
+
+        papis mv --folder-name to_research --all author:"Rick Astley"
+
+- You can use formatting rules to generate the folder name too. For instance, to
+  organise all documents by author "Rick Astley" by year, you can use (Python and
+  Jinja2 formatting, respectively):
+
+   .. code:: sh
+
+        # Python format
+        papis mv --folder-name "{doc[year]}" --all author:"Rick Astley"
+
+        # Jinja2 format
+        papis mv --folder-name "{{doc.year}}" --all author:"Rick Astley"
+
+- If you want to rename all documents without narrowing down your selection in
+  the picker, you can use the ``--all`` flag. Be careful when combining this
+  with ``--batch``, as you might end up renaming a lot folders without
+  confirmation:
+
+   .. code:: sh
+
+        papis rename --all author:"Rick Astley"
 
 Command-line Interface
 ^^^^^^^^^^^^^^^^^^^^^^
@@ -13,6 +62,7 @@ from typing import Optional, Tuple
 
 import click
 
+import papis.format
 import papis.config
 import papis.git
 import papis.utils
@@ -22,31 +72,31 @@ import papis.cli
 import papis.pick
 import papis.strings
 import papis.logging
+import papis.tui.utils
 from papis.exceptions import DocumentFolderNotFound
 
 logger = papis.logging.get_logger(__name__)
 
 
 def run(document: papis.document.Document,
-        new_folder_path: str,
+        dest_path: str,
         git: bool = False) -> None:
 
-    folder = document.get_main_folder()
-    if not folder:
+    doc_main_folder = document.get_main_folder()
+    if not doc_main_folder:
         raise DocumentFolderNotFound(papis.document.describe(document))
 
     if git:
-        papis.git.mv(folder, new_folder_path)
+        papis.git.mv(doc_main_folder, dest_path)
     else:
         import shutil
-        shutil.move(folder, new_folder_path)
+        shutil.move(doc_main_folder, dest_path)
 
     db = papis.database.get()
     db.delete(document)
 
-    new_document_folder = os.path.join(
-        new_folder_path,
-        os.path.basename(folder))
+    new_document_folder = os.path.join(dest_path,
+                                       os.path.basename(doc_main_folder))
     logger.debug("New document folder: '%s'.", new_document_folder)
 
     document.set_folder(new_document_folder)
@@ -55,58 +105,92 @@ def run(document: papis.document.Document,
 
 @click.command("mv")
 @click.help_option("--help", "-h")
+@click.option(
+    "--folder-name",
+    help="Name for the document's folder (papis format)"
+    )
 @papis.cli.query_argument()
 @papis.cli.git_option()
 @papis.cli.sort_option()
 @papis.cli.doc_folder_option()
+@papis.cli.all_option()
 def cli(query: str,
         git: bool,
+        _all: bool,
+        folder_name: str,
         sort_field: Optional[str],
         doc_folder: Tuple[str, ...],
         sort_reverse: bool) -> None:
-    """Move a document into some other path"""
+    """Move a document into another folder"""
     # Leave this imports here for performance
     import prompt_toolkit
     import prompt_toolkit.completion
 
-    documents = papis.cli.handle_doc_folder_query_sort(query,
-                                                       doc_folder,
-                                                       sort_field,
-                                                       sort_reverse)
+    documents = papis.cli.handle_doc_folder_query_all_sort(query,
+                                                           doc_folder,
+                                                           sort_field,
+                                                           sort_reverse,
+                                                           _all
+                                                           )
     if not documents:
         logger.warning(papis.strings.no_documents_retrieved_message)
         return
 
-    document = documents[0]
-
     lib_dir = os.path.expanduser(papis.config.get_lib_dirs()[0])
-
     completer = prompt_toolkit.completion.PathCompleter(
         only_directories=True,
-        get_paths=lambda: [lib_dir]
+        get_paths=lambda: [str(lib_dir)]
     )
 
-    try:
-        new_folder = os.path.join(
-            lib_dir,
-            prompt_toolkit.prompt(
-                message=(
-                    "Enter directory  : (Tab completion enabled)\n"
-                    "Current directory: ({dir})\n".format(
-                        dir=document.get_main_folder_name()
-                    ) + ">  "
-                ),
+    # If the user hasn't passed ``--folder_name``, we'll prompt for one that
+    # will be used throughout the process.
+    dest_path = None
+    if folder_name is None:
+        try:
+            subfolder = prompt_toolkit.prompt(
+                message="Destination folder: (Tab completion enabled)\n> ",
                 completer=completer,
-                complete_while_typing=True
-            ))
-    except Exception as exc:
-        logger.error("Failed to choose directory.", exc_info=exc)
-        return
+                complete_while_typing=True)
+            dest_path = os.path.join(lib_dir, subfolder)
+        except Exception as exc:
+            logger.error("Failed to choose directory.", exc_info=exc)
+            return
 
-    logger.info("New document folder: '%s'.", new_folder)
+    if not git and os.path.exists(os.path.join(lib_dir, ".git")):
+        git = papis.tui.utils.confirm("Add the changes to git?")
 
-    if not os.path.exists(new_folder):
-        logger.info("Creating path '%s'.", new_folder)
-        os.makedirs(new_folder, mode=papis.config.getint("dir-umask") or 0o666)
+    moves = []
+    for document in documents:
+        # If the user has passed ``--folder-name``, use that to generate the subfolder
+        if folder_name is not None:
+            subfolder = papis.format.format(folder_name, document)
+            dest_path = os.path.join(lib_dir, subfolder)
 
-    run(document, new_folder, git=git)
+        # This should never be reached, but just in case.
+        if dest_path is None:
+            logger.error("Failed to construct a folder path for the document.")
+            return
+
+        # Check against erroneous documents
+        doc_main_folder = document.get_main_folder()
+        if not doc_main_folder:
+            raise DocumentFolderNotFound(papis.document.describe(document))
+
+        # This can happen when using format but a key doesn't exist and so the
+        # format expression evaluates to ''.
+        if dest_path.rstrip(os.path.sep) == os.path.dirname(doc_main_folder):
+            logger.warning("Skipping '%s': its source and destination are the same",
+                           doc_main_folder)
+            continue
+
+        if not os.path.exists(dest_path):
+            logger.info("Creating path '%s'.", dest_path)
+            os.makedirs(dest_path, mode=papis.config.getint("dir-umask") or 0o666)
+
+        logger.info("Moving document from '%s' to the destination subfolder: '%s'.",
+                    doc_main_folder, dest_path)
+
+        moves.append((document, dest_path))
+
+    for document, dest_path in moves:
+        run(document, dest_path, git=git)
