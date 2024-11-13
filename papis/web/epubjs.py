340e7f61817f00d24f163e1a0a32877f65d1dda8
gesh <gesh@gesh.uni.cx>
2024-11-13 16:28:38 +0200

    Align code structure of epubjs.py and pdfjs.py
    Done to guide planning for #946, this makes diffing easier

--- papis/web/epubjs.py
+++ papis/web/epubjs.py
@@ -1,15 +1,45 @@
 import os.path
+import urllib.parse
 
 import dominate.tags as t
 
 import papis.web.html as wh
 import papis.web.static
 
-VIEWER_PATH = "epubjs-reader/reader/index.html"
 EPUBJS_URL = "https://github.com/futurepress/epubjs-reader"
+VIEWER_PATH = "epubjs-reader/reader/index.html"
 
 
-def detect_local_installation() -> bool:
+def widget(unquoted_file_path: str) -> None:
+    """
+    Widget for epub files.
+    """
+
+    _file_path = urllib.parse.quote(unquoted_file_path, safe="")
+
+    viewer_path = f"/static/{VIEWER_PATH}?bookPath={_file_path}"
+
+    with wh.flex("center"):
+        with t.div(cls="btn-group", role="group"):
+            with t.a(href=viewer_path,
+                     cls="btn btn-outline-success",
+                     target="_blank"):
+                wh.icon_span("square-arrow-up-right", "Open in new window")
+            with t.a(href=unquoted_file_path,
+                     cls="btn btn-outline-success",
+                     target="_blank"):
+                wh.icon_span("download", "Download")
+
+    if detect_epubjs():
+        t.iframe(src=viewer_path,
+                 style="resize: vertical",
+                 width="100%",
+                 height="800")
+    else:
+        t.pre(error_message(), cls="alert alert-warning")
+
+
+def detect_epubjs() -> bool:
     for path in papis.web.static.static_paths():
         viewer = os.path.join(path, VIEWER_PATH)
         if os.path.exists(viewer):
@@ -37,30 +67,3 @@ On linux and mac you can simply run the following lines
     mkdir -p ~/.config/papis/web/
     git clone {EPUBJS_URL} ~/.config/papis/web/epubjs-reader
     """
-
-
-def widget(unquoted_file_path: str) -> None:
-    """
-    Widget for epub files.
-    """
-
-    viewer_path = f"/static/{VIEWER_PATH}?bookPath={unquoted_file_path}"
-
-    with wh.flex("center"):
-        with t.div(cls="btn-group", role="group"):
-            with t.a(href=viewer_path,
-                     cls="btn btn-outline-success",
-                     target="_blank"):
-                wh.icon_span("square-arrow-up-right", "Open in new window")
-            with t.a(href=unquoted_file_path,
-                     cls="btn btn-outline-success",
-                     target="_blank"):
-                wh.icon_span("download", "Download")
-
-    if detect_local_installation():
-        t.iframe(src=viewer_path,
-                 style="resize: vertical",
-                 width="100%",
-                 height="800")
-    else:
-        t.pre(error_message(), cls="alert alert-warning")
