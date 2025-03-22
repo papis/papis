87bf6af2f94a9480f32cbf39a256c7a02d9abbb4
Alexandru Fikl <alexfikl@gmail.com>
2025-03-22 21:49:17 +0200

    feat(database): cleanup database base class interface
    * Add some token documentation to all methods
    * Add deprecation warnings for some uneeded methods
    * Add some more checking to implemented functions
    * Remove unused `match` function

--- papis/database/base.py
+++ papis/database/base.py
@@ -1,125 +1,144 @@
-"""
-Here the database abstraction for the libraries is defined.
-"""
-
 from abc import ABC, abstractmethod
-from typing import Optional, List, Dict
+from typing import Dict, List, Optional
+from warnings import warn
 
-import papis.utils
-import papis.config
-import papis.library
-import papis.document
-import papis.id
+from papis.document import Document
+from papis.library import Library
 
 
 class Database(ABC):
-    """Abstract class for the database backends
-    """
+    """Abstract base class for Papis caching database backends."""
 
-    def __init__(self, library: Optional[papis.library.Library] = None) -> None:
-        self.lib = library or papis.config.get_lib()
-        assert isinstance(self.lib, papis.library.Library)
+    def __init__(self, library: Optional[Library] = None) -> None:
+        if library is None:
+            from papis.config import get_lib
+            library = get_lib()
+
+        if not isinstance(library, Library):
+            raise TypeError(f"Provided library has unsupported type: {type(library)}")
+
+        self.lib = library
 
     @abstractmethod
     def initialize(self) -> None:
-        pass
+        """Initialize the caching database backend.
+
+        This can involve creating any necessary directories, opening files, etc.
+        This function should be called in the constructor of the database class,
+        as needed.
+        """
 
     @abstractmethod
     def get_backend_name(self) -> str:
-        pass
+        """Get the name of the database backend.
 
-    def get_lib(self) -> str:
-        """Get library name
+        This name has to match the one used in the configuration file in the
+        :confval:`database-backend` setting.
         """
-        return self.lib.name
 
-    def get_dirs(self) -> List[str]:
-        """Get directories of the library
-        """
-        return self.lib.paths
-
+    @abstractmethod
     def get_cache_path(self) -> str:
-        """Get the path to the actual cache file or directory.
-        """
-        raise NotImplementedError("Cache path not defined for backend '{}'"
-                                  .format(self.get_backend_name()))
-
-    def match(
-            self,
-            document: papis.document.Document,
-            query_string: str) -> bool:
-        """
-        Whether or not document matches query_string.
-
-        :param document: Document to be matched
-        :param query_string: Query string
-        """
-        raise NotImplementedError(
-            f"Match not defined for backend '{self.get_backend_name()}'")
+        """Get the path to the database cache file (or directory)."""
 
     @abstractmethod
     def clear(self) -> None:
-        pass
+        """Clear the database by removing all files and directories.
+
+        After clearing the database, calling :meth:`initialize` may be necessary
+        to ensure that it is in the correct state.
+        """
 
     @abstractmethod
-    def add(self, document: papis.document.Document) -> None:
-        pass
+    def add(self, document: Document) -> None:
+        """Add a new document to the database."""
 
     @abstractmethod
-    def update(self, document: papis.document.Document) -> None:
-        pass
+    def update(self, document: Document) -> None:
+        """Update an existing document in the database.
+
+        The given *document* will overwrite all the common fields of the document
+        that already exists in the database, if any.
+        """
 
     @abstractmethod
-    def delete(self, document: papis.document.Document) -> None:
-        pass
+    def delete(self, document: Document) -> None:
+        """Remove a document from the database."""
 
     @abstractmethod
-    def query(self, query_string: str) -> List[papis.document.Document]:
-        pass
+    def query(self, query_string: str) -> List[Document]:
+        """Find a document in the database by the given *query_string*.
+
+        The query string can have a more complex syntax based on the database
+        backend.
+        """
 
     @abstractmethod
-    def query_dict(
-            self, query: Dict[str, str]) -> List[papis.document.Document]:
-        pass
+    def query_dict(self, query: Dict[str, str]) -> List[Document]:
+        """Find a document in the database that matches the keys in *query*."""
 
     @abstractmethod
-    def get_all_documents(self) -> List[papis.document.Document]:
-        pass
+    def get_all_documents(self) -> List[Document]:
+        """Get all documents in the database."""
 
     @abstractmethod
     def get_all_query_string(self) -> str:
-        pass
+        """Get the default query string that will match all documents."""
 
-    def find_by_id(self, identifier: str) -> Optional[papis.document.Document]:
-        results = self.query_dict({Database.get_id_key(): identifier})
+    def find_by_id(self, identifier: str) -> Optional[Document]:
+        """Find a document in the library by its Papis ID *identifier*."""
+        from papis.id import ID_KEY_NAME
+
+        results = self.query_dict({ID_KEY_NAME: identifier})
         if len(results) > 1:
-            raise ValueError("More than one document matches the unique id '{}'"
-                             .format(identifier))
+            raise ValueError(f"More than one document matches the ID '{identifier}'")
 
         return results[0] if results else None
 
+    def maybe_compute_id(self, doc: Document) -> None:
+        """Compute a Papis ID for the document *doc*.
+
+        If the document already has an ID, then the document is skipped and the
+        ID is not checked for duplicates. Otherwise a new unique ID is created
+        and the document :ref:`info-file` is updated accordingly.
+        """
+        from papis.id import ID_KEY_NAME, compute_an_id
+
+        if ID_KEY_NAME in doc:
+            return
+
+        # NOTE: `compute_an_id` adds a random seed to the ID, so this is quite
+        # unlikely to become an infinite loop
+        new_id = compute_an_id(doc)
+        while self.query_dict({ID_KEY_NAME: new_id}):
+            new_id = compute_an_id(doc)
+
+        # FIXME: Should this save the document?
+        doc[ID_KEY_NAME] = new_id
+        doc.save()
+
+    def get_lib(self) -> str:
+        warn(f"Calling '{type(self).__name__}.get_lib' directly is deprecated "
+             "and will be removed in the next version of Papis (after 0.15). Use "
+             "the 'self.lib.name' member directly.",
+             DeprecationWarning, stacklevel=2)
+
+        return self.lib.name
+
+    def get_dirs(self) -> List[str]:
+        warn(f"Calling '{type(self).__name__}.get_dirs' directly is deprecated "
+             "and will be removed in the next version of Papis (after 0.15). Use "
+             "the 'self.lib.paths' member directly.",
+             DeprecationWarning, stacklevel=2)
+
+        return self.lib.paths
+
     @staticmethod
     def get_id_key() -> str:
-        """
-        Get the unique key identifier name of the documents in the database
-        """
-        return papis.id.key_name()
+        from warnings import warn
 
-    def maybe_compute_id(self, doc: papis.document.Document) -> None:
-        """
-        Compute a Papis ID for the document doc.
+        warn("This function is deprecated and will be removed in the next "
+             "version of Papis (after 0.15). Use 'papis.id.ID_KEY_NAME' instead.",
+             DeprecationWarning, stacklevel=2)
 
-        If the document already has an ID, then the document is skipped (without
-        checking for duplicates). Otherwise try to create a new ID that is
-        unique in this database and update the document YAML accordingly.
-        """
-        key_name = papis.id.key_name()
-        if key_name in doc:
-            return
-        while True:
-            new_id = papis.id.compute_an_id(doc)
-            other_docs = self.query_dict({key_name: new_id})
-            if not other_docs:
-                break
-        doc[key_name] = new_id
-        doc.save()
+        from papis.id import ID_KEY_NAME
+        return ID_KEY_NAME
