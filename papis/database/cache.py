f0040d0104ef057e7e5fb5954cbabd06e42949e6
Alexandru Fikl <alexfikl@gmail.com>
2025-03-22 23:17:56 +0200

    feat(database): cleanup and update docs for papis database

--- papis/database/cache.py
+++ papis/database/cache.py
@@ -1,57 +1,23 @@
 import os
-import sys
 from typing import Dict, List, Match, Optional, Pattern, Tuple
 
-import papis.utils
-import papis.docmatcher
-import papis.document
 import papis.config
-import papis.format
-import papis.database.base
 import papis.logging
+from papis.database.base import Database as DatabaseBase, get_cache_file_path
+from papis.document import Document, describe
+from papis.exceptions import DocumentFolderNotFound
+from papis.library import Library
 from papis.strings import AnyString
 
 logger = papis.logging.get_logger(__name__)
 
 
-def get_cache_file_name(libpaths: str) -> str:
-    """Create a cache file name out of the path of a given directory.
+def filter_documents(documents: List[Document], search: str = "") -> List[Document]:
+    """Filter documents based on the *search* string.
 
-    :param libpaths: folder names to be used as a seed for the cache name.
-    :returns: a name for the cache file specific to *libpaths*.
-
-    >>> get_cache_file_name('path/to/my/lib')
-    'a8c689820a94babec20c5d6269c7d488-lib'
-    >>> get_cache_file_name('papers')
-    'a566b2bebc62611dff4cdaceac1a7bbd-papers'
-    """
-    import hashlib
-    return "{}-{}".format(
-        hashlib.md5(libpaths.encode()).hexdigest(),
-        os.path.basename(libpaths))
-
-
-def get_cache_file_path(libpaths: str) -> str:
-    """Get the full path to the cache file.
-
-    :param libpaths: a cache file specific for the given library paths.
-    """
-    cache_name = get_cache_file_name(libpaths)
-    folder = os.path.join(papis.utils.get_cache_home(), "database")
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-
-    return os.path.join(folder, cache_name)
-
-
-def filter_documents(
-        documents: List[papis.document.Document],
-        search: str = "") -> List[papis.document.Document]:
-    """Filter documents. It can be done in a multi core way.
-
-    :param documents: List of Papis documents.
-    :param search: Valid Papis search string.
-    :returns: List of filtered documents
+    :param search: a search string that will be parsed by
+        :class:`~papis.docmatcher.DocMatcher`.
+    :returns: a list of filtered documents.
 
     >>> document = papis.document.from_data({'author': 'einstein'})
     >>> len(filter_documents([document], search="einstein")) == 1
@@ -62,38 +28,44 @@ def filter_documents(
     False
 
     """
-    papis.docmatcher.DocMatcher.set_search(search)
-    papis.docmatcher.DocMatcher.set_matcher(match_document)
-    papis.docmatcher.DocMatcher.parse()
+    from papis.docmatcher import DocMatcher
+
+    DocMatcher.set_search(search)
+    DocMatcher.set_matcher(match_document)
+    DocMatcher.parse()
 
     logger.debug("Filtering %d docs (search '%s').", len(documents), search)
 
+    import sys
     import time
-    begin_t = time.time()
+
+    t_start = time.time()
+
     # FIXME: find a better solution for this that works for both OSes
     if sys.platform == "win32":
         filtered_docs = [
-            d for d in [papis.docmatcher.DocMatcher.return_if_match(d)
-                        for d in documents] if d is not None]
+            d for d in (DocMatcher.return_if_match(d) for d in documents)
+            if d is not None]
     else:
-        result = papis.utils.parmap(papis.docmatcher.DocMatcher.return_if_match,
-                                    documents)
+        from papis.utils import parmap
+
+        result = parmap(DocMatcher.return_if_match, documents)
         filtered_docs = [d for d in result if d is not None]
 
-    _delta = 1000 * (time.time() - begin_t)
-    logger.debug("Finished filtering in %.2fms (%d docs).", _delta, len(filtered_docs))
+    t_delta = 1000 * (time.time() - t_start)
+    logger.debug("Finished querying in %.2fms (%d docs).", t_delta, len(filtered_docs))
 
     return filtered_docs
 
 
 def match_document(
-        document: papis.document.Document,
+        document: Document,
         search: Pattern[str],
         match_format: Optional[AnyString] = None,
         doc_key: Optional[str] = None) -> Optional[Match[str]]:
     """Match a document's keys to a given search pattern.
 
-    See ``papis.docmatcher.MatcherCallable``.
+    See :class:`~papis.docmatcher.MatcherCallable`.
 
     >>> from papis.docmatcher import get_regex_from_search as regex
     >>> document = papis.document.from_data({'author': 'einstein'})
@@ -107,122 +79,145 @@ def match_document(
     if doc_key is not None:
         match_string = str(document[doc_key])
     else:
+        from papis.format import format
+
         match_format = match_format or papis.config.getformattedstring("match-format")
-        match_string = papis.format.format(match_format, document)
+        match_string = format(match_format, document)
 
     return search.match(match_string)
 
 
-class Database(papis.database.base.Database):
+class Database(DatabaseBase):
+    """A caching database backend for Papis based on :mod:`pickle`."""
 
-    def __init__(self, library: Optional[papis.library.Library] = None) -> None:
+    def __init__(self, library: Optional[Library] = None) -> None:
         super().__init__(library)
 
-        self.documents: Optional[List[papis.document.Document]] = None
+        self.use_cache = papis.config.getboolean("use-cache")
+        self.documents: Optional[List[Document]] = None
         self.initialize()
-
-    def get_cache_path(self) -> str:
-        return self._get_cache_file_path()
 
     def get_backend_name(self) -> str:
         return "papis"
 
+    def get_cache_path(self) -> str:
+        return self._get_cache_file_path()
+
+    def get_all_query_string(self) -> str:
+        return "."
+
     def initialize(self) -> None:
         pass
 
-    def get_documents(self) -> List[papis.document.Document]:
+    def clear(self) -> None:
+        cache_path = self._get_cache_file_path()
+        if os.path.exists(cache_path):
+            logger.info("Clearing cache at '%s'.", cache_path)
+            os.remove(cache_path)
+
+        if self.documents:
+            self.documents.clear()
+            self.documents = None
+
+    def add(self, document: Document) -> None:
+        if not self.use_cache:
+            return
+
+        folder = document.get_main_folder()
+        if folder is None:
+            raise ValueError("Cannot add a document without a main folder to database")
+
+        if not os.path.exists(folder):
+            raise ValueError(f"Document folder '{folder}' does not exist")
+
+        logger.debug("Adding document: '%s'.", describe(document))
+        docs = self._get_documents()
+
+        self.maybe_compute_id(document)
+        docs.append(document)
+
+        self._save_documents()
+
+    def update(self, document: Document) -> None:
+        if not self.use_cache:
+            return
+
+        logger.debug("Updating document: '%s'.", describe(document))
+
+        docs = self._get_documents()
+        result = self._locate_document(document)
+        if not result:
+            raise DocumentFolderNotFound(describe(document))
+
+        index, _ = result[0]
+        docs[index] = document
+        self._save_documents()
+
+    def delete(self, document: Document) -> None:
+        if not self.use_cache:
+            return
+
+        logger.debug("Deleting document: '%s'.", describe(document))
+
+        docs = self._get_documents()
+        result = self._locate_document(document)
+        if not result:
+            raise DocumentFolderNotFound(describe(document))
+
+        index, _ = result[0]
+        docs.pop(index)
+        self._save_documents()
+
+    def query(self, query_string: str) -> List[Document]:
+        logger.debug("Querying database for '%s'.", query_string)
+
+        docs = self._get_documents()
+        if query_string == self.get_all_query_string():
+            return docs
+
+        return filter_documents(docs, query_string)
+
+    def query_dict(self, query: Dict[str, str]) -> List[Document]:
+        query_string = " ".join(f'{key}:"{val}" ' for key, val in query.items())
+        return self.query(query_string)
+
+    def get_all_documents(self) -> List[Document]:
+        return self._get_documents()
+
+    def _get_documents(self) -> List[Document]:
         if self.documents is not None:
             return self.documents
-        use_cache = papis.config.getboolean("use-cache")
+
         cache_path = self._get_cache_file_path()
-        if use_cache and os.path.exists(cache_path):
+        if self.use_cache and os.path.exists(cache_path):
             logger.debug("Getting documents from cache at '%s'.", cache_path)
 
             import pickle
             with open(cache_path, "rb") as fd:
                 self.documents = pickle.load(fd)
         elif self.lib.paths:
+            from papis.utils import get_folders, folders_to_documents
+
             logger.info("Indexing library. This might take a while...")
-            folders: List[str] = sum(
-                [papis.utils.get_folders(d) for d in self.lib.paths],
-                [])
-            self.documents = papis.utils.folders_to_documents(folders)
-            logger.debug("Computing 'papis_id' for each document.")
+            folders: List[str] = sum((get_folders(d) for d in self.lib.paths), [])
+            self.documents = folders_to_documents(folders)
+
+            from papis.id import ID_KEY_NAME
+            logger.debug("Computing '%s' for each document.", ID_KEY_NAME)
+
             for doc in self.documents:
                 self.maybe_compute_id(doc)
-            if use_cache:
-                self.save()
+
+            if self.use_cache:
+                self._save_documents()
         else:
             self.documents = []
 
         logger.debug("Loaded %d documents.", len(self.documents))
         return self.documents
 
-    def add(self, document: papis.document.Document) -> None:
-        logger.debug("Adding document: '%s'.", papis.document.describe(document))
-        docs = self.get_documents()
-        self.maybe_compute_id(document)
-        docs.append(document)
-        assert docs[-1].get_main_folder() == document.get_main_folder()
-        _folder = document.get_main_folder()
-        assert _folder is not None
-        assert os.path.exists(_folder)
-        self.save()
-
-    def update(self, document: papis.document.Document) -> None:
-        if not papis.config.getboolean("use-cache"):
-            return
-        logger.debug("Updating document: '%s'.", papis.document.describe(document))
-
-        docs = self.get_documents()
-        result = self._locate_document(document)
-        index = result[0][0]
-        docs[index] = document
-        self.save()
-
-    def delete(self, document: papis.document.Document) -> None:
-        if not papis.config.getboolean("use-cache"):
-            return
-        logger.debug("Deleting document: '%s'.", papis.document.describe(document))
-
-        docs = self.get_documents()
-        result = self._locate_document(document)
-        index = result[0][0]
-        docs.pop(index)
-        self.save()
-
-    def clear(self) -> None:
-        cache_path = self._get_cache_file_path()
-        logger.warning("Clearing cache at '%s'.", cache_path)
-
-        if os.path.exists(cache_path):
-            os.remove(cache_path)
-
-    def query_dict(self,
-                   dictionary: Dict[str, str]) -> List[papis.document.Document]:
-        query_string = " ".join(
-            [f'{key}:"{val}" ' for key, val in dictionary.items()])
-        return self.query(query_string)
-
-    def query(self, query_string: str) -> List[papis.document.Document]:
-        logger.debug("Querying database for '%s'.", query_string)
-
-        docs = self.get_documents()
-        # This makes it faster, if it's the all query string, return everything
-        # without filtering
-        if query_string == self.get_all_query_string():
-            return docs
-        else:
-            return filter_documents(docs, query_string)
-
-    def get_all_query_string(self) -> str:
-        return "."
-
-    def get_all_documents(self) -> List[papis.document.Document]:
-        return self.get_documents()
-
-    def save(self) -> None:
-        docs = self.get_documents()
+    def _save_documents(self) -> None:
+        docs = self._get_documents()
         logger.debug("Saving %d documents.", len(docs))
 
         import pickle
@@ -233,25 +228,27 @@ class Database(papis.database.base.Database):
     def _get_cache_file_path(self) -> str:
         return get_cache_file_path(self.lib.path_format())
 
-    def _locate_document(
-            self,
-            document: papis.document.Document
-            ) -> List[Tuple[int, papis.document.Document]]:
-        assert isinstance(document, papis.document.Document)
+    def _locate_document(self, document: Document) -> List[Tuple[int, Document]]:
+        from papis.id import ID_KEY_NAME
 
-        result = list(filter(
-            lambda d: d[1]["papis_id"] == document["papis_id"],
-            enumerate(self.get_documents())))
-
+        # FIXME: Why are we iterating twice over the documents here?
+        # first try to match by ID
+        result = [
+            (i, doc) for i, doc in enumerate(self._get_documents())
+            if doc[ID_KEY_NAME] == document[ID_KEY_NAME]
+        ]
         if result:
             return result
 
-        result = list(filter(
-            lambda d: d[1].get_main_folder() == document.get_main_folder(),
-            enumerate(self.get_documents())))
-        if not result:
-            raise ValueError(
-                "The document passed could not be found in the library: '{}'"
-                .format(papis.document.describe(document)))
+        # if no documents match, try matching by main folder
+        result = [
+            (i, doc) for i, doc in enumerate(self._get_documents())
+            if doc.get_main_folder() == document.get_main_folder()
+        ]
+        if result:
+            return result
 
-        return result
+        # otherwise, we error
+        raise ValueError(
+            f"Document could not be found in the library '{self.lib.name}': "
+            f"'{describe(document)}'")
