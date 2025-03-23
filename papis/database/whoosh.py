1a40a319b3f9f83ddc3e4d18b6f7fe5daf06b8c5
Alexandru Fikl <alexfikl@gmail.com>
2025-03-23 10:37:16 +0200

    feat(database): cleanup and update docs for whoosh database

--- papis/database/whoosh.py
+++ papis/database/whoosh.py
@@ -1,56 +1,48 @@
-"""This is the whoosh interface to Papis. For future Papis developers
-here are some considerations.
+"""This is the whoosh interface to Papis.
 
-Whoosh works with 3 main objects, the Index, the Writer and the Schema.
+For future Papis developers here are some considerations.
+
+Whoosh works with 3 main objects, the ``Index``, the ``Writer`` and the ``Schema``.
 The indices are stored in a subfolder of :func:`~papis.utils.get_cache_home`.
-The name of the indices folders is similar to the cache files of the papis
+The name of the indices folders is similar to the cache files of the ``papis``
 cache database.
 
-Once the index is created in the mentioned folder, a Schema is initialized,
+Once the Index is created in the mentioned folder, a Schema is initialized,
 which is a declaration of the data prototype of the database, or the
-definition of the table in sql parlance. This is controlled by the
-Papis configuration through the `whoosh-schema-prototype`. For instance
+definition of the table in SQL parlance. This is controlled by the
+Papis configuration through the :confval:`whoosh-schema-prototype`. For instance
 if the database is supposed to only contain the key fields
-``author``, ``title``, ``year`` and ``tags``, then the
-``whoosh-schema-prototype`` STRING should look like the following:
+``[author, title, year, tags]``, then the :confval:`whoosh-schema-prototype``
+string should look like the following:
 
-::
+.. code:: python
 
-        {
-            "author": TEXT(stored=True),
-            "title": TEXT(stored=True),
-            "year": TEXT(stored=True),
-            "tags": TEXT(stored=True),
-        }
+    {
+        "author": TEXT(stored=True),
+        "title": TEXT(stored=True),
+        "year": TEXT(stored=True),
+        "tags": TEXT(stored=True),
+    }
 
 where all the fields are explained in the whoosh
 `documentation <https://whoosh.readthedocs.io/en/latest/schema.html>`__.
 
-After this Schema is created, the folders of the library are recursed over
-and the documents are added to the database where only these
-properties are stored. This means, if ``publisher`` is not in the above list,
-you will not be able to parse the publisher through a search.
-
-.. note::
-
-    This is a point where maybe a great deal of discussion and optimization
-    should be made.
-
-
-
+After this Schema is created, the folders of the library are traversed
+and the documents are added to the database. When adding documents, only the
+keys in the schema are stored. This means, if ``publisher`` is not in the schema,
+you will not be able to search for the publisher through a query.
 """
+
 import os
-from typing import List, Dict, Optional, Any, KeysView, TYPE_CHECKING
+from typing import List, Dict, Optional, KeysView, TYPE_CHECKING
 
 import papis.config
-import papis.strings
-import papis.document
 import papis.logging
-import papis.database.base
-import papis.database.cache
-from papis.id import ID_KEY_NAME
-from papis.utils import get_cache_home, get_folders, folders_to_documents
+from papis.database.base import Database as DatabaseBase, get_cache_file_name
+from papis.document import Document, describe, from_folder
 from papis.exceptions import DocumentFolderNotFound
+from papis.id import ID_KEY_NAME
+from papis.library import Library
 
 if TYPE_CHECKING:
     from whoosh.index import Index
@@ -60,250 +52,204 @@ if TYPE_CHECKING:
 logger = papis.logging.get_logger(__name__)
 
 
-class Database(papis.database.base.Database):
-
-    def __init__(self, library: Optional[papis.library.Library] = None) -> None:
+class Database(DatabaseBase):
+    def __init__(self, library: Optional[Library] = None) -> None:
         super().__init__(library)
+
+        from papis.utils import get_cache_home
 
         self.cache_dir = os.path.join(get_cache_home(), "database", "whoosh")
         self.index_dir = os.path.expanduser(
-            os.path.join(
-                self.cache_dir,
-                papis.database.cache.get_cache_file_name(
-                    self.lib.path_format()
-                )))
+            os.path.join(self.cache_dir, get_cache_file_name(self.lib.path_format()))
+            )
 
         self.initialize()
-
-    def get_cache_path(self) -> str:
-        return self.index_dir
 
     def get_backend_name(self) -> str:
         return "whoosh"
 
-    def clear(self) -> None:
-        import shutil
-        if self.index_exists():
-            logger.warning("Clearing the database at '%s'...", self.get_cache_path())
-            shutil.rmtree(self.index_dir)
-
-    def add(self, document: papis.document.Document) -> None:
-        schema_keys = self.get_schema_init_fields().keys()
-
-        logger.debug("Adding document: '%s'.", papis.document.describe(document))
-        writer = self.get_writer()
-        self.add_document_with_writer(document, writer, schema_keys)
-
-        logger.debug("Committing document: '%s'.", papis.document.describe(document))
-        writer.commit()
-
-    def update(self, document: papis.document.Document) -> None:
-        """As it says in the docs, just delete the document and add it again
-        """
-        self.delete(document)
-        self.add(document)
-
-    def delete(self, document: papis.document.Document) -> None:
-        writer = self.get_writer()
-
-        logger.debug("Deleting document: '%s'.", papis.document.describe(document))
-        writer.delete_by_term(ID_KEY_NAME, self.get_id_value(document))
-
-        logger.debug("Committing document: '%s'.", papis.document.describe(document))
-        writer.commit()
-
-    def query_dict(self,
-                   dictionary: Dict[str, str]) -> List[papis.document.Document]:
-        query_string = " AND ".join(
-            [f'{key}:"{val}" ' for key, val in dictionary.items()])
-        return self.query(query_string)
-
-    def query(self, query_string: str) -> List[papis.document.Document]:
-        logger.debug("Querying database for '%s'.", query_string)
-
-        import whoosh.qparser
-        index = self.get_index()
-        qp = whoosh.qparser.MultifieldParser(["title", "author", "tags"],
-                                             schema=self.get_schema())
-        qp.add_plugin(whoosh.qparser.FuzzyTermPlugin())
-        query = qp.parse(query_string)
-        with index.searcher() as searcher:
-            results = searcher.search(query, limit=None)
-            logger.debug("Found %d results: %s", len(results), results)
-            documents = [
-                papis.document.from_folder(r.get("papis-folder"))
-                for r in results]
-        return documents
+    def get_cache_path(self) -> str:
+        return self.index_dir
 
     def get_all_query_string(self) -> str:
         return "*"
 
-    def get_all_documents(self) -> List[papis.document.Document]:
+    def initialize(self) -> None:
+        if self._index_exists():
+            index = self._get_index()
+
+            user_fields = self._get_schema_init_fields()
+            db_fields = index.schema
+
+            # NOTE: the database should be rebuilt if new fields have been added
+            # or if the field types have been changed in any way by the user
+            rebuild_db = (
+                set(user_fields) != set(db_fields.names())
+                or any(user_fields[name] != db_fields[name] for name in user_fields)
+            )
+            if not rebuild_db:
+                logger.debug("Initialized index found for library.")
+                return
+
+            logger.debug("Rebuilding database because fields do not match.")
+            self.clear()
+        else:
+            logger.info("Indexing library. This might take a while...")
+
+        self._create_index()
+        self._index_documents()
+
+    def clear(self) -> None:
+        import shutil
+        if self._index_exists():
+            logger.warning("Clearing the database at '%s'...", self.get_cache_path())
+            shutil.rmtree(self.index_dir)
+
+    def add(self, document: Document) -> None:
+        logger.debug("Adding document: '%s'.", describe(document))
+        schema_keys = self._get_schema_init_fields().keys()
+        index = self._get_index()
+        writer = index.writer()
+
+        self._add_document_with_writer(document, writer, schema_keys)
+        writer.commit()
+
+    def update(self, document: Document) -> None:
+        self.delete(document)
+        self.add(document)
+
+    def delete(self, document: Document) -> None:
+        logger.debug("Deleting document: '%s'.", describe(document))
+        index = self._get_index()
+        writer = index.writer()
+
+        writer.delete_by_term(ID_KEY_NAME, document[ID_KEY_NAME])
+        writer.commit()
+
+    def query(self, query_string: str) -> List[Document]:
+        logger.debug("Querying database for '%s'.", query_string)
+
+        import time
+        from whoosh.qparser import MultifieldParser, FuzzyTermPlugin
+
+        index = self._get_index()
+        qp = MultifieldParser(["title", "author", "tags"], schema=index.schema)
+        qp.add_plugin(FuzzyTermPlugin())
+
+        t_start = time.time()
+        query = qp.parse(query_string)
+        with index.searcher() as searcher:
+            results = searcher.search(query, limit=None)
+            documents = [from_folder(r.get("papis-folder")) for r in results]
+
+        t_delta = 1000 * (time.time() - t_start)
+        logger.debug("Finished querying in %.2fms (%d docs).", t_delta, len(documents))
+
+        return documents
+
+    def query_dict(self, query: Dict[str, str]) -> List[Document]:
+        query_string = " AND ".join(f'{key}:"{val}" ' for key, val in query.items())
+        return self.query(query_string)
+
+    def get_all_documents(self) -> List[Document]:
         return self.query(self.get_all_query_string())
 
-    def get_id_value(self, document: papis.document.Document) -> str:
-        """Get the value that is stored in the unique key identifier
-        of the documents in the database.
+    def _create_index(self) -> None:
+        """Create a new index.
 
-        :param document: Papis document
-        :returns: The papis-id
-        """
-        return str(document[ID_KEY_NAME])
-
-    def _get_doc_folder(self, document: papis.document.Document) -> str:
-        _folder = document.get_main_folder()
-        if _folder is None:
-            raise DocumentFolderNotFound(papis.document.describe(document))
-        else:
-            return _folder
-
-    def create_index(self) -> None:
-        """Create a brand new index, notice that if an index already
-        exists it will delete it and create a new one.
+        If an index already exists, this will delete it and create a new one.
         """
         logger.debug("Creating index.")
         if not os.path.exists(self.index_dir):
             logger.debug("Creating index directory '%s'.", self.index_dir)
             os.makedirs(self.index_dir)
 
-        import whoosh.index
-        whoosh.index.create_in(self.index_dir, self.create_schema())
+        from whoosh.index import create_in
+        create_in(self.index_dir, self._create_schema())
 
-    def index_exists(self) -> Any:
+    def _index_exists(self) -> bool:
         """Check if index already exists in :attr:`index_dir`."""
-        import whoosh.index
-        return whoosh.index.exists_in(self.index_dir)
+        from whoosh.index import exists_in
+        return bool(exists_in(self.index_dir))
 
-    def add_document_with_writer(self,
-                                 document: papis.document.Document,
-                                 writer: "IndexWriter",
-                                 schema_keys: KeysView[str]) -> None:
-        """Helper function that takes a writer and a dictionary
-        containing the keys of the schema and adds the document to the writer.
-        Notice that this function does only two things, creating a suitable
-        dictionary to be added to the database and adding it to the writer.
-        It DOES NOT commit the change to the writer, this has to be done
-        separately.
+    def _add_document_with_writer(self,
+                                  document: Document,
+                                  writer: "IndexWriter",
+                                  schema_keys: KeysView[str]) -> None:
+        """Helper function that adds a document document (without committing).
 
-        :param document: Papis document
-        :param writer: Whoosh writer
-        :param schema_keys: Dictionary containing the defining keys of the
-            database Schema
+        This function does only two things: creates a suitable dictionary to be
+        added to the database and adds it to the writer. It does not commit the
+        change to the writer, so that has to be done separately.
         """
-        # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        # maybe_compute_id overwrites the info file, so put it before
-        # anything else, else you'll get `papis-folder` on your info.yaml
-        # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        # NOTE: `maybe_compute_id` overwrites the info file, so put it before
+        # anything else, otherwise get `papis-folder` in your info.yaml
         self.maybe_compute_id(document)
-        document["papis-folder"] = self._get_doc_folder(document)
+
+        folder = document.get_main_folder()
+        if not folder:
+            raise DocumentFolderNotFound(describe(document))
+
+        document["papis-folder"] = folder
         doc_d = {k: str(document[k]) or "" for k in schema_keys}
         writer.add_document(**doc_d)
 
-    def do_indexing(self) -> None:
-        """This function initializes the database. Basically it goes through
-        all folders from the library (that contain an `info.yaml` file)
-        and adds the documents to the database index. This function is
-        expensive and will be called only if no index is present, so
-        at the time of building a brand new index.
+    def _index_documents(self) -> None:
+        """Initializes the database with an index of all the documents.
+
+        This function basically it goes through all folders from the library
+        (that contain an `info.yaml` file) and adds the documents to the database
+        index. It is quite expensive and will only be called if no index is present
+        or a rebuild is necessary.
         """
+        from papis.utils import get_folders, folders_to_documents
+
         logger.debug("Indexing the library, this might take a while...")
         folders: List[str] = sum([get_folders(d) for d in self.lib.paths], [])
         documents = folders_to_documents(folders)
-        schema_keys = self.get_schema_init_fields().keys()
-        writer = self.get_writer()
+
+        schema_keys = self._get_schema_init_fields().keys()
+        index = self._get_index()
+        writer = index.writer()
+
         for doc in documents:
-            self.add_document_with_writer(doc, writer, schema_keys)
+            self._add_document_with_writer(doc, writer, schema_keys)
         writer.commit()
 
-    def initialize(self) -> None:
-        """Function to be called every time a database object is created.
-        It checks if an index exists, if not, it creates one and
-        indexes the library.
-
-        If the schema fields have been changed, it updates the database.
-        """
-        if self.index_exists():
-            user_fields = self.get_schema_init_fields()
-            db_fields = self.get_schema()
-
-            user_field_names = sorted(user_fields)
-            db_field_names = sorted(db_fields.names())
-
-            # If the user fields and the fields in the DB
-            # aren't the same, then we have to rebuild the
-            # database.
-            if user_field_names != db_field_names:
-                logger.debug("Rebuilding database because field names do not match.")
-                self.rebuild()
-            else:
-                # Otherwise, verify that the fields are
-                # all the same and rebuild if any have
-                # changed at all.
-                rebuilt_db = False
-                for field in user_field_names:
-                    if user_fields[field] != db_fields[field]:
-                        logger.debug(
-                            "Rebuilding database because field types do not match.")
-
-                        self.rebuild()
-                        rebuilt_db = True
-                        break
-
-                if not rebuilt_db:
-                    logger.debug("Initialized index found for library.")
-                    return
-        else:
-            logger.info("Indexing library. This might take a while...")
-
-        self.create_index()
-        self.do_indexing()
-
-    def rebuild(self) -> None:
-        self.clear()
-        self.create_index()
-        self.do_indexing()
-
-    def get_index(self) -> "Index":
+    def _get_index(self) -> "Index":
         """Gets the index for the current library
         """
-        import whoosh.index
-        return whoosh.index.open_dir(self.index_dir)
+        from whoosh.index import open_dir
+        return open_dir(self.index_dir)
 
-    def get_writer(self) -> "IndexWriter":
-        """Gets the writer for the current library
-        """
-        return self.get_index().writer()
-
-    def get_schema(self) -> "Schema":
-        """Gets current schema
-        """
-        return self.get_index().schema
-
-    def create_schema(self) -> "Schema":
-        """Creates and returns whoosh schema to be applied to the library
-        """
-        logger.debug("Creating schema.")
-        fields = self.get_schema_init_fields()
-
+    def _create_schema(self) -> "Schema":
+        """Creates and returns whoosh schema to be applied to the library"""
         from whoosh.fields import Schema
+        logger.debug("Creating schema.")
+
+        fields = self._get_schema_init_fields()
         return Schema(**fields)
 
-    def get_schema_init_fields(self) -> Dict[str, "FieldType"]:
-        """Returns the arguments to be passed to the whoosh schema
-        object instantiation found in the method `get_schema`.
+    def _get_schema_init_fields(self) -> Dict[str, "FieldType"]:
         """
+        :returns: the keyword arguments to be passed to the whoosh schema object
+            (see :meth:`_create_schema`).
+        """
+        # NOTE: these are imported here so that `eval` sees them
         from whoosh.fields import TEXT, ID, KEYWORD, STORED  # noqa: F401
 
-        # This part is non-negotiable
+        # TODO: this is a security risk, find a way to fix it
+        user_prototype = eval(papis.config.getstring("whoosh-schema-prototype"))
+
+        # add default fields that should always be in the database
         fields = {
             ID_KEY_NAME: ID(stored=True, unique=True),
             "papis-folder": TEXT(stored=True)
         }
-
-        # TODO: this is a security risk, find a way to fix it
-        user_prototype = eval(papis.config.getstring("whoosh-schema-prototype"))
+        # add user provided fields
         fields.update(user_prototype)
 
+        # add simpler text fields
         fields_list = papis.config.getlist("whoosh-schema-fields")
         for field in fields_list:
             fields.update({field: TEXT(stored=True)})
