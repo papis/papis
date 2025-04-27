43bcd4bdc36cff40e8f641a2e87ba1659cc81c7b
Alexandru Fikl <alexfikl@gmail.com>
2025-04-27 09:57:27 +0300

    tests: add sqlite to ci testing

--- tests/database/test_sqlite.py
+++ tests/database/test_sqlite.py
@@ -11,28 +11,19 @@ if TYPE_CHECKING:
     from papis.testing import TemporaryLibrary
 
 
-@pytest.mark.library_setup(settings={"database-backend": "whoosh"})
+@pytest.mark.library_setup(settings={"database-backend": "sqlite"})
 def test_database_query(tmp_library: TemporaryLibrary) -> None:
-    pytest.importorskip("whoosh")
-
     db = papis.database.get()
-    assert db.get_backend_name() == "whoosh"
+    assert db.get_backend_name() == "sqlite"
 
     docs = db.query(db.get_all_query_string())
-    all_docs = db.get_all_documents()
     assert len(docs) > 0
-    assert len(docs) == len(all_docs)
 
 
-@pytest.mark.library_setup(settings={"database-backend": "whoosh"})
+@pytest.mark.library_setup(settings={"database-backend": "sqlite"})
 def test_cache_path(tmp_library: TemporaryLibrary) -> None:
-    pytest.importorskip("whoosh")
-
     db = papis.database.get()
-
     assert os.path.exists(db.get_cache_path())
-    assert os.path.isdir(db.get_cache_path())
 
     db.clear()
-
     assert not os.path.exists(db.get_cache_path())
