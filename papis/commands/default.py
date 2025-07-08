9d2949e83d9407bc0bd2080a0fc7a06181e1b3e8
Tuure Piitulainen <tuure.piitulainen@gmail.com>
2025-07-08 04:26:27 +0300

    feat: tab-complete library names

--- papis/commands/default.py
+++ papis/commands/default.py
@@ -138,6 +138,7 @@ def generate_profile_writing_function(profiler: "cProfile.Profile",
 @click.option(
     "-l", "--lib",
     help="Choose a library name or library path (unnamed library).",
+    type=papis.cli.LibraryParamType(),
     default=lambda: papis.config.getstring("default-library"))
 @click.option(
     "-c",
