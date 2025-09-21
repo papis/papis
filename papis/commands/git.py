7b0dbc5b27c6d967cf393532b30dcb3387bd7c4d
Alexandru Fikl <alexfikl@gmail.com>
2025-09-21 20:41:12 +0300

    feat: make git a standalone command

--- papis/commands/git.py
+++ papis/commands/git.py
@@ -25,13 +25,38 @@ Command-line interface
     :prog: papis git
 """
 
-import copy
-
 import click
 
-import papis.commands.run
+import papis.cli
 
-cli = copy.deepcopy(papis.commands.run.cli)
-cli.name = "git"
-cli.help = "Run git command in a library or document folder."
-cli = click.option("--prefix", hidden=True, default="git", type=str)(cli)
+
+@click.command("git", context_settings={"ignore_unknown_options": True})
+@click.argument("arguments", metavar="<ARGUMENTS>", nargs=-1)
+@click.help_option("--help", "-h")
+@click.option(
+    "--pick", "-p",
+    help="Give a query to pick a document to run the command in its folder.",
+    metavar="<QUERY>",
+    type=str,
+    default="")
+@papis.cli.sort_option()
+@papis.cli.doc_folder_option()
+@papis.cli.all_option()
+@click.pass_context
+def cli(ctx: click.Context,
+        arguments: list[str],
+        pick: str,
+        sort_field: str,
+        sort_reverse: bool,
+        doc_folder: tuple[str, ...],
+        _all: bool) -> None:
+    """Run git command in a library or document folder."""
+
+    from papis.commands.run import cli as run_cli
+    ctx.invoke(run_cli,
+               run_command=arguments,
+               pick=pick,
+               sort_field=sort_field, sort_reverse=sort_reverse,
+               prefix="git",
+               doc_folder=doc_folder,
+               _all=_all)
