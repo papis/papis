85d7ebca426f01bed251f8c7a911adc61a90ca1a
Alexandru Fikl <alexfikl@gmail.com>
2026-01-23 19:53:34 +0200

    fix: add upper bound for Sphinx on the CI

--- tools/ci-install.sh
+++ tools/ci-install.sh
@@ -2,7 +2,10 @@
 
 set -o errexit -o noglob -o pipefail
 
-# NOTE: setuptools is needed as a runtime dependency of isbnlib
-python -m pip install --upgrade setuptools
 python -m pip install --upgrade pip hatchling wheel
 python -m pip install --editable '.[develop,docs,optional]'
+
+# NOTE: setuptools is needed as a runtime dependency of isbnlib
+python -m pip install --upgrade setuptools
+# NOTE: sphinx>=9.1.0 uses 3.12 syntax for typing and breaks mypy on the CI
+python -m pip install --upgrade 'sphinx<9.1.0'
