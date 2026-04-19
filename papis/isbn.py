ae78e83c684ccb9dea75bed34f67f4d61bd9e29a
fdf-uni <171835561+fdf-uni@users.noreply.github.com>
2026-04-19 16:10:32 +0200

    fix: use get_session() and further minor fixes

--- papis/isbn.py
+++ papis/isbn.py
@@ -11,19 +11,17 @@ import requests
 
 import papis.config
 import papis.logging
+from papis.utils import get_session
 
 logger = papis.logging.get_logger(__name__)
 
 #: A list of services supported by :mod:`isbnlib`. Note that not all versions
 #: have support for all of these versions.
 ISBN_SERVICE_NAMES = ("goob", "openl", "wiki")
-USER_AGENT = "Mozilla/5.0"
 
 
 def format_title(title: str | None, subtitle: str | None = None) -> str:
-    return (f"{title or ''} - {subtitle}" if subtitle else title or "").strip(
-        ",.:;-_ "
-    )
+    return (f"{title or ''} - {subtitle}" if subtitle else title or "").strip(",.:;-_ ")
 
 
 def strip_isbnlike(isbn_like: str) -> str:
@@ -31,6 +29,14 @@ def strip_isbnlike(isbn_like: str) -> str:
 
 
 def notisbn_fallback(isbn: str | None) -> bool:
+    """
+    This method invalidates ISBN-like strings that do not conform to the
+    ISBN format. For this it verifies the length of the relevant symbols
+    as well as the checksum.
+    The latter are covered in the following Wikipedia article:
+    - https://en.wikipedia.org/wiki/ISBN#Check_digits
+    It might also be helpful to read this article's "Overview" section.
+    """
     if isbn is None:
         return True
     # Remove non-required symbols such as hyphens or spaces
@@ -52,77 +58,77 @@ def notisbn_fallback(isbn: str | None) -> bool:
     return sum((1 + 2 * (i % 2)) * int(isbn[i]) for i in range(13)) % 10 != 0
 
 
-def json_request(url: str) -> Any | None:
-    r = requests.get(url, headers={"user-agent": USER_AGENT})
-    if r.status_code != 200:
+def json_request(url: str, params: dict | None = None) -> Any | None:
+    with get_session() as session:
+        response = session.get(url, params=params)
+    if not response.ok:
         return None
     try:
-        data = r.json()
+        data = response.json()
     except requests.exceptions.JSONDecodeError:
         return None
     return data
 
 
-def googlebooks_isbn_search(query: str) -> str:
+def googlebooks_isbn_search(query: str) -> str | None:
     # This assumes the query to be an ISBN
     query = strip_isbnlike(query)
-    base_api_link = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
-    data = json_request(requests.utils.requote_uri(base_api_link + query))
+    url = "https://www.googleapis.com/books/v1/volumes"
+    params = {"q": f"isbn:{query}"}
+    data = json_request(url, params)
     if not data:
-        return ""
-    try:
-        items = data["items"]
-    except KeyError:
-        return ""
+        return None
+    if "items" not in data:
+        return None
     # Return the first ISBN that was provided by the API
-    for i in items:
+    for i in data["items"]:
         for d in i.get("volumeInfo", {}).get("industryIdentifiers", {}):
             # isbnlib also doesn't consider ISBN-10 results
             if d.get("type") == "ISBN_13":
                 return str(d["identifier"])
-    return ""
+    return None
 
 
-def isbn_from_words_fallback(query: str) -> str:
-    search_provider_url = "http://www.google.com/search?q="
-    search_url = (
-        search_provider_url
-        + "ISBN+"
-        + requests.utils.requote_uri(query.replace(" ", "+"))
-    )
-    r = requests.get(
-        search_url,
-        headers={
-            "user-agent": USER_AGENT,
+def isbn_from_words_fallback(query: str) -> str | None:
+    search_url = "http://www.google.com/search"
+    params = {"q": f"ISBN {query}"}
+
+    with get_session() as session:
+        session.headers.update({
             "Content-Type": 'text/plain; charset="UTF-8"',
             "Content-Transfer-Encoding": "Quoted-Printable",
-        },
-    )
+        })
+        response = session.get(search_url, params=params)
+    if not response.ok:
+        return None
     regex = re.compile(
-        r"97[89]-?[0-9]{10}|" r"97[89]-[-0-9]{13}|" r"[-0-9]{9,15}[0-9xX]",
+        r"97[89]-?[0-9]{10}|97[89]-[-0-9]{13}|[-0-9]{9,15}[0-9xX]",
         re.M,
     )
-    potential_isbns = regex.findall(r.text)
-    isbn = ""
+    potential_isbns = regex.findall(response.text)
     for i in potential_isbns:
         if not notisbn_fallback(i):
             isbn = googlebooks_isbn_search(i)
-            if len(isbn) > 0:
+            if isbn is not None:
                 break
     return isbn
 
 
 def meta_goob(isbn: str) -> dict[str, Any] | None:
-    url = (
-        "https://www.googleapis.com/books/v1/volumes?q={isbn_query}&fields="
-        "items/volumeInfo(title,subtitle,authors,publisher,publishedDate,"
-        "language,industryIdentifiers,description,imageLinks)&maxResults=1"
-    )
-    data = json_request(url.format(isbn_query="isbn:" + isbn))
+    url = "https://www.googleapis.com/books/v1/volumes"
+    params = {
+        "q": f"isbn:{isbn}",
+        "fields": "items/volumeInfo("
+        + "title,subtitle,authors,publisher,publishedDate,"
+        + "language,industryIdentifiers,description,imageLinks)",
+        "maxResults": "1",
+    }
+    data = json_request(url, params)
     # Apparently this sometimes works when the previous request doesn't.
     # See: https://github.com/xlcnd/isbnlib/issues/119
     if not data:
-        data = json_request(url.format(isbn_query=isbn))
+        params["q"] = isbn
+        data = json_request(url, params)
     if not data:
         return None
     try:
@@ -149,11 +155,9 @@ def meta_goob(isbn: str) -> dict[str, Any] | None:
 
 
 def meta_openl(isbn: str) -> dict[str, Any] | None:
-    url = (
-        f"https://openlibrary.org/api/books.json?bibkeys=ISBN:{isbn}"
-        "&jscmd=data"
-    )
-    data = json_request(url)
+    url = "https://openlibrary.org/api/books.json"
+    params = {"bibkeys": f"ISBN:{isbn}", "jscmd": "data"}
+    data = json_request(url, params)
     if not data:
         return None
     try:
@@ -165,9 +169,7 @@ def meta_openl(isbn: str) -> dict[str, Any] | None:
     title = volumeinfo.get("title", "").replace(" :", ":")
     subtitle = volumeinfo.get("subtitle", "")
     meta["Title"] = format_title(title, subtitle)
-    meta["Authors"] = [
-        a["name"] for a in volumeinfo.get("authors", [{"name": ""}])
-    ]
+    meta["Authors"] = [a["name"] for a in volumeinfo.get("authors", [{"name": ""}])]
     meta["Publisher"] = volumeinfo.get("publishers", [{"name": ""}])[0]["name"]
     if date := volumeinfo.get("publish_date", ""):
         match = re.search(r"\d{4}", date)
@@ -180,9 +182,8 @@ def meta_openl(isbn: str) -> dict[str, Any] | None:
 
 
 def meta_wiki(isbn: str) -> dict[str, Any] | None:
-    url = (
-        f"https://en.wikipedia.org/api/rest_v1/data/citation/mediawiki/{isbn}"
-    )
+    url = f"https://en.wikipedia.org/api/rest_v1/data/citation/mediawiki/{
+        isbn}"
     data = json_request(url)
     if not data:
         return None
@@ -196,9 +197,7 @@ def meta_wiki(isbn: str) -> dict[str, Any] | None:
     meta["Title"] = format_title(title)
     meta["Authors"] = [" ".join(a) for a in volumeinfo.get("author", [""])]
     if not meta["Authors"] or len(meta["Authors"]) == 0:
-        meta["Authors"] = [
-            " ".join(a) for a in volumeinfo.get("contributor", [""])
-        ]
+        meta["Authors"] = [" ".join(a) for a in volumeinfo.get("contributor", [""])]
     meta["Publisher"] = volumeinfo.get("publisher", "")
     if date := volumeinfo.get("date", ""):
         match = re.search(r"\d{4}", date)
@@ -225,26 +224,20 @@ def meta_fallback(isbn: str | None, service: str) -> dict[str, Any] | None:
 
 try:
     from isbnlib import notisbn
-
-    __all__ = ["notisbn"]
 except ImportError:
     notisbn = notisbn_fallback
 
 
-def get_data(
-    query: str = "", isbn_like: str = "", service: str | None = None
-) -> list[dict[str, Any]]:
+def get_data(query: str = "",
+             isbn_like: str = "", service: str | None = None) -> list[dict[str, Any]]:
     logger.debug("Trying to retrieve ISBN from query: '%s'.", query)
 
     if service is None:
         service = papis.config.getstring("isbn-service")
 
     if service not in ISBN_SERVICE_NAMES:
-        logger.error(
-            "ISBN service '%s' is not known. Available services: '%s'.",
-            service,
-            "', '".join(ISBN_SERVICE_NAMES),
-        )
+        logger.error("ISBN service '%s' is not known. Available services: '%s'.",
+                     service, "', '".join(ISBN_SERVICE_NAMES))
         return []
 
     if isbn_like:
@@ -289,26 +282,21 @@ def data_to_papis(data: dict[str, Any]) -> dict[str, Any]:
     )
 
     key_conversion = [
-        KeyConversionPair(
-            "authors", [{"key": "author_list", "action": split_authors_name}]
-        ),
-        KeyConversionPair(
-            "isbn-13",
-            [
-                {"key": "isbn", "action": None},
-                {"key": "isbn-13", "action": None},
-            ],
-        ),
-        KeyConversionPair(
-            "language",
-            [{"key": "language", "action": lambda x: x if x else "en"}],
-        ),
-    ]
+        KeyConversionPair("authors", [{
+            "key": "author_list",
+            "action": split_authors_name
+        }]),
+        KeyConversionPair("isbn-13", [
+            {"key": "isbn", "action": None},
+            {"key": "isbn-13", "action": None},
+        ]),
+        KeyConversionPair("language", [
+            {"key": "language", "action": lambda x: x if x else "en"}
+        ])
+        ]
 
     data = {k.lower(): data[k] for k in data}
-    result = keyconversion_to_data(
-        key_conversion, data, keep_unknown_keys=True
-    )
+    result = keyconversion_to_data(key_conversion, data, keep_unknown_keys=True)
 
     # NOTE: 'isbnlib' does not give a type at all, so we can't know if this is
     # a proceeding or any other book-like format. Also, 'isbnlib' always uses
