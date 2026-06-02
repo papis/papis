fba2348938281d5b98dd6d2be485b9f537074a99
Alexandru Fikl <alexfikl@gmail.com>
2026-06-02 11:26:59 +0300

    test: update tests for isbnlib changes

--- tools/ci-install.sh
+++ tools/ci-install.sh
@@ -4,6 +4,3 @@ set -o errexit -o noglob -o pipefail
 
 python -m pip install --upgrade pip hatchling wheel
 python -m pip install --editable '.[develop,docs,optional]'
-
-# NOTE: setuptools is needed as a runtime dependency of isbnlib
-python -m pip install --upgrade 'setuptools<82.0.0'
