1825c628aa74995a4c4391fb2bc86957d9203a50
Alexandru Fikl <alexfikl@gmail.com>
2026-08-08 17:04:35 +0300

    fix: use papis version in flake

--- flake.nix
+++ flake.nix
@@ -73,7 +73,13 @@
                 attrs
                 // {
                   # Because we're following main, use the git rev as version
-                  version = if (self ? rev) then self.shortRev else self.dirtyShortRev;
+                  version = if (self ? rev)
+                    then "${project.pyproject.project.version}+git.${self.shortRev}"
+                    else self.dirtyShortRev;
+
+                  nativeBuildInputs = (attrs.nativeBuildInputs or []) ++ [
+                    final.pyprojectVersionPatchHook
+                  ];
                 }
               );
           };
